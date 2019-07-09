796e6d83f2236f9e03ff5136424dbf0bda15d7ca
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-07-10 00:31:32 +0900

    add: MirageFairyColorSet

---
+++ src/main/java/miragefairy2019/mod/app/MirageFairyColorSet.java
@@ -0,0 +1,19 @@
+package miragefairy2019.mod.app;
+
+public final class MirageFairyColorSet
+{
+
+	public final int skin;
+	public final int bright;
+	public final int dark;
+	public final int hair;
+
+	public MirageFairyColorSet(int skin, int bright, int dark, int hair)
+	{
+		this.skin = skin;
+		this.bright = bright;
+		this.dark = dark;
+		this.hair = hair;
+	}
+
+}
