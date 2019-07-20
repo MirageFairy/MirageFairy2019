181f9383891ef520aee4368f727f4cdf7abb56ea
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-07-20 16:37:49 +0900

    EnumVariantOre1: 宝石の鉱石を破壊すると宝石アイテムをドロップする

--- src/main/java/miragefairy2019/mod/modules/ore/EnumVariantOre1.java
+++ src/main/java/miragefairy2019/mod/modules/ore/EnumVariantOre1.java
@@ -3,15 +3,22 @@ package miragefairy2019.mod.modules.ore;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
+import java.util.function.Supplier;
 
+import miragefairy2019.mod.api.ApiOre;
+import miragefairy2019.mod.lib.Utils;
 import mirrg.boron.util.suppliterator.ISuppliterator;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.MathHelper;
 
 public enum EnumVariantOre1 implements IStringSerializable, IOreVariant
 {
-	APATITE_ORE(0, "apatite_ore", "oreApatite", 1),
-	FLUORITE_ORE(1, "fluorite_ore", "oreFluorite", 2),
-	;
+	APATITE_ORE(0, "apatite_ore", "oreApatite", 1, new GemProvider(() -> ApiOre.itemStackGemApatite.copy(), 3, 2, 1, 3)),
+	FLUORITE_ORE(1, "fluorite_ore", "oreFluorite", 2, new GemProvider(() -> ApiOre.itemStackGemFluorite.copy(), 3, 2, 20, 40)),;
 
 	//
 
@@ -44,13 +51,35 @@ public enum EnumVariantOre1 implements IStringSerializable, IOreVariant
 	public final String resourceName;
 	public final String unlocalizedName;
 	public final int harvestLevel;
+	public final GemProvider nGemProvider;
 
-	private EnumVariantOre1(int metadata, String resourceName, String unlocalizedName, int harvestLevel)
+	private static class GemProvider
+	{
+
+		private final Supplier<ItemStack> sItemStack;
+		private final double amount;
+		private final double amountPerFortune;
+		private final int expMin;
+		private final int expMax;
+
+		public GemProvider(Supplier<ItemStack> sItemStack, double amount, double amountPerFortune, int expMin, int expMax)
+		{
+			this.sItemStack = sItemStack;
+			this.amount = amount;
+			this.amountPerFortune = amountPerFortune;
+			this.expMin = expMin;
+			this.expMax = expMax;
+		}
+
+	}
+
+	private EnumVariantOre1(int metadata, String resourceName, String unlocalizedName, int harvestLevel, GemProvider nGemProvider)
 	{
 		this.metadata = metadata;
 		this.resourceName = resourceName;
 		this.unlocalizedName = unlocalizedName;
 		this.harvestLevel = harvestLevel;
+		this.nGemProvider = nGemProvider;
 	}
 
 	public String toString()
@@ -85,6 +114,29 @@ public enum EnumVariantOre1 implements IStringSerializable, IOreVariant
 	public int getHarvestLevel()
 	{
 		return harvestLevel;
+	}
+
+	@Override
+	public void getDrops(NonNullList<ItemStack> drops, Random random, Block block, int metadata, int fortune)
+	{
+		if (nGemProvider != null) {
+			int count = Utils.randomInt(random, nGemProvider.amount + nGemProvider.amountPerFortune * fortune);
+			for (int i = 0; i < count; i++) {
+				drops.add(nGemProvider.sItemStack.get());
+			}
+		} else {
+			IOreVariant.super.getDrops(drops, random, block, metadata, fortune);
+		}
+	}
+
+	@Override
+	public int getExpDrop(Random random, int fortune)
+	{
+		if (nGemProvider != null) {
+			return MathHelper.getInt(random, nGemProvider.expMin, nGemProvider.expMax);
+		} else {
+			return IOreVariant.super.getExpDrop(random, fortune);
+		}
 	}
 
 }
