b16ce71e5dbe2aed2d501a48b3480beac1e58720
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-08-04 10:40:46 +0900

    AbilitySet: add: get

--- src/main/java/miragefairy2019/mod/api/fairy/AbilitySet.java
+++ src/main/java/miragefairy2019/mod/api/fairy/AbilitySet.java
@@ -1,5 +1,9 @@
 package miragefairy2019.mod.api.fairy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
 import mirrg.boron.util.struct.ImmutableArray;
 import mirrg.boron.util.struct.Tuple;
 
@@ -7,10 +11,19 @@ public final class AbilitySet
 {
 
 	public final ImmutableArray<Tuple<IAbilityType, Double>> tuples;
+	private Map<IAbilityType, Double> map = new HashMap<>();
 
 	public AbilitySet(ImmutableArray<Tuple<IAbilityType, Double>> tuples)
 	{
 		this.tuples = tuples;
+		for (Tuple<IAbilityType, Double> tuple : tuples) {
+			map.put(tuple.x, tuple.y);
+		}
+	}
+
+	public double get(EnumAbilityType abilityType)
+	{
+		return map.getOrDefault(abilityType, 0.0);
 	}
 
 }
