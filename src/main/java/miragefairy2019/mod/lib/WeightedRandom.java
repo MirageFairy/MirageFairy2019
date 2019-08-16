5f84d41fd4265706fad7334c6386c255edffca00
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-08-17 05:58:59 +0900

    WeightedRandom: add: unique

--- src/main/java/miragefairy2019/mod/lib/WeightedRandom.java
+++ src/main/java/miragefairy2019/mod/lib/WeightedRandom.java
@@ -1,8 +1,13 @@
 package miragefairy2019.mod.lib;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
+import java.util.function.BiPredicate;
+
+import mirrg.boron.util.suppliterator.ISuppliterator;
 
 public class WeightedRandom
 {
@@ -41,5 +46,49 @@ public class WeightedRandom
 			this.item = item;
 		}
 
+	}
+
+	public static <T> List<Item<T>> unique(List<Item<T>> dropTable, BiPredicate<T, T> equals)
+	{
+		class A
+		{
+
+			private T t;
+
+			public A(T t)
+			{
+				this.t = t;
+			}
+
+			@Override
+			public int hashCode()
+			{
+				return 0;
+			}
+
+			@Override
+			public boolean equals(Object obj)
+			{
+				if (this == obj) return true;
+				if (obj == null) return false;
+				if (getClass() != obj.getClass()) return false;
+				A other = (A) obj;
+				if (t == null) {
+					if (other.t != null) return false;
+				} else if (!equals.test(t, other.t)) return false;
+				return true;
+			}
+
+		}
+
+		Map<A, Double> map = new HashMap<>();
+
+		for (Item<T> item : dropTable) {
+			map.put(new A(item.item), map.getOrDefault(new A(item.item), 0.0) + item.weight);
+		}
+
+		return ISuppliterator.ofIterable(map.entrySet())
+			.map(e -> new Item<>(e.getKey().t, e.getValue()))
+			.toList();
 	}
 }
