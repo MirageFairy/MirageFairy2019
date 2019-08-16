88c90719bd6b34a8311a86449898746ddba66c49
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-08-17 06:00:56 +0900

    ドロップ機構をリニューアル

---
+++ src/main/java/miragefairy2019/mod/api/fairycrystal/RightClickDrops.java
@@ -0,0 +1,203 @@
+package miragefairy2019.mod.api.fairycrystal;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+
+import mirrg.boron.util.suppliterator.ISuppliterator;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.oredict.OreIngredient;
+
+public class RightClickDrops
+{
+
+	public static IRightClickDrop fixed(IDrop drop)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testUseItem(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+			{
+				return true;
+			}
+		};
+	}
+
+	public static IRightClickDrop world(IDrop drop, BiPredicate<World, BlockPos> predicate)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testWorld(World world, BlockPos pos)
+			{
+				return predicate.test(world, pos);
+			}
+		};
+	}
+
+	public static IRightClickDrop blocks(IDrop drop, Block... blocks)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testBlock(Block block)
+			{
+				for (Block block2 : blocks) {
+					if (block.equals(block2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	public static IRightClickDrop blockStates(IDrop drop, IBlockState... blockStates)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testBlockState(IBlockState blockState)
+			{
+				for (IBlockState blockState2 : blockStates) {
+					if (blockState.equals(blockState2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	public static IRightClickDrop items(IDrop drop, Item... items)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testItem(Item item)
+			{
+				for (Item item2 : items) {
+					if (item.equals(item2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	public static IRightClickDrop ores(IDrop drop, String... ores)
+	{
+		List<OreIngredient> ingredients = ISuppliterator.ofObjArray(ores)
+			.map(o -> new OreIngredient(o))
+			.toList();
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testItemStack(ItemStack itemStack)
+			{
+				for (OreIngredient ingredient : ingredients) {
+					if (ingredient.test(itemStack)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	public static IRightClickDrop biomeTypes(IDrop drop, BiomeDictionary.Type... biomeTypes)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testBiomeType(Type biomeType)
+			{
+				for (BiomeDictionary.Type biomeType2 : biomeTypes) {
+					if (biomeType.equals(biomeType2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	public static IRightClickDrop biomes(IDrop drop, Biome... biomes)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testBiome(Biome biome)
+			{
+				for (Biome biome2 : biomes) {
+					if (biome.equals(biome2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+	@SafeVarargs
+	public static IRightClickDrop classEntities(IDrop drop, Class<? extends Entity>... classEntities)
+	{
+		return new IRightClickDrop() {
+			@Override
+			public IDrop getDrop()
+			{
+				return drop;
+			}
+
+			@Override
+			public boolean testClassEntity(Class<? extends Entity> classEntity)
+			{
+				for (Class<? extends Entity> classEntity2 : classEntities) {
+					if (classEntity.isAssignableFrom(classEntity2)) return true;
+				}
+				return false;
+			}
+		};
+	}
+
+}
--- src/main/java/miragefairy2019/mod/modules/fairycrystal/ItemFairyCrystal.java
+++ src/main/java/miragefairy2019/mod/modules/fairycrystal/ItemFairyCrystal.java
@@ -1,24 +1,34 @@
 package miragefairy2019.mod.modules.fairycrystal;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import miragefairy2019.mod.api.ApiFairyCrystal;
+import miragefairy2019.mod.api.fairycrystal.IDrop;
 import miragefairy2019.mod.lib.WeightedRandom;
 import miragefairy2019.mod.modules.fairy.ModuleFairy;
 import mirrg.boron.util.suppliterator.ISuppliterator;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 
 public class ItemFairyCrystal extends Item
 {
@@ -33,15 +43,10 @@ public class ItemFairyCrystal extends Item
 			if (world.isRemote) return EnumActionResult.SUCCESS;
 
 			// ガチャリスト取得
-			List<WeightedRandom.Item<ItemStack>> list = ISuppliterator.ofIterable(ApiFairyCrystal.dropsFairyCrystal)
-				.mapIfPresent(d -> d.getDrop(player, world, pos, hand, facing, hitX, hitY, hitZ))
-				.map(d -> new WeightedRandom.Item<>(d.getItemStack(), d.getWeight()))
-				.toList();
-			double totalWeight = WeightedRandom.getTotalWeight(list);
-			if (totalWeight < 1) list.add(new WeightedRandom.Item<>(ModuleFairy.FairyTypes.air[0].createItemStack(), 1 - totalWeight));
+			List<WeightedRandom.Item<ItemStack>> dropTable = getDropTable(player, world, pos, hand, facing, hitX, hitY, hitZ);
 
 			// ガチャを引く
-			Optional<ItemStack> oItemStack = WeightedRandom.getRandomItem(world.rand, list);
+			Optional<ItemStack> oItemStack = WeightedRandom.getRandomItem(world.rand, dropTable);
 
 			// ガチャが成功した場合、
 			if (oItemStack.isPresent()) {
@@ -65,17 +70,14 @@ public class ItemFairyCrystal extends Item
 			if (!world.isRemote) return EnumActionResult.SUCCESS;
 
 			// ガチャリスト取得
-			List<WeightedRandom.Item<ItemStack>> list = ISuppliterator.ofIterable(ApiFairyCrystal.dropsFairyCrystal)
-				.mapIfPresent(d -> d.getDrop(player, world, pos, hand, facing, hitX, hitY, hitZ))
-				.map(d -> new WeightedRandom.Item<>(d.getItemStack(), d.getWeight()))
-				.toList();
-			double totalWeight = WeightedRandom.getTotalWeight(list);
-			if (totalWeight < 1) list.add(new WeightedRandom.Item<>(ModuleFairy.FairyTypes.air[0].createItemStack(), 1 - totalWeight));
+			List<WeightedRandom.Item<ItemStack>> dropTable = getDropTable(player, world, pos, hand, facing, hitX, hitY, hitZ);
 
 			// 表示
 			List<String> lines = new ArrayList<>();
 			lines.add("---- " + itemStackCrystal.getDisplayName() + " ----");
-			for (WeightedRandom.Item<ItemStack> item : ISuppliterator.ofIterable(list)
+			double totalWeight = WeightedRandom.getTotalWeight(dropTable);
+			for (WeightedRandom.Item<ItemStack> item : ISuppliterator.ofIterable(dropTable)
+				.sortedObj(i -> i.item.getDisplayName())
 				.sortedDouble(i -> i.weight)) {
 				lines.add("" + String.format("%f%%", 100 * item.weight / totalWeight) + ": " + item.item.getDisplayName());
 			}
@@ -85,6 +87,114 @@ public class ItemFairyCrystal extends Item
 			return EnumActionResult.SUCCESS;
 		}
 
+	}
+
+	private List<WeightedRandom.Item<ItemStack>> getDropTable(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+	{
+		BlockPos pos2 = world.getBlockState(pos).isFullBlock() ? pos.offset(facing) : pos;
+
+		Set<Block> blocks = new HashSet<>();
+		Set<IBlockState> blockStates = new HashSet<>();
+		Set<Item> items = new HashSet<>();
+		Set<ItemStack> itemStacks = new HashSet<>();
+		Set<Biome> biomes = new HashSet<>();
+		Set<BiomeDictionary.Type> biomeTypes = new HashSet<>();
+
+		// ワールドブロック
+		for (int xi = -2; xi <= 2; xi++) {
+			for (int yi = -2; yi <= 2; yi++) {
+				for (int zi = -2; zi <= 2; zi++) {
+					IBlockState blockState = world.getBlockState(pos.add(xi, yi, zi));
+					blockStates.add(blockState);
+					blocks.add(blockState.getBlock());
+				}
+			}
+		}
+
+		// インベントリ
+		for (ItemStack itemStack : player.inventory.mainInventory) {
+			itemStacks.add(itemStack);
+			items.add(itemStack.getItem());
+			Block block = Block.getBlockFromItem(itemStack.getItem());
+			if (block != Blocks.AIR) blocks.add(block);
+		}
+		for (ItemStack itemStack : player.inventory.armorInventory) {
+			itemStacks.add(itemStack);
+			items.add(itemStack.getItem());
+			Block block = Block.getBlockFromItem(itemStack.getItem());
+			if (block != Blocks.AIR) blocks.add(block);
+		}
+		for (ItemStack itemStack : player.inventory.offHandInventory) {
+			itemStacks.add(itemStack);
+			items.add(itemStack.getItem());
+			Block block = Block.getBlockFromItem(itemStack.getItem());
+			if (block != Blocks.AIR) blocks.add(block);
+		}
+
+		// バイオーム
+		{
+			Biome biome = world.getBiome(pos2);
+			biomes.add(biome);
+			biomeTypes.addAll(BiomeDictionary.getTypes(biome));
+		}
+
+		// エンティティ
+		List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(
+			player.posX - 10,
+			player.posY - 10,
+			player.posZ - 10,
+			player.posX + 10,
+			player.posY + 10,
+			player.posZ + 10));
+		Set<Class<? extends Entity>> classEntities = new HashSet<>();
+		for (Entity entity : entities) {
+			classEntities.add(entity.getClass());
+		}
+
+		// リスト作成
+		List<WeightedRandom.Item<ItemStack>> dropTable = ISuppliterator.ofIterable(ApiFairyCrystal.dropsFairyCrystal)
+			.mapIfNotNull(d -> {
+				IDrop drop = d.getDrop();
+
+				if (d.testUseItem(player, world, pos, hand, facing, hitX, hitY, hitZ)) return drop;
+				if (d.testWorld(world, pos2)) return drop;
+
+				for (Block block : blocks) {
+					if (d.testBlock(block)) return drop;
+				}
+				for (IBlockState blockState : blockStates) {
+					if (d.testBlockState(blockState)) return drop;
+				}
+				for (Item item : items) {
+					if (d.testItem(item)) return drop;
+				}
+				for (ItemStack itemStack : itemStacks) {
+					if (d.testItemStack(itemStack)) return drop;
+				}
+				for (Biome biome : biomes) {
+					if (d.testBiome(biome)) return drop;
+				}
+				for (BiomeDictionary.Type biomeType : biomeTypes) {
+					if (d.testBiomeType(biomeType)) return drop;
+				}
+				for (Class<? extends Entity> classEntity : classEntities) {
+					if (d.testClassEntity(classEntity)) return drop;
+				}
+				for (Entity entity : entities) {
+					if (d.testEntity(entity)) return drop;
+				}
+
+				return null;
+			})
+			.map(d -> new WeightedRandom.Item<>(d.getItemStack(), d.getWeight()))
+			.toList();
+		dropTable = WeightedRandom.unique(dropTable, (a, b) -> ItemStack.areItemStacksEqualUsingNBTShareTag(a, b));
+
+		// 1に満たない場合はairを入れて詰める
+		double totalWeight = WeightedRandom.getTotalWeight(dropTable);
+		if (totalWeight < 1) dropTable.add(new WeightedRandom.Item<>(ModuleFairy.FairyTypes.air[0].createItemStack(), 1 - totalWeight));
+
+		return dropTable;
 	}
 
 }
