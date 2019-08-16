72a2f1b9ccb8fe3014d131403ebe3832b7926f15
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-08-16 17:59:02 +0900

    ModuleFairyCrystal: 物質系妖精は対応する物を所持していないとガチャで出ない

--- src/main/java/miragefairy2019/mod/modules/fairycrystal/ModuleFairyCrystal.java
+++ src/main/java/miragefairy2019/mod/modules/fairycrystal/ModuleFairyCrystal.java
@@ -1,5 +1,7 @@
 package miragefairy2019.mod.modules.fairycrystal;
 
+import java.util.function.Predicate;
+
 import miragefairy2019.mod.ModMirageFairy2019;
 import miragefairy2019.mod.api.ApiFairyCrystal;
 import miragefairy2019.mod.api.ApiMain;
@@ -7,8 +9,14 @@ import miragefairy2019.mod.api.fairycrystal.RightClickDropFixed;
 import miragefairy2019.mod.api.fairycrystal.RightClickDropInventory;
 import miragefairy2019.mod.lib.EventRegistryMod;
 import miragefairy2019.mod.modules.fairy.ModuleFairy;
+import mirrg.boron.util.struct.ImmutableArray;
+import mirrg.boron.util.suppliterator.ISuppliterator;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreDictionary;
@@ -53,18 +61,18 @@ public class ModuleFairyCrystal
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.thunder[0].createItemStack(), 0.0045));
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.sun[0].createItemStack(), 0.000081));
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.moon[0].createItemStack(), 0.000081));
-
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.star[0].createItemStack(), 0.00027));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.stone[0].createItemStack(), 0.5));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.dirt[0].createItemStack(), 0.5));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.sand[0].createItemStack(), 0.5));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.gravel[0].createItemStack(), 0.15));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.iron[0].createItemStack(), 0.06));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.gold[0].createItemStack(), 0.018));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.diamond[0].createItemStack(), 0.0054));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.emerald[0].createItemStack(), 0.0054));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.redstone[0].createItemStack(), 0.018, new OreIngredient("dustRedstone")));
-				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.lapislazuli[0].createItemStack(), 0.0054, new OreIngredient("gemLapis")));
+
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.stone[0].createItemStack(), 0.5, block(Blocks.STONE)));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.dirt[0].createItemStack(), 0.5, block(Blocks.STONE)));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.sand[0].createItemStack(), 0.5, block(Blocks.STONE)));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.gravel[0].createItemStack(), 0.15, block(Blocks.STONE)));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.iron[0].createItemStack(), 0.06, ore("ingotIron", "blockIron")));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.gold[0].createItemStack(), 0.018, ore("ingotGold", "blockGold")));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.diamond[0].createItemStack(), 0.0054, ore("gemDiamond", "blockDiamond")));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.emerald[0].createItemStack(), 0.0054, ore("gemEmerald", "blockEmerald")));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.redstone[0].createItemStack(), 0.018, ore("dustRedstone", "blockRedstone")));
+				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropInventory(ModuleFairy.FairyTypes.lapislazuli[0].createItemStack(), 0.0054, ore("gemLapis", "blockLapis")));
 
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.enderman[0].createItemStack(), 0.0027));
 				ApiFairyCrystal.dropsFairyCrystal.add(new RightClickDropFixed(ModuleFairy.FairyTypes.spider[0].createItemStack(), 0.03));
@@ -93,6 +101,28 @@ public class ModuleFairyCrystal
 			}
 
 		});
+	}
+
+	private static Predicate<ItemStack> a(ISuppliterator<Predicate<ItemStack>> sPredicates)
+	{
+		ImmutableArray<Predicate<ItemStack>> predicates = sPredicates
+			.toImmutableArray();
+		return itemStack -> {
+			for (Predicate<ItemStack> predicate : predicates) {
+				if (predicate.test(itemStack)) return true;
+			}
+			return false;
+		};
+	}
+
+	private static Predicate<ItemStack> block(Block... blocks)
+	{
+		return a(ISuppliterator.ofObjArray(blocks).map(block -> Ingredient.fromItems(Item.getItemFromBlock(block))));
+	}
+
+	private static Predicate<ItemStack> ore(String... ores)
+	{
+		return a(ISuppliterator.ofObjArray(ores).map(ore -> new OreIngredient(ore)));
 	}
 
 }
