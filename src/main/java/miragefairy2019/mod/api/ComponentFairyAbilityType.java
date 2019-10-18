a3dc90b94dd2a341c8aff4edf2ab013ea95daedb
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-10-18 22:16:29 +0900

    ComponentFairyAbilityType: fix

--- src/main/java/miragefairy2019/mod/api/ComponentFairyAbilityType.java
+++ src/main/java/miragefairy2019/mod/api/ComponentFairyAbilityType.java
@@ -2,7 +2,6 @@ package miragefairy2019.mod.api;
 
 import miragefairy2019.mod.ModMirageFairy2019;
 import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
-import miragefairy2019.mod.lib.Utils;
 import miragefairy2019.mod.lib.UtilsMinecraft;
 import miragefairy2019.mod.lib.component.Component;
 import net.minecraft.util.ResourceLocation;
@@ -14,7 +13,7 @@ public class ComponentFairyAbilityType extends Component
 
 	public ComponentFairyAbilityType(EnumAbilityType abilityType)
 	{
-		super(new ResourceLocation(ModMirageFairy2019.MODID, "cuticle" + Utils.toUpperCaseHead(abilityType.getName())));
+		super(new ResourceLocation(ModMirageFairy2019.MODID, "cuticle_" + abilityType.getName()));
 		this.abilityType = abilityType;
 	}
 
