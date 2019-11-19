c335c9610595ae15cb30fdb2047ad9c29ed6ec66
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-19 21:54:26 +0900

    ItemCraftingFairyWand{2,3}: デバッガ削除

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/items/ItemCraftingFairyWand2.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/items/ItemCraftingFairyWand2.java
@@ -1,24 +1,13 @@
 package miragefairy2019.mod.modules.fairyweapon.items;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
-import miragefairy2019.mod.api.ApiMain;
 import miragefairy2019.mod.api.Components;
 import miragefairy2019.mod.lib.component.Composite;
 import miragefairy2019.mod.modules.fairyweapon.ItemFairyCraftingToolBase;
-import miragefairy2019.mod.modules.ore.BlockOreSeed;
-import miragefairy2019.mod.modules.ore.BlockOreSeed.EnumVariant;
-import mirrg.boron.util.suppliterator.ISuppliterator;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -44,30 +33,6 @@ public class ItemCraftingFairyWand2 extends ItemFairyCraftingToolBase
 
 		super.addInformation(itemStack, world, tooltip, flag);
 
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-	{
-
-		if (ApiMain.side.isClient()) {
-
-			// 鉱石生成確率表示
-			List<String> lines = new ArrayList<>();
-			lines.add("===== Ore List =====");
-			for (EnumVariant variant : EnumVariant.values()) {
-				lines.add("----- " + variant.name());
-				BlockOreSeed.getList(world, player.getPosition(), variant).stream()
-					.forEach(t -> lines.add(String.format("%.2f", t.weight) + ": " + t.item.get().getBlock().getItem(world, pos, t.item.get()).getDisplayName()));
-			}
-			lines.add("====================");
-			player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
-				.join("\n")), false);
-
-		}
-
-		return EnumActionResult.SUCCESS;
 	}
 
 }
