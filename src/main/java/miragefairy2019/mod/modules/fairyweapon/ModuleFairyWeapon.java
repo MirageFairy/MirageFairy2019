35b427fc5c1d0291028207bea3f3d9cd819fd4ac
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-21 23:42:07 +0900

    add: miragium sword

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/ModuleFairyWeapon.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/ModuleFairyWeapon.java
@@ -14,6 +14,7 @@ import miragefairy2019.mod.modules.fairyweapon.items.ItemLightMagicWand;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemMeltingFairyWand;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemMeltingFairyWand2;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemMiragiumAxe;
+import miragefairy2019.mod.modules.fairyweapon.items.ItemMiragiumSword;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemOcarinaBase;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemSummoningFairyWand;
 import miragefairy2019.mod.modules.fairyweapon.items.ItemTemptationOcarina;
@@ -47,6 +48,7 @@ public class ModuleFairyWeapon
 	public static ItemBreakingFairyWand2 itemBreakingFairyWand2;
 	public static ItemTemptationOcarina itemTemptationOcarina;
 	public static ItemOcarinaBase itemOcarinaBase;
+	public static ItemMiragiumSword itemMiragiumSword;
 
 	public static void init(EventRegistryMod erMod)
 	{
@@ -65,6 +67,7 @@ public class ModuleFairyWeapon
 			itemBreakingFairyWand2 = item(new ItemBreakingFairyWand2(), "breaking_fairy_wand_2", "breakingFairyWand2"); // 破壊のステッキ2
 			itemTemptationOcarina = item(new ItemTemptationOcarina(), "temptation_ocarina", "temptationOcarina"); // 魅惑のオカリナ
 			itemOcarinaBase = item(new ItemOcarinaBase(), "ocarina_base", "ocarinaBase"); // オカリナベース
+			itemMiragiumSword = item(new ItemMiragiumSword(), "miragium_sword", "miragiumSword"); // ミラジウムの剣
 		});
 		erMod.createItemStack.register(ic -> {
 			OreDictionary.registerOre("mirageFairy2019CraftingToolFairyWandCrafting", new ItemStack(itemCraftingFairyWand, 1, OreDictionary.WILDCARD_VALUE));
---
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/items/ItemMiragiumSword.java
@@ -0,0 +1,39 @@
+package miragefairy2019.mod.modules.fairyweapon.items;
+
+import java.util.List;
+
+import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
+import miragefairy2019.mod.api.Components;
+import miragefairy2019.mod.modules.fairyweapon.ItemFairyWeaponBase;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemMiragiumSword extends ItemFairyWeaponBase
+{
+
+	public ItemMiragiumSword()
+	{
+		composite = composite
+			.add(Components.MIRAGIUM, 2)
+			.add(Components.WOOD, 0.5)
+			.add(Components.fairyAbilityType(EnumAbilityType.attack))
+			.add(Components.fairyAbilityType(EnumAbilityType.slash));
+		setMaxDamage(64 - 1);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, World world, List<String> tooltip, ITooltipFlag flag)
+	{
+
+		// ポエム
+		tooltip.add("切断の力を何に使うかはあなた次第");
+
+		super.addInformation(itemStack, world, tooltip, flag);
+
+	}
+
+}
