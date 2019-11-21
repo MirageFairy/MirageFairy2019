175ecd75bbc26d561141870d55febfd1c796202e
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-21 23:16:16 +0900

    ModuleFairyWeapon: refactor

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/ModuleFairyWeapon.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/ModuleFairyWeapon.java
@@ -51,175 +51,20 @@ public class ModuleFairyWeapon
 	public static void init(EventRegistryMod erMod)
 	{
 		erMod.registerItem.register(b -> {
-
-			// 技巧のステッキ
-			{
-				ItemCraftingFairyWand item = new ItemCraftingFairyWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "crafting_fairy_wand");
-				item.setUnlocalizedName("craftingFairyWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemCraftingFairyWand = item;
-			}
-
-			// 技巧のステッキ2
-			{
-				ItemCraftingFairyWand2 item = new ItemCraftingFairyWand2();
-				item.setRegistryName(ModMirageFairy2019.MODID, "crafting_fairy_wand_2");
-				item.setUnlocalizedName("craftingFairyWand2");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemCraftingFairyWand2 = item;
-			}
-
-			// 技巧のステッキ3
-			{
-				ItemCraftingFairyWand3 item = new ItemCraftingFairyWand3();
-				item.setRegistryName(ModMirageFairy2019.MODID, "crafting_fairy_wand_3");
-				item.setUnlocalizedName("craftingFairyWand3");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemCraftingFairyWand3 = item;
-			}
-
-			// 紅蓮のステッキ
-			{
-				ItemMeltingFairyWand item = new ItemMeltingFairyWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "melting_fairy_wand");
-				item.setUnlocalizedName("meltingFairyWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemMeltingFairyWand = item;
-			}
-
-			// 紅蓮のステッキ
-			{
-				ItemMeltingFairyWand2 item = new ItemMeltingFairyWand2();
-				item.setRegistryName(ModMirageFairy2019.MODID, "melting_fairy_wand_2");
-				item.setUnlocalizedName("meltingFairyWand2");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemMeltingFairyWand2 = item;
-			}
-
-			// 妖精剣
-			{
-				ItemFairySword item = new ItemFairySword();
-				item.setRegistryName(ModMirageFairy2019.MODID, "fairy_sword");
-				item.setUnlocalizedName("fairySword");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemFairySword = item;
-			}
-
-			// ミラジウムの斧
-			{
-				ItemMiragiumAxe item = new ItemMiragiumAxe();
-				item.setRegistryName(ModMirageFairy2019.MODID, "miragium_axe");
-				item.setUnlocalizedName("miragiumAxe");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemMiragiumAxe = item;
-			}
-
-			// 光のロッド
-			{
-				ItemLightMagicWand item = new ItemLightMagicWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "light_magic_wand");
-				item.setUnlocalizedName("lightMagicWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemLightMagicWand = item;
-			}
-
-			// 召喚のステッキ
-			{
-				ItemSummoningFairyWand item = new ItemSummoningFairyWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "summoning_fairy_wand");
-				item.setUnlocalizedName("summoningFairyWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemSummoningFairyWand = item;
-			}
-
-			// 収集のロッド
-			{
-				ItemCollectingMagicWand item = new ItemCollectingMagicWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "collecting_magic_wand");
-				item.setUnlocalizedName("collectingMagicWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemCollectingMagicWand = item;
-			}
-
-			// 破壊のステッキ
-			{
-				ItemBreakingFairyWand item = new ItemBreakingFairyWand();
-				item.setRegistryName(ModMirageFairy2019.MODID, "breaking_fairy_wand");
-				item.setUnlocalizedName("breakingFairyWand");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemBreakingFairyWand = item;
-			}
-
-			// 破壊のステッキ2
-			{
-				ItemBreakingFairyWand2 item = new ItemBreakingFairyWand2();
-				item.setRegistryName(ModMirageFairy2019.MODID, "breaking_fairy_wand_2");
-				item.setUnlocalizedName("breakingFairyWand2");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemBreakingFairyWand2 = item;
-			}
-
-			// 魅惑のオカリナ
-			{
-				ItemTemptationOcarina item = new ItemTemptationOcarina();
-				item.setRegistryName(ModMirageFairy2019.MODID, "temptation_ocarina");
-				item.setUnlocalizedName("temptationOcarina");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemTemptationOcarina = item;
-			}
-
-			// オカリナベース
-			{
-				ItemOcarinaBase item = new ItemOcarinaBase();
-				item.setRegistryName(ModMirageFairy2019.MODID, "ocarina_base");
-				item.setUnlocalizedName("ocarinaBase");
-				item.setCreativeTab(ApiMain.creativeTab);
-				ForgeRegistries.ITEMS.register(item);
-				hookBakedModelWrapper(item);
-				if (ApiMain.side.isClient()) ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				itemOcarinaBase = item;
-			}
-
+			itemCraftingFairyWand = item(new ItemCraftingFairyWand(), "crafting_fairy_wand", "craftingFairyWand"); // 技巧のステッキ
+			itemCraftingFairyWand2 = item(new ItemCraftingFairyWand2(), "crafting_fairy_wand_2", "craftingFairyWand2"); // 技巧のステッキ2
+			itemCraftingFairyWand3 = item(new ItemCraftingFairyWand3(), "crafting_fairy_wand_3", "craftingFairyWand3"); // 技巧のステッキ3
+			itemMeltingFairyWand = item(new ItemMeltingFairyWand(), "melting_fairy_wand", "meltingFairyWand"); // 紅蓮のステッキ
+			itemMeltingFairyWand2 = item(new ItemMeltingFairyWand2(), "melting_fairy_wand_2", "meltingFairyWand2"); // 紅蓮のステッキ
+			itemFairySword = item(new ItemFairySword(), "fairy_sword", "fairySword"); // 妖精剣
+			itemMiragiumAxe = item(new ItemMiragiumAxe(), "miragium_axe", "miragiumAxe"); // ミラジウムの斧
+			itemLightMagicWand = item(new ItemLightMagicWand(), "light_magic_wand", "lightMagicWand"); // 光のロッド
+			itemSummoningFairyWand = item(new ItemSummoningFairyWand(), "summoning_fairy_wand", "summoningFairyWand"); // 召喚のステッキ
+			itemCollectingMagicWand = item(new ItemCollectingMagicWand(), "collecting_magic_wand", "collectingMagicWand"); // 収集のロッド
+			itemBreakingFairyWand = item(new ItemBreakingFairyWand(), "breaking_fairy_wand", "breakingFairyWand"); // 破壊のステッキ
+			itemBreakingFairyWand2 = item(new ItemBreakingFairyWand2(), "breaking_fairy_wand_2", "breakingFairyWand2"); // 破壊のステッキ2
+			itemTemptationOcarina = item(new ItemTemptationOcarina(), "temptation_ocarina", "temptationOcarina"); // 魅惑のオカリナ
+			itemOcarinaBase = item(new ItemOcarinaBase(), "ocarina_base", "ocarinaBase"); // オカリナベース
 		});
 		erMod.createItemStack.register(ic -> {
 			OreDictionary.registerOre("mirageFairy2019CraftingToolFairyWandCrafting", new ItemStack(itemCraftingFairyWand, 1, OreDictionary.WILDCARD_VALUE));
@@ -243,9 +88,15 @@ public class ModuleFairyWeapon
 		});
 	}
 
-	private static void hookBakedModelWrapper(Item item)
+	private static <I extends Item> I item(I item, String registryName, String unlocalizedName)
 	{
+		item.setRegistryName(ModMirageFairy2019.MODID, registryName);
+		item.setUnlocalizedName(unlocalizedName);
+		item.setCreativeTab(ApiMain.creativeTab);
+		ForgeRegistries.ITEMS.register(item);
 		if (ApiMain.side.isClient()) {
+
+			// 搭乗妖精の描画
 			MinecraftForge.EVENT_BUS.register(new Object() {
 				@SubscribeEvent
 				public void accept(ModelBakeEvent event)
@@ -254,7 +105,11 @@ public class ModuleFairyWeapon
 					event.getModelRegistry().putObject(new ModelResourceLocation(item.getRegistryName(), null), new BakedModelBuiltinWrapper(bakedModel));
 				}
 			});
+
+			ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
+
 		}
+		return item;
 	}
 
 }
