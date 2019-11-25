14895ffdfc1f8b9757b2d3efc0a1898691302e5a
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-25 22:05:16 +0900

    add: MainColorMakerFairy

---
+++ src/main/java/miragefairy2019/colormaker/MainColorMakerFairy.java
@@ -0,0 +1,26 @@
+package miragefairy2019.colormaker;
+
+import java.awt.Color;
+
+import miragefairy2019.colormaker.core.ImageLoader;
+import miragefairy2019.mod.ModMirageFairy2019;
+import mirrg.boron.util.struct.ImmutableArray;
+import mirrg.boron.util.struct.Tuple3;
+
+public class MainColorMakerFairy
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		new WindowColorMakerSingle(
+			new ImageLoader(ModMirageFairy2019.class.getClassLoader(), ModMirageFairy2019.MODID),
+			ImmutableArray.of(
+				Tuple3.of("skin", "fairy_layer0", new Color(0xFFFFFF)),
+				Tuple3.of("dress", "fairy_layer1", new Color(0xFFFFFF)),
+				Tuple3.of("dark", "fairy_layer2", new Color(0xFFFFFF)),
+				Tuple3.of("bright", "fairy_layer3", new Color(0xFFFFFF)),
+				Tuple3.of("hair", "fairy_layer4", new Color(0xFFFFFF))),
+			ImmutableArray.of("skin", "bright", "dark", "hair")).setVisible(true);
+	}
+
+}
