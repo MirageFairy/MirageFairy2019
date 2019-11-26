a87f6ece0bdb8a8efe58f8962dde4164d2910f4e
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-26 23:33:39 +0900

    ItemBellBase: レシピ変更

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemBellBase.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemBellBase.java
@@ -9,9 +9,9 @@ public class ItemBellBase extends ItemFairyWeaponBase
 	public ItemBellBase()
 	{
 		addComponent(Components.MIRAGIUM, 0.5);
-		addComponent(Components.MIRAGIUM, 1);
+		addComponent(Components.MIRAGIUM, 3);
 		addComponent(Components.fairyAbilityType(EnumAbilityType.submission));
-		setMaxDamage(128 - 1);
+		setMaxDamage(64 - 1);
 		setDescription("妖精の力を解放せよ");
 	}
 
