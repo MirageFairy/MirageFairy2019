a30214a2476fab6c37d3ea405e2b4ab24872c892
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-28 21:19:49 +0900

    ステッキのデバッグツールはMiniaを搭乗させているときにのみ使用可能

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandBreaking.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandBreaking.java
@@ -7,10 +7,12 @@ import java.util.Map;
 
 import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
 import miragefairy2019.mod.api.Components;
+import miragefairy2019.mod.modules.fairy.ModuleFairy;
 import miragefairy2019.mod.modules.ore.ModuleOre;
 import mirrg.boron.util.suppliterator.ISuppliterator;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -33,36 +35,48 @@ public class ItemFairyWandBreaking extends ItemFairyWeaponCraftingToolBase
 	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
 
+		// 指定の妖精を持っていない場合、抜ける
+		a:
+		{
+			ItemStack itemStackFairy = getCombinedFairy(getCombinedFairy(player.getHeldItem(hand)));
+			if (getFairy(itemStackFairy).isPresent()) {
+				if (getFairy(itemStackFairy).get().modid.equals(ModuleFairy.FairyTypes.mina[0].type.modid)) {
+					if (getFairy(itemStackFairy).get().name.equals(ModuleFairy.FairyTypes.mina[0].type.name)) {
+						break a;
+					}
+				}
+			}
+			return EnumActionResult.PASS;
+		}
+
 		if (!world.isRemote) {
-			if (player.isSneaking()) {
 
-				Map<IBlockState, Integer> map = new HashMap<>();
-				int x = Math.floorDiv(pos.getX(), 16);
-				int z = Math.floorDiv(pos.getZ(), 16);
+			Map<IBlockState, Integer> map = new HashMap<>();
+			int x = Math.floorDiv(pos.getX(), 16);
+			int z = Math.floorDiv(pos.getZ(), 16);
 
-				for (int xi = 0; xi < 16; xi++) {
-					for (int zi = 0; zi < 16; zi++) {
-						for (int yi = 0; yi < 256; yi++) {
-							IBlockState blockState = world.getBlockState(new BlockPos(x + xi, yi, z + zi));
-							if (blockState.getBlock().equals(ModuleOre.blockOreSeed)) {
-								map.put(blockState, map.getOrDefault(blockState, 0) + 1);
-							}
+			for (int xi = 0; xi < 16; xi++) {
+				for (int zi = 0; zi < 16; zi++) {
+					for (int yi = 0; yi < 256; yi++) {
+						IBlockState blockState = world.getBlockState(new BlockPos(x + xi, yi, z + zi));
+						if (blockState.getBlock().equals(ModuleOre.blockOreSeed)) {
+							map.put(blockState, map.getOrDefault(blockState, 0) + 1);
 						}
 					}
 				}
-
-				// 鉱石生成確率表示
-				List<String> lines = new ArrayList<>();
-				lines.add("===== Ore Seed =====");
-				lines.add("------");
-				map.entrySet().stream().forEach(e -> {
-					lines.add(e.getKey() + ": " + e.getValue());
-				});
-				lines.add("====================");
-				player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
-					.join("\n")), false);
-
 			}
+
+			// 鉱石生成確率表示
+			List<String> lines = new ArrayList<>();
+			lines.add("===== Ore Seed =====");
+			lines.add("------");
+			map.entrySet().stream().forEach(e -> {
+				lines.add(e.getKey() + ": " + e.getValue());
+			});
+			lines.add("====================");
+			player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
+				.join("\n")), false);
+
 		}
 
 		return EnumActionResult.SUCCESS;
--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandCrafting.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandCrafting.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
 import miragefairy2019.mod.api.Components;
+import miragefairy2019.mod.modules.fairy.ModuleFairy;
 import miragefairy2019.mod.modules.ore.BlockOreSeed;
 import miragefairy2019.mod.modules.ore.BlockOreSeed.EnumVariant;
 import mirrg.boron.util.suppliterator.ISuppliterator;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -32,22 +34,34 @@ public class ItemFairyWandCrafting extends ItemFairyWeaponCraftingToolBase
 	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
 
-		if (!world.isRemote) {
-			if (player.isSneaking()) {
-
-				// 鉱石生成確率表示
-				List<String> lines = new ArrayList<>();
-				lines.add("===== Ore List =====");
-				for (EnumVariant variant : EnumVariant.values()) {
-					lines.add("----- " + variant.name());
-					BlockOreSeed.getList(world, player.getPosition(), variant).stream()
-						.forEach(t -> lines.add(String.format("%.2f", t.weight) + ": " + t.item.get().getBlock().getItem(world, pos, t.item.get()).getDisplayName()));
+		// 指定の妖精を持っていない場合、抜ける
+		a:
+		{
+			ItemStack itemStackFairy = getCombinedFairy(getCombinedFairy(player.getHeldItem(hand)));
+			if (getFairy(itemStackFairy).isPresent()) {
+				if (getFairy(itemStackFairy).get().modid.equals(ModuleFairy.FairyTypes.mina[0].type.modid)) {
+					if (getFairy(itemStackFairy).get().name.equals(ModuleFairy.FairyTypes.mina[0].type.name)) {
+						break a;
+					}
 				}
-				lines.add("====================");
-				player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
-					.join("\n")), false);
-
 			}
+			return EnumActionResult.PASS;
+		}
+
+		if (!world.isRemote) {
+
+			// 鉱石生成確率表示
+			List<String> lines = new ArrayList<>();
+			lines.add("===== Ore List =====");
+			for (EnumVariant variant : EnumVariant.values()) {
+				lines.add("----- " + variant.name());
+				BlockOreSeed.getList(world, player.getPosition(), variant).stream()
+					.forEach(t -> lines.add(String.format("%.2f", t.weight) + ": " + t.item.get().getBlock().getItem(world, pos, t.item.get()).getDisplayName()));
+			}
+			lines.add("====================");
+			player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
+				.join("\n")), false);
+
 		}
 
 		return EnumActionResult.SUCCESS;
