af14b991ec1f719dba4a89d3f1bd8594618a018d
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-11-28 21:20:09 +0900

    ItemFairyWandMelting: ミラ花の成長率表示

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandMelting.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWandMelting.java
@@ -1,7 +1,21 @@
 package miragefairy2019.mod.modules.fairyweapon.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import miragefairy2019.mod.api.ApiFairy.EnumAbilityType;
 import miragefairy2019.mod.api.Components;
+import miragefairy2019.mod.modules.fairy.ModuleFairy;
+import miragefairy2019.mod.modules.mirageflower.ModuleMirageFlower;
+import mirrg.boron.util.suppliterator.ISuppliterator;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 
 public class ItemFairyWandMelting extends ItemFairyWeaponCraftingToolBase
 {
@@ -12,6 +26,39 @@ public class ItemFairyWandMelting extends ItemFairyWeaponCraftingToolBase
 		addComponent(Components.fairyAbilityType(EnumAbilityType.flame));
 		setMaxDamage(32 - 1);
 		setDescription("金属を溶かすほどの情熱");
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+	{
+
+		// 指定の妖精を持っていない場合、抜ける
+		a:
+		{
+			ItemStack itemStackFairy = getCombinedFairy(getCombinedFairy(player.getHeldItem(hand)));
+			if (getFairy(itemStackFairy).isPresent()) {
+				if (getFairy(itemStackFairy).get().modid.equals(ModuleFairy.FairyTypes.mina[0].type.modid)) {
+					if (getFairy(itemStackFairy).get().name.equals(ModuleFairy.FairyTypes.mina[0].type.name)) {
+						break a;
+					}
+				}
+			}
+			return EnumActionResult.PASS;
+		}
+
+		if (!world.isRemote) {
+
+			// 鉱石生成確率表示
+			List<String> lines = new ArrayList<>();
+			lines.add("===== Mirage Flower Grow Rate =====");
+			lines.add(String.format("%.2f%%", ModuleMirageFlower.blockMirageFlower.getGrowRate(world, pos) * 100));
+			lines.add("====================");
+			player.sendStatusMessage(new TextComponentString(ISuppliterator.ofIterable(lines)
+				.join("\n")), false);
+
+		}
+
+		return EnumActionResult.SUCCESS;
 	}
 
 }
