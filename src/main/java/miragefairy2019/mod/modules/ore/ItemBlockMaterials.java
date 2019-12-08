678fa2445b012dc72474ee43fa9628b7f243986c
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-12-08 17:02:05 +0900

    _

--- src/main/java/miragefairy2019/mod/modules/ore/ItemBlockMaterials.java
+++ src/main/java/miragefairy2019/mod/modules/ore/ItemBlockMaterials.java
@@ -23,9 +23,9 @@ public class ItemBlockMaterials<V extends IBlockVariant> extends ItemBlock
 	}
 
 	@Override
-	public String getUnlocalizedName(ItemStack stack)
+	public String getUnlocalizedName(ItemStack itemStack)
 	{
-		return "tile." + block2.variantList.byMetadata(stack.getMetadata()).getUnlocalizedName();
+		return "tile." + block2.variantList.byMetadata(itemStack.getMetadata()).getUnlocalizedName();
 	}
 
 }
