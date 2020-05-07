a2a2c1d4dc7c7b27e8d131e0e16c4bcf146fdd94
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-05-08 08:46:19 +0900

    refactor: add: component(saltpeter)

--- src/main/java/miragefairy2019/mod/api/composite/Components.java
+++ src/main/java/miragefairy2019/mod/api/composite/Components.java
@@ -20,5 +20,6 @@ public class Components
 	public static final Supplier<IComponent> emerald = () -> miragefairy2019.mod.modules.composite.Component.emerald;
 	public static final Supplier<IComponent> pyrope = () -> miragefairy2019.mod.modules.composite.Component.pyrope;
 	public static final Supplier<IComponent> smithsonite = () -> miragefairy2019.mod.modules.composite.Component.smithsonite;
+	public static final Supplier<IComponent> saltpeter = () -> miragefairy2019.mod.modules.composite.Component.saltpeter;
 
 }
