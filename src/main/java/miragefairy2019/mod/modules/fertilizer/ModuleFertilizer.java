fdc55098e257d647dedcc5bdc4c3d10248cabf35
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-05-10 07:54:56 +0900

    refactor: fix: ModuleFertilizer

--- src/main/java/miragefairy2019/mod/modules/fertilizer/ModuleFertilizer.java
+++ src/main/java/miragefairy2019/mod/modules/fertilizer/ModuleFertilizer.java
@@ -5,7 +5,6 @@ import static miragefairy2019.mod.lib.Configurator.*;
 import miragefairy2019.mod.ModMirageFairy2019;
 import miragefairy2019.mod.api.main.ApiMain;
 import miragefairy2019.mod.lib.EventRegistryMod;
-import miragefairy2019.mod.lib.Monad;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
@@ -19,10 +18,9 @@ public class ModuleFertilizer
 		// 肥料
 		item(erMod, ItemFertilizer::new, new ResourceLocation(ModMirageFairy2019.MODID, "fertilizer"), "fertilizer")
 			.bind(setCreativeTab(() -> ApiMain.creativeTab()))
-			.bind(c -> {
-				if (ApiMain.side().isClient()) ModelLoader.setCustomModelResourceLocation(c.get(), 0, new ModelResourceLocation(c.get().getRegistryName(), null));
-				return Monad.of(c);
-			});
+			.bind(onRegisterItem(i -> {
+				if (ApiMain.side().isClient()) ModelLoader.setCustomModelResourceLocation(i, 0, new ModelResourceLocation(i.getRegistryName(), null));
+			}));
 
 	}
 
