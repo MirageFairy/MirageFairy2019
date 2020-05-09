85aae7187021bced0b17e91ac414e66160f7aea0
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-05-09 09:18:18 +0900

    refactor: ModuleFertilizer

--- src/main/java/miragefairy2019/mod/modules/fertilizer/ModuleFertilizer.java
+++ src/main/java/miragefairy2019/mod/modules/fertilizer/ModuleFertilizer.java
@@ -1,35 +1,29 @@
 package miragefairy2019.mod.modules.fertilizer;
 
+import static miragefairy2019.mod.lib.Configurator.*;
+
 import miragefairy2019.mod.ModMirageFairy2019;
 import miragefairy2019.mod.api.main.ApiMain;
 import miragefairy2019.mod.lib.EventRegistryMod;
+import miragefairy2019.mod.lib.Monad;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModuleFertilizer
 {
 
-	static ItemFertilizer itemFertilizer;
-
 	public static void init(EventRegistryMod erMod)
 	{
-		erMod.registerItem.register(b -> {
 
-			// 肥料のアイテム登録
-			{
-				ItemFertilizer item = new ItemFertilizer();
-				item.setRegistryName(ModMirageFairy2019.MODID, "fertilizer");
-				item.setUnlocalizedName("fertilizer");
-				item.setCreativeTab(ApiMain.creativeTab());
-				ForgeRegistries.ITEMS.register(item);
-				if (ApiMain.side().isClient()) {
-					ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), null));
-				}
-				itemFertilizer = item;
-			}
+		// 肥料
+		item(erMod, ItemFertilizer::new, new ResourceLocation(ModMirageFairy2019.MODID, "fertilizer"), "fertilizer")
+			.bind(setCreativeTab(() -> ApiMain.creativeTab()))
+			.bind(c -> {
+				if (ApiMain.side().isClient()) ModelLoader.setCustomModelResourceLocation(c.get(), 0, new ModelResourceLocation(c.get().getRegistryName(), null));
+				return Monad.of(c);
+			});
 
-		});
 	}
 
 }
