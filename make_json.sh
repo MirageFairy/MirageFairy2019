3543d136f98b3eb6a69ee69662a37abbbed7eb66
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-05-10 09:35:36 +0900

    refactor: update: make_json.sh

--- make_json.sh
+++ make_json.sh
@@ -1,7 +1,10 @@
-mkdir -p fluorite-7
-(
-	cd fluorite-7
-	curl https://raw.githubusercontent.com/MirrgieRiana/fluorite-7/master/install.bash | bash
-)
+if [ ! -d fluorite-7 ]
+then
+	mkdir -p fluorite-7
+	(
+		cd fluorite-7
+		curl https://raw.githubusercontent.com/MirrgieRiana/fluorite-7/master/install.bash | bash
+	)
+fi
 
 bash fluorite-7/fl7 "$(cat make_json.fl7)"
