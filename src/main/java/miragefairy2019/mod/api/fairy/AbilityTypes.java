b9bf0817ad56964407a0a9bc4c1ebaeb2b21bf3f
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-06-10 03:06:59 +0900

    format

--- src/main/java/miragefairy2019/mod/api/fairy/AbilityTypes.java
+++ src/main/java/miragefairy2019/mod/api/fairy/AbilityTypes.java
@@ -2,31 +2,29 @@ package miragefairy2019.mod.api.fairy;
 
 import java.util.function.Supplier;
 
-import miragefairy2019.mod.modules.fairy.EnumAbilityType;
-
 public class AbilityTypes
 {
 
-	public static final Supplier<IAbilityType> attack = () -> EnumAbilityType.attack;
-	public static final Supplier<IAbilityType> craft = () -> EnumAbilityType.craft;
-	public static final Supplier<IAbilityType> fell = () -> EnumAbilityType.fell;
-	public static final Supplier<IAbilityType> light = () -> EnumAbilityType.light;
-	public static final Supplier<IAbilityType> flame = () -> EnumAbilityType.flame;
-	public static final Supplier<IAbilityType> water = () -> EnumAbilityType.water;
-	public static final Supplier<IAbilityType> crystal = () -> EnumAbilityType.crystal;
-	public static final Supplier<IAbilityType> art = () -> EnumAbilityType.art;
-	public static final Supplier<IAbilityType> store = () -> EnumAbilityType.store;
-	public static final Supplier<IAbilityType> warp = () -> EnumAbilityType.warp;
-	public static final Supplier<IAbilityType> shoot = () -> EnumAbilityType.shoot;
-	public static final Supplier<IAbilityType> breaking = () -> EnumAbilityType.breaking;
-	public static final Supplier<IAbilityType> chemical = () -> EnumAbilityType.chemical;
-	public static final Supplier<IAbilityType> slash = () -> EnumAbilityType.slash;
-	public static final Supplier<IAbilityType> food = () -> EnumAbilityType.food;
-	public static final Supplier<IAbilityType> knowledge = () -> EnumAbilityType.knowledge;
-	public static final Supplier<IAbilityType> energy = () -> EnumAbilityType.energy;
-	public static final Supplier<IAbilityType> submission = () -> EnumAbilityType.submission;
-	public static final Supplier<IAbilityType> christmas = () -> EnumAbilityType.christmas;
-	public static final Supplier<IAbilityType> freeze = () -> EnumAbilityType.freeze;
-	public static final Supplier<IAbilityType> thunder = () -> EnumAbilityType.thunder;
+	public static final Supplier<IAbilityType> attack = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.attack;
+	public static final Supplier<IAbilityType> craft = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.craft;
+	public static final Supplier<IAbilityType> fell = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.fell;
+	public static final Supplier<IAbilityType> light = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.light;
+	public static final Supplier<IAbilityType> flame = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.flame;
+	public static final Supplier<IAbilityType> water = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.water;
+	public static final Supplier<IAbilityType> crystal = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.crystal;
+	public static final Supplier<IAbilityType> art = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.art;
+	public static final Supplier<IAbilityType> store = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.store;
+	public static final Supplier<IAbilityType> warp = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.warp;
+	public static final Supplier<IAbilityType> shoot = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.shoot;
+	public static final Supplier<IAbilityType> breaking = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.breaking;
+	public static final Supplier<IAbilityType> chemical = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.chemical;
+	public static final Supplier<IAbilityType> slash = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.slash;
+	public static final Supplier<IAbilityType> food = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.food;
+	public static final Supplier<IAbilityType> knowledge = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.knowledge;
+	public static final Supplier<IAbilityType> energy = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.energy;
+	public static final Supplier<IAbilityType> submission = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.submission;
+	public static final Supplier<IAbilityType> christmas = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.christmas;
+	public static final Supplier<IAbilityType> freeze = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.freeze;
+	public static final Supplier<IAbilityType> thunder = () -> miragefairy2019.mod.modules.fairy.EnumAbilityType.thunder;
 
 }
