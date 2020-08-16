f743d8cd4ac0c9d4b64b9ae9a9476aa3a84233b7
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-08-16 19:05:16 +0900

    refactor: BlockOreSeed

--- src/main/java/miragefairy2019/mod/modules/oreseed/BlockOreSeed.java
+++ src/main/java/miragefairy2019/mod/modules/oreseed/BlockOreSeed.java
@@ -111,71 +111,39 @@ public class BlockOreSeed extends Block
 	@Override
 	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand)
 	{
-		if (!world.isRemote) {
-			update(world, pos, state);
-		}
+		if (!world.isRemote) update(world, pos);
 	}
 
 	@Override
 	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos)
 	{
-		if (!worldIn.isRemote) {
-			update(worldIn, pos, state);
-		}
+		if (!worldIn.isRemote) update(worldIn, pos);
 	}
 
 	@Override
 	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn)
 	{
 		super.onBlockClicked(worldIn, pos, playerIn);
-		if (!worldIn.isRemote) {
-			update(worldIn, pos, worldIn.getBlockState(pos));
-		}
+		if (!worldIn.isRemote) update(worldIn, pos);
 	}
 
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		if (!worldIn.isRemote) {
-			update(worldIn, pos, state);
-		}
+		if (!worldIn.isRemote) update(worldIn, pos);
 		return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
 	}
 
 	//
 
-	protected void update(World world, BlockPos pos, IBlockState state)
+	protected void update(World world, BlockPos pos)
 	{
-		if (canMutate(world, pos)) {
-			Random random = new Random(pos.getX() * 15946848L + pos.getY() * 29135678L + pos.getZ() * 65726816L);
-			IBlockState blockStateAfter = RegisterOreSeedDrop.drop(
-				getVariant(state).shape,
-				world,
-				pos,
-				random).orElseGet(() -> sBlockStateDefault.get());
-
-			Deque<BlockPos> poses = new ArrayDeque<>();
-			poses.addLast(pos);
-			int t = 4096;
-			while (!poses.isEmpty()) {
-				BlockPos pos2 = poses.removeFirst();
-
-				world.setBlockState(pos2, blockStateAfter, 2);
-				if (world.getBlockState(pos2.up()).equals(state)) poses.addLast(pos2.up());
-				if (world.getBlockState(pos2.down()).equals(state)) poses.addLast(pos2.down());
-				if (world.getBlockState(pos2.west()).equals(state)) poses.addLast(pos2.west());
-				if (world.getBlockState(pos2.east()).equals(state)) poses.addLast(pos2.east());
-				if (world.getBlockState(pos2.north()).equals(state)) poses.addLast(pos2.north());
-				if (world.getBlockState(pos2.south()).equals(state)) poses.addLast(pos2.south());
-
-				t--;
-				if (t <= 0) break;
-			}
-
+		if (shouldMutate(world, pos)) {
+			mutate(world, pos);
 		}
 	}
 
-	protected boolean canMutate(IBlockAccess world, BlockPos pos)
+	protected boolean shouldMutate(IBlockAccess world, BlockPos pos)
 	{
 		if (!world.getBlockState(pos.up()).isSideSolid(world, pos.up(), EnumFacing.DOWN)) return true;
 		if (!world.getBlockState(pos.down()).isSideSolid(world, pos.down(), EnumFacing.UP)) return true;
@@ -184,6 +152,35 @@ public class BlockOreSeed extends Block
 		if (!world.getBlockState(pos.north()).isSideSolid(world, pos.north(), EnumFacing.SOUTH)) return true;
 		if (!world.getBlockState(pos.south()).isSideSolid(world, pos.south(), EnumFacing.NORTH)) return true;
 		return false;
+	}
+
+	protected void mutate(World world, BlockPos pos)
+	{
+		IBlockState state = world.getBlockState(pos);
+		Random random = new Random(pos.getX() * 15946848L + pos.getY() * 29135678L + pos.getZ() * 65726816L);
+		IBlockState blockStateAfter = RegisterOreSeedDrop.drop(
+			getVariant(state).shape,
+			world,
+			pos,
+			random).orElseGet(() -> sBlockStateDefault.get());
+
+		Deque<BlockPos> poses = new ArrayDeque<>();
+		poses.addLast(pos);
+		int t = 4096;
+		while (!poses.isEmpty()) {
+			BlockPos pos2 = poses.removeFirst();
+
+			world.setBlockState(pos2, blockStateAfter, 2);
+			if (world.getBlockState(pos2.up()).equals(state)) poses.addLast(pos2.up());
+			if (world.getBlockState(pos2.down()).equals(state)) poses.addLast(pos2.down());
+			if (world.getBlockState(pos2.west()).equals(state)) poses.addLast(pos2.west());
+			if (world.getBlockState(pos2.east()).equals(state)) poses.addLast(pos2.east());
+			if (world.getBlockState(pos2.north()).equals(state)) poses.addLast(pos2.north());
+			if (world.getBlockState(pos2.south()).equals(state)) poses.addLast(pos2.south());
+
+			t--;
+			if (t <= 0) break;
+		}
 	}
 
 }
