90889ade7431a063b0668d046a94119c29833f88
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-08-17 21:10:45 +0900

    change: ItemFairyStick: add: poem

--- src/main/java/miragefairy2019/mod/modules/materialsfairy/ItemFairyStick.java
+++ src/main/java/miragefairy2019/mod/modules/materialsfairy/ItemFairyStick.java
@@ -1,11 +1,17 @@
 package miragefairy2019.mod.modules.materialsfairy;
 
+import java.util.List;
+
+import miragefairy2019.mod.lib.UtilsMinecraft;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -23,6 +29,24 @@ public class ItemFairyStick extends Item
 	public boolean isFull3D()
 	{
 		return true;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public final void addInformation(ItemStack itemStack, World world, List<String> tooltip, ITooltipFlag flag)
+	{
+
+		// ポエム
+		if (UtilsMinecraft.canTranslate(getUnlocalizedName() + ".poem")) {
+			String string = UtilsMinecraft.translateToLocal(getUnlocalizedName() + ".poem");
+			if (!string.isEmpty()) {
+				tooltip.add(string);
+			}
+		}
+
+		// 機能
+		tooltip.add(TextFormatting.RED + "Right Click: World Craft");
+
 	}
 
 	@Override
