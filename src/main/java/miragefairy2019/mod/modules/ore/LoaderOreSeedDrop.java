c9b7b8c0806208972cc290850a95b634cca35f12
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-08-28 00:44:36 +0900

    change: 鉱脈システム追加

--- src/main/java/miragefairy2019/mod/modules/ore/LoaderOreSeedDrop.java
+++ src/main/java/miragefairy2019/mod/modules/ore/LoaderOreSeedDrop.java
@@ -3,9 +3,16 @@ package miragefairy2019.mod.modules.ore;
 import static miragefairy2019.mod.api.oreseed.EnumOreSeedShape.*;
 import static miragefairy2019.mod.api.oreseed.EnumOreSeedType.*;
 import static miragefairy2019.mod.api.oreseed.GenerationConditions.*;
+import static miragefairy2019.mod.modules.ore.ore.EnumVariantOre1.*;
 
+import java.util.function.Supplier;
+
+import miragefairy2019.mod.api.oreseed.EnumOreSeedShape;
+import miragefairy2019.mod.api.oreseed.EnumOreSeedType;
+import miragefairy2019.mod.api.oreseed.IGenerationCondition;
 import miragefairy2019.mod.api.oreseed.RegistryOreSeedDrop;
-import miragefairy2019.mod.modules.ore.ore.EnumVariantOre1;
+import net.minecraft.block.state.IBlockState;
+import scala.util.Random;
 
 public class LoaderOreSeedDrop
 {
@@ -13,24 +20,65 @@ public class LoaderOreSeedDrop
 	public static void loadOreSeedDrop()
 	{
 
-		RegistryOreSeedDrop.register(STONE, LARGE, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.APATITE_ORE));
-		RegistryOreSeedDrop.register(STONE, PYRAMID, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.FLUORITE_ORE));
-		RegistryOreSeedDrop.register(STONE, STAR, 0.15, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.SULFUR_ORE), maxY(15));
-		RegistryOreSeedDrop.register(STONE, POINT, 0.15, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.CINNABAR_ORE), maxY(15));
-		RegistryOreSeedDrop.register(STONE, COAL, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.MAGNETITE_ORE));
-		RegistryOreSeedDrop.register(STONE, TINY, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.MOONSTONE_ORE), minY(40), maxY(50));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(APATITE_ORE));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(FLUORITE_ORE));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(SULFUR_ORE), maxY(15));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(CINNABAR_ORE), maxY(15));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(MAGNETITE_ORE));
+		r(STONE, POINT, 0.01, () -> ModuleOre.blockOre1.getState(MOONSTONE_ORE), minY(40), maxY(50));
 
-		RegistryOreSeedDrop.register(STONE, POINT, 0.05, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.PYROPE_ORE), maxY(50));
-		RegistryOreSeedDrop.register(STONE, LARGE, 0.08, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.SMITHSONITE_ORE), minY(30));
-		RegistryOreSeedDrop.register(STONE, LAPIS, 0.15, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NEPHRITE_ORE));
+		r(STONE, LARGE, 1, () -> ModuleOre.blockOre1.getState(APATITE_ORE), vein(4378, 32, 8, 0.1));
+		r(STONE, PYRAMID, 1, () -> ModuleOre.blockOre1.getState(FLUORITE_ORE), vein(5686, 32, 8, 0.1));
+		r(STONE, STAR, 1, () -> ModuleOre.blockOre1.getState(SULFUR_ORE), vein(5543, 32, 8, 0.15), maxY(15));
+		r(STONE, POINT, 1, () -> ModuleOre.blockOre1.getState(CINNABAR_ORE), vein(4891, 32, 8, 0.15), maxY(15));
+		r(STONE, COAL, 1, () -> ModuleOre.blockOre1.getState(MAGNETITE_ORE), vein(7868, 32, 8, 0.1));
+		r(STONE, TINY, 1, () -> ModuleOre.blockOre1.getState(MOONSTONE_ORE), vein(4508, 16, 4, 0.1), minY(40), maxY(50));
 
-		RegistryOreSeedDrop.register(NETHERRACK, LARGE, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_APATITE_ORE), minY(90));
-		RegistryOreSeedDrop.register(NETHERRACK, PYRAMID, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_FLUORITE_ORE), minY(90));
-		RegistryOreSeedDrop.register(NETHERRACK, TINY, 0.30, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_SULFUR_ORE), minY(20), maxY(40));
-		RegistryOreSeedDrop.register(NETHERRACK, IRON, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_CINNABAR_ORE));
-		RegistryOreSeedDrop.register(NETHERRACK, COAL, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_MAGNETITE_ORE));
-		RegistryOreSeedDrop.register(NETHERRACK, TINY, 0.10, () -> ModuleOre.blockOre1.getState(EnumVariantOre1.NETHERRACK_MOONSTONE_ORE), maxY(32));
+		r(STONE, POINT, 1, () -> ModuleOre.blockOre1.getState(PYROPE_ORE), vein(5348, 16, 4, 0.05), maxY(50));
+		r(STONE, LARGE, 1, () -> ModuleOre.blockOre1.getState(SMITHSONITE_ORE), vein(2894, 32, 8, 0.08), minY(30));
+		r(STONE, MEDIUM, 1, () -> ModuleOre.blockOre1.getState(NEPHRITE_ORE), vein(3485, 64, 16, 0.1));
 
+		r(NETHERRACK, LARGE, 0.10, () -> ModuleOre.blockOre1.getState(NETHERRACK_APATITE_ORE), minY(90));
+		r(NETHERRACK, PYRAMID, 0.10, () -> ModuleOre.blockOre1.getState(NETHERRACK_FLUORITE_ORE), minY(90));
+		r(NETHERRACK, TINY, 0.30, () -> ModuleOre.blockOre1.getState(NETHERRACK_SULFUR_ORE), minY(20), maxY(40));
+		r(NETHERRACK, IRON, 0.10, () -> ModuleOre.blockOre1.getState(NETHERRACK_CINNABAR_ORE));
+		r(NETHERRACK, COAL, 0.10, () -> ModuleOre.blockOre1.getState(NETHERRACK_MAGNETITE_ORE));
+		r(NETHERRACK, TINY, 0.10, () -> ModuleOre.blockOre1.getState(NETHERRACK_MOONSTONE_ORE), maxY(32));
+
+	}
+
+	private static void r(
+		EnumOreSeedType type,
+		EnumOreSeedShape shape,
+		double weight,
+		Supplier<IBlockState> block,
+		IGenerationCondition... generationConditions)
+	{
+		RegistryOreSeedDrop.register(type, shape, weight, block, generationConditions);
+	}
+
+	private static IGenerationCondition vein(long seed, int horizontalSize, int verticalSize, double rate)
+	{
+		return (type, shape, world, pos) -> rand(13788169L
+			+ seed * 86802673L
+			+ div(pos.getX(), horizontalSize) * 84663211L
+			+ div(pos.getY(), verticalSize) * 34193609L
+			+ div(pos.getZ(), horizontalSize) * 79500227L) < rate;
+	}
+
+	private static int div(int a, int b)
+	{
+		if (a < 0) a -= b - 1;
+		return a / b;
+	}
+
+	private static double rand(long seed)
+	{
+		Random random = new Random(seed);
+		random.nextDouble();
+		random.nextDouble();
+		random.nextDouble();
+		return random.nextDouble();
 	}
 
 }
