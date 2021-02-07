a251d8dc4a127f291ca7c9b407be9278d8bb46de
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-02-08 00:10:16 +0900

    internal: SelectorRayTrace: 機能追加

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/magic/SelectorRayTrace.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/magic/SelectorRayTrace.java
@@ -9,7 +9,9 @@ import mirrg.boron.util.struct.Tuple;
 import mirrg.boron.util.suppliterator.ISuppliterator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
@@ -25,28 +27,80 @@ public class SelectorRayTrace
 	private final World world;
 	private final Optional<RayTraceResult> oRayTraceResult;
 	private final Vec3d position;
+	private final BlockPos blockPos;
 
 	public SelectorRayTrace(World world, EntityPlayer player, double additionalReach)
 	{
 		this.world = world;
 		this.oRayTraceResult = Optional.ofNullable(rayTrace(world, player, false, additionalReach));
-		this.position = oRayTraceResult.isPresent()
-			? oRayTraceResult.get().hitVec
-			: getSight(player, player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue() + additionalReach);
+		if (oRayTraceResult.isPresent()) {
+			this.position = oRayTraceResult.get().hitVec;
+			this.blockPos = oRayTraceResult.get().getBlockPos() != null
+				? oRayTraceResult.get().getBlockPos()
+				: new BlockPos(this.position);
+		} else {
+			this.position = getSight(player, player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue() + additionalReach);
+			this.blockPos = new BlockPos(this.position);
+		}
 	}
 
 	//
+
+	public Optional<RayTraceResult> getRayTraceResult()
+	{
+		return oRayTraceResult;
+	}
+
+	public boolean isHit()
+	{
+		return oRayTraceResult.isPresent();
+	}
 
 	public Vec3d getPosition()
 	{
 		return position;
 	}
 
-	public Optional<Entity> getTargetEntity()
+	public BlockPos getBlockPos()
+	{
+		return blockPos;
+	}
+
+	public Optional<Entity> getEntityHit()
 	{
 		if (oRayTraceResult.isPresent()) {
 			if (oRayTraceResult.get().typeOfHit == Type.ENTITY) {
 				return Optional.of(oRayTraceResult.get().entityHit);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<Vec3d> getPositionHit()
+	{
+		if (oRayTraceResult.isPresent()) {
+			if (oRayTraceResult.get().typeOfHit == Type.ENTITY || oRayTraceResult.get().typeOfHit == Type.BLOCK) {
+				return Optional.of(oRayTraceResult.get().hitVec);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<BlockPos> getBlockPosHit()
+	{
+		if (oRayTraceResult.isPresent()) {
+			if (oRayTraceResult.get().typeOfHit == Type.BLOCK) {
+				return Optional.of(oRayTraceResult.get().getBlockPos());
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<EnumFacing> getSideHit()
+	{
+		if (oRayTraceResult.isPresent()) {
+			if (oRayTraceResult.get().typeOfHit == Type.BLOCK) {
+				return Optional.of(oRayTraceResult.get().sideHit);
 			}
 		}
 		return Optional.empty();
