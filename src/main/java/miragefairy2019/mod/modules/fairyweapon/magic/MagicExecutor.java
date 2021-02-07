77cfe13c256069d73731a05136244c134dbeec64
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-02-08 00:10:31 +0900

    internal: MagicExecutor: abstractでなくなった

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/magic/MagicExecutor.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/magic/MagicExecutor.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
@@ -12,12 +13,12 @@ public class MagicExecutor
 
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
 	{
-		throw new RuntimeException("Not implemented");
+		return new ActionResult<>(EnumActionResult.PASS, player.getHeldItem(hand));
 	}
 
 	public void onUpdate(ItemStack itemStack, World world, Entity entity, int itemSlot, boolean isSelected)
 	{
-		throw new RuntimeException("Not implemented");
+
 	}
 
 }
