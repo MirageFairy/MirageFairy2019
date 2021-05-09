a97b1012443c30c5e1014629e03a2b8fd5baf0f9
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-05-10 01:54:14 +0900

    _

--- src/main/java/miragefairy2019/mod/api/fairystick/IFairyStickCraftRegistry.java
+++ src/main/java/miragefairy2019/mod/api/fairystick/IFairyStickCraftRegistry.java
@@ -2,6 +2,7 @@ package miragefairy2019.mod.api.fairystick;
 
 import java.util.Optional;
 
+import mirrg.boron.util.suppliterator.ISuppliterator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -13,5 +14,7 @@ public interface IFairyStickCraftRegistry
 	public void registerRecipe(IFairyStickCraftRecipe recipe);
 
 	public Optional<IFairyStickCraftResult> getResult(Optional<EntityPlayer> oPlayer, World world, BlockPos pos, ItemStack itemStackFairyStick);
+
+	public ISuppliterator<IFairyStickCraftRecipe> getRecipes();
 
 }
