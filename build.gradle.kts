7fd6732af421cee14a99ba994fa1ec827a3131a7
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-29 12:55:30 +0900

    [buildscript] add: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,20 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+lateinit var adder: Configuration
+configurations {
+    adder = create("adder")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://mirrgieriana.github.io/mirrg.boron/maven")
+    // Progwml6 maven: location of the maven that hosts JEI files
+    maven("https://dvs1.progwml6.com/files/maven/")
+    // ModMaven: location of a maven mirror for JEI files, as a fallback
+    maven("https://modmaven.k-4u.nl")
+}
+
 minecraft {
     version = "1.12.2-14.23.5.2847"
     // !!gitHubAction-minecraftVersion="1.12.2-14.23.5.2847"
@@ -34,6 +48,16 @@ minecraft {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
+
+    compile("mirrg.boron:mirrg.boron.util:[3.19,4[")
+    adder("mirrg.boron:mirrg.boron.util:[3.19,4[")
+
+    compile("mirrg.boron:mirrg.boron.swing:0.1+")
+
+    compile("mezz.jei:jei_1.12.2:4.16.1.302")
+    //runtime("mezz.jei:jei_1.12.2:4.16.1.302")
+
+    compile(files("lib/appliedenergistics2/appliedenergistics2-rv6-stable-7-api.jar"))
 }
 
 (tasks["processResources"] as ProcessResources).run {
