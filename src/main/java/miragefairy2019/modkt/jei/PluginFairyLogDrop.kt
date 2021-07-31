10df873b2e13b7778a729d3f2dab34745eff38ff
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-31 17:32:59 +0900

    [refactor] kotlin: PluginFairyLogDrop

---
+++ src/main/java/miragefairy2019/modkt/jei/PluginFairyLogDrop.kt
@@ -0,0 +1,67 @@
+package miragefairy2019.modkt.jei
+
+import mezz.jei.api.IModPlugin
+import mezz.jei.api.IModRegistry
+import mezz.jei.api.JEIPlugin
+import mezz.jei.api.gui.IDrawable
+import mezz.jei.api.gui.IRecipeLayout
+import mezz.jei.api.ingredients.IIngredients
+import mezz.jei.api.ingredients.VanillaTypes
+import mezz.jei.api.recipe.IRecipeCategory
+import mezz.jei.api.recipe.IRecipeCategoryRegistration
+import mezz.jei.api.recipe.IRecipeWrapper
+import miragefairy2019.mod.api.ApiMirageFlower
+import miragefairy2019.mod.common.fairylogdrop.FairyLogDropRecipe
+import net.minecraft.client.Minecraft
+import net.minecraft.item.ItemStack
+
+@JEIPlugin
+class PluginFairyLogDrop : IModPlugin {
+    companion object {
+        const val uid = "miragefairy2019.fairyLogDrop"
+    }
+
+    override fun registerCategories(registry: IRecipeCategoryRegistration) {
+        registry.addRecipeCategories(object : IRecipeCategory<RecipeWrapperFairyLogDrop> {
+            override fun getUid() = Companion.uid
+            override fun getTitle() = "Fairy Log Drop"
+            override fun getModName() = "MirageFairy2019"
+            override fun getBackground() = object : IDrawable {
+                override fun getWidth() = 18 * 9
+                override fun getHeight() = 18 * 1
+                override fun draw(minecraft: Minecraft, xOffset: Int, yOffset: Int) {
+                    JeiUtilities.drawSlot(0f, 0f)
+                    JeiUtilities.drawSlot(45f, 0f)
+                }
+            }
+
+            override fun getIcon(): IDrawable? = registry.jeiHelpers.guiHelper.createDrawableIngredient(ItemStack(ApiMirageFlower.itemBlockFairyLog))
+            override fun setRecipe(recipeLayout: IRecipeLayout, recipeWrapper: RecipeWrapperFairyLogDrop, ingredients: IIngredients) {
+                val guiItemStackGroup = recipeLayout.itemStacks
+                guiItemStackGroup.init(0, true, 0, 0)
+                guiItemStackGroup.init(1, false, 45, 0)
+                guiItemStackGroup.set(ingredients)
+            }
+        })
+    }
+
+    override fun register(registry: IModRegistry) {
+        registry.addRecipes(ApiMirageFlower.fairyLogDropRegistry.recipes.toList(), uid)
+        registry.handleRecipes(FairyLogDropRecipe::class.java, { RecipeWrapperFairyLogDrop(registry, it) }, uid)
+    }
+
+    class RecipeWrapperFairyLogDrop(registry: IModRegistry, private val recipe: FairyLogDropRecipe) : IRecipeWrapper {
+        override fun getIngredients(ingredients: IIngredients) {
+            ingredients.setInput(VanillaTypes.ITEM, ItemStack(ApiMirageFlower.itemBlockFairyLog))
+            ingredients.setOutput(VanillaTypes.ITEM, recipe.itemStackOutput)
+        }
+
+        override fun drawInfo(minecraft: Minecraft, recipeWidth: Int, recipeHeight: Int, mouseX: Int, mouseY: Int) {
+            val descriptions = recipe.conditions.toList().chunked(2).map { it.joinToString(", ") { condition -> condition.localizedDescription } }
+            val string = String.format("%.0f%%", recipe.rate * 100)
+            minecraft.fontRenderer.drawString(string, 45 - 2 - minecraft.fontRenderer.getStringWidth(string), 4, 0x444444)
+            minecraft.fontRenderer.drawString(recipe.itemStackOutput.displayName, 65, 0, 0x444444)
+            descriptions.forEachIndexed { i, it -> minecraft.fontRenderer.drawString(it, 65, 10 + 10 * i, 0x444444) }
+        }
+    }
+}
