e7bcc399bf981fe32a3843c7dd72c0eff335e2e1
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-08-04 15:16:41 +0900

    [internal] TextComponentBuilder: add: translate

--- src/main/java/miragefairy2019/libkt/TextComponent.kt
+++ src/main/java/miragefairy2019/libkt/TextComponent.kt
@@ -2,14 +2,16 @@ package miragefairy2019.libkt
 
 import net.minecraft.util.text.ITextComponent
 import net.minecraft.util.text.TextComponentString
+import net.minecraft.util.text.TextComponentTranslation
 import net.minecraft.util.text.TextFormatting
 
 class TextComponentBuilder {
     private val textComponents = mutableListOf<ITextComponent>()
     fun build() = textComponents.fold(TextComponentString("") as ITextComponent) { a, b -> a.appendSibling(b) }!!
-    fun text(textComponent: ITextComponent) = textComponent.also { textComponents.add(it) }
+    fun <T : ITextComponent> text(textComponent: T) = textComponent.also { textComponents.add(it) }
     fun text(text: String) = text(TextComponentString(text))
     fun format(format: String, vararg args: Any?) = text(String.format(format, *args))
+    fun translate(translationKey: String, vararg args: Any?) = text(TextComponentTranslation(translationKey, *args))
     fun text(block: TextComponentBuilder.() -> Unit) = text(TextComponentBuilder().also { it.block() }.build())
 }
 
