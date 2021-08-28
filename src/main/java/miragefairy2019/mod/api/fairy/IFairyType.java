a65765cd2830ba7215e64a0c85cafc34d4c27e42
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-08-29 07:06:39 +0900

    [internal] IFairyType: add: mana, erg

--- src/main/java/miragefairy2019/mod/api/fairy/IFairyType.java
+++ src/main/java/miragefairy2019/mod/api/fairy/IFairyType.java
@@ -1,7 +1,10 @@
 package miragefairy2019.mod.api.fairy;
 
-import miragefairy2019.modkt.api.mana.IManaSet;
 import miragefairy2019.modkt.api.erg.IErgSet;
+import miragefairy2019.modkt.api.erg.IErgType;
+import miragefairy2019.modkt.api.mana.IManaSet;
+import miragefairy2019.modkt.api.mana.IManaType;
+import miragefairy2019.modkt.impl.ManaSetKt;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
@@ -17,7 +20,15 @@ public interface IFairyType {
 
     public IManaSet getManas();
 
+    public default double mana(IManaType manaType) {
+        return ManaSetKt.getMana(getManas(), manaType);
+    }
+
     public IErgSet getAbilities();
+
+    public default double erg(IErgType ergType) {
+        return getAbilities().getPower(ergType);
+    }
 
     public ITextComponent getDisplayName();
 
