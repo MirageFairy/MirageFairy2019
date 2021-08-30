f31034e4dc449ad4ee6097441417ae576ab354e6
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-08-30 17:58:14 +0900

    [refactor] kotlin: 妖精の関係登録部分

--- src/main/java/miragefairy2019/mod/modules/fairy/ModuleFairy.java
+++ src/main/java/miragefairy2019/mod/modules/fairy/ModuleFairy.java
@@ -2,24 +2,18 @@ package miragefairy2019.mod.modules.fairy;
 
 import miragefairy2019.mod.ModMirageFairy2019;
 import miragefairy2019.mod.api.fairy.ApiFairy;
-import miragefairy2019.mod.api.fairy.registry.ApiFairyRegistry;
 import miragefairy2019.mod.api.fairy.relation.IIngredientFairyRelation;
 import miragefairy2019.mod.api.main.ApiMain;
 import miragefairy2019.mod.common.fairy.relation.FairyRelationRegistry;
 import miragefairy2019.mod.lib.EventRegistryMod;
-import miragefairy2019.mod.lib.InitializationContext;
 import miragefairy2019.mod.lib.OreIngredientComplex;
 import miragefairy2019.modkt.api.erg.IErgEntry;
 import mirrg.boron.util.UtilsString;
 import mirrg.boron.util.struct.Tuple;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
@@ -33,8 +27,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.OreIngredient;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
-
-import java.util.function.Consumer;
 
 public class ModuleFairy {
 
@@ -197,133 +189,7 @@ public class ModuleFairy {
 
         });
 
-        // 妖精関係レジストリー
-        erMod.createItemStack.register(ic -> {
-
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockDiamond"), FairyTypes.instance.getDiamond().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockEmerald"), FairyTypes.instance.getEmerald().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockPyrope"), FairyTypes.instance.getPyrope().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockMoonstone"), FairyTypes.instance.getMoonstone().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockApatite"), FairyTypes.instance.getApatite().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("obsidian"), FairyTypes.instance.getObsidian().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockFluorite"), FairyTypes.instance.getFluorite().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockCinnabar"), FairyTypes.instance.getCinnabar().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockMagnetite"), FairyTypes.instance.getMagnetite().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("glowstone"), FairyTypes.instance.getGlowstone().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockSmithsonite"), FairyTypes.instance.getSmithsonite().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockLapis"), FairyTypes.instance.getLapislazuli().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockSulfur"), FairyTypes.instance.getSulfur().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockGold"), FairyTypes.instance.getGold().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockRedstone"), FairyTypes.instance.getRedstone().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(Ingredient.fromStacks(new ItemStack(Blocks.SAND)), FairyTypes.instance.getSand().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockNephrite"), FairyTypes.instance.getNephrite().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockTourmaline"), FairyTypes.instance.getTourmaline().get(0).getType().getBreed());
-            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(new OreIngredient("blockTopaz"), FairyTypes.instance.getTopaz().get(0).getType().getBreed());
-            // TODO ほとんどの妖精とアイテムの関連付けは妖精レジストリーを使う
-
-        });
-
-        // 妖精の関係を登録
-        erMod.createItemStack.register(new Consumer<InitializationContext>() {
-            @Override
-            public void accept(InitializationContext ic) {
-                r(1, FairyTypes.instance.getStone().get(0), i(Blocks.STONE, 0));
-                r(1, FairyTypes.instance.getDirt().get(0), i(Blocks.DIRT, 0));
-                r(1, FairyTypes.instance.getIron().get(0), i("ingotIron"));
-                r(1, FairyTypes.instance.getDiamond().get(0), i("gemDiamond"));
-                r(1, FairyTypes.instance.getMagnetite().get(0), i("gemMagnetite"));
-                r(1, FairyTypes.instance.getApatite().get(0), i("gemApatite"));
-                r(1, FairyTypes.instance.getFluorite().get(0), i("gemFluorite"));
-                r(1, FairyTypes.instance.getSulfur().get(0), i("gemSulfur"));
-                r(1, FairyTypes.instance.getCinnabar().get(0), i("gemCinnabar"));
-                r(1, FairyTypes.instance.getMoonstone().get(0), i("gemMoonstone"));
-                r(1, FairyTypes.instance.getPyrope().get(0), i("gemPyrope"));
-                r(1, FairyTypes.instance.getSmithsonite().get(0), i("gemSmithsonite"));
-                r(1, FairyTypes.instance.getRedstone().get(0), i("dustRedstone"));
-                r(1, FairyTypes.instance.getSand().get(0), i(Blocks.SAND));
-                r(1, FairyTypes.instance.getGold().get(0), i("ingotGold"));
-                r(1, FairyTypes.instance.getWheat().get(0), i(Items.WHEAT));
-                r(1, FairyTypes.instance.getLilac().get(0), i(Blocks.DOUBLE_PLANT, 1));
-                r(1, FairyTypes.instance.getTorch().get(0), i(Blocks.TORCH));
-                r(1, FairyTypes.instance.getGravel().get(0), i(Blocks.GRAVEL));
-                r(1, FairyTypes.instance.getEmerald().get(0), i("gemEmerald"));
-                r(1, FairyTypes.instance.getLapislazuli().get(0), i("gemLapislazuli"));
-                r(1, FairyTypes.instance.getFurnace().get(0), i(Blocks.FURNACE));
-                r(1, FairyTypes.instance.getMagentaglazedterracotta().get(0), i(Blocks.MAGENTA_GLAZED_TERRACOTTA));
-                r(1, FairyTypes.instance.getBread().get(0), i(Items.BREAD));
-                r(1, FairyTypes.instance.getApple().get(0), i(Items.APPLE));
-                r(1, FairyTypes.instance.getCarrot().get(0), i(Items.CARROT));
-                r(1, FairyTypes.instance.getCactus().get(0), i(Blocks.CACTUS));
-                r(1, FairyTypes.instance.getAxe().get(0), i(Items.IRON_AXE));
-                r(1, FairyTypes.instance.getChest().get(0), i(Blocks.CHEST));
-                r(1, FairyTypes.instance.getCraftingtable().get(0), i(Blocks.CRAFTING_TABLE));
-                r(1, FairyTypes.instance.getPotion().get(0), i(Items.POTIONITEM));
-                r(1, FairyTypes.instance.getSword().get(0), i(Items.IRON_SWORD));
-                r(1, FairyTypes.instance.getDispenser().get(0), i(Blocks.DISPENSER));
-                r(1, FairyTypes.instance.getCod().get(0), i(Items.FISH, 0));
-                r(1, FairyTypes.instance.getSalmon().get(0), i(Items.FISH, 1));
-                r(1, FairyTypes.instance.getPufferfish().get(0), i(Items.FISH, 3));
-                r(1, FairyTypes.instance.getClownfish().get(0), i(Items.FISH, 2));
-                r(1, FairyTypes.instance.getSpruce().get(0), i(Blocks.LOG, 1), i(Blocks.SAPLING, 1));
-                r(1, FairyTypes.instance.getAnvil().get(0), i(Blocks.ANVIL));
-                r(1, FairyTypes.instance.getObsidian().get(0), i(Blocks.OBSIDIAN));
-                r(1, FairyTypes.instance.getSeed().get(0), i(Items.WHEAT_SEEDS));
-                r(1, FairyTypes.instance.getGlowstone().get(0), i(Items.GLOWSTONE_DUST), i(Blocks.GLOWSTONE));
-                r(1, FairyTypes.instance.getCoal().get(0), i(Items.COAL, 0));
-                r(1, FairyTypes.instance.getNetherstar().get(0), i(Items.NETHER_STAR));
-                r(1, FairyTypes.instance.getBrewingstand().get(0), i(Items.BREWING_STAND));
-                r(1, FairyTypes.instance.getHoe().get(0), i(Items.IRON_HOE));
-                r(1, FairyTypes.instance.getShield().get(0), i(Items.SHIELD));
-                r(1, FairyTypes.instance.getHopper().get(0), i(Blocks.HOPPER));
-                r(1, FairyTypes.instance.getNephrite().get(0), i("gemNephrite"));
-                r(1, FairyTypes.instance.getTourmaline().get(0), i("gemTourmaline"));
-                r(1, FairyTypes.instance.getTopaz().get(0), i("gemTopaz"));
-                r(1, FairyTypes.instance.getCookie().get(0), i(Items.COOKIE));
-                r(1, FairyTypes.instance.getCake().get(0), i(Items.CAKE));
-                r(1, FairyTypes.instance.getEnchantedgoldenapple().get(0), i(Items.GOLDEN_APPLE, 1));
-                r(1, FairyTypes.instance.getSugar().get(0), i(Items.SUGAR));
-                r(1, FairyTypes.instance.getRottenflesh().get(0), i(Items.ROTTEN_FLESH));
-                r(1, FairyTypes.instance.getPoisonouspotato().get(0), i(Items.POISONOUS_POTATO));
-                r(1, FairyTypes.instance.getMelon().get(0), i(Items.MELON));
-                r(1, FairyTypes.instance.getBakedpotato().get(0), i(Items.BAKED_POTATO));
-                r(1, FairyTypes.instance.getCookedchicken().get(0), i(Items.COOKED_CHICKEN));
-                r(1, FairyTypes.instance.getCookedsalmon().get(0), i(Items.COOKED_FISH, 1));
-                r(1, FairyTypes.instance.getSteak().get(0), i(Items.COOKED_BEEF));
-                r(1, FairyTypes.instance.getGoldenapple().get(0), i(Items.GOLDEN_APPLE, 0));
-
-            }
-
-            private Ingredient i(Item item) {
-                return i(item, 32767);
-            }
-
-            private Ingredient i(Item item, int meta) {
-                return Ingredient.fromStacks(new ItemStack(item, 1, meta));
-            }
-
-            private Ingredient i(Block block) {
-                return i(block, 32767);
-            }
-
-            private Ingredient i(Block block, int meta) {
-                return Ingredient.fromStacks(new ItemStack(Item.getItemFromBlock(block), 1, meta));
-            }
-
-            @SuppressWarnings("unused")
-            private Ingredient i(ItemStack itemStack) {
-                return Ingredient.fromStacks(itemStack);
-            }
-
-            private Ingredient i(String ore) {
-                return new OreIngredient(ore);
-            }
-
-            private void r(double relevance, VariantFairy variantFairy, Ingredient... ingredients) {
-                for (Ingredient ingredient : ingredients) {
-                    ApiFairy.fairyRelationRegistry.registerIngredientFairyRelation(relevance, variantFairy.createItemStack(), ingredient);
-                }
-            }
-        });
+        ModuleFairyKt.init(erMod);
 
         // 妖精の確定レシピ
         erMod.addRecipe.register(() -> {
---
+++ src/main/java/miragefairy2019/mod/modules/fairy/ModuleFairy.kt
@@ -0,0 +1,124 @@
+package miragefairy2019.mod.modules.fairy
+
+import miragefairy2019.mod.api.fairy.ApiFairy
+import miragefairy2019.mod.api.fairy.registry.ApiFairyRegistry
+import miragefairy2019.mod.lib.EventRegistryMod
+import net.minecraft.block.Block
+import net.minecraft.init.Blocks
+import net.minecraft.init.Items
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.item.crafting.Ingredient
+import net.minecraftforge.oredict.OreIngredient
+import java.util.function.Consumer
+
+fun init(erMod: EventRegistryMod) {
+    fun i(item: Item, meta: Int = 32767) = Ingredient.fromStacks(ItemStack(item, 1, meta))
+    fun i(block: Block, meta: Int = 32767) = Ingredient.fromStacks(ItemStack(Item.getItemFromBlock(block), 1, meta))
+    fun i(itemStack: ItemStack) = Ingredient.fromStacks(itemStack)
+    fun i(ore: String) = OreIngredient(ore)
+
+    // 妖精関係レジストリー
+    // TODO ほとんどの妖精とアイテムの関連付けは妖精レジストリーを使う
+    erMod.createItemStack.register(Consumer {
+        operator fun RankedFairyTypeBundle.invoke(ingredient: Ingredient) {
+            ApiFairyRegistry.getFairyRelationRegistry().registerFairyRelationItemStack(ingredient, this.main.type.breed)
+        }
+
+        FairyTypes.instance.run {
+            diamond(i("blockDiamond"))
+            emerald(i("blockEmerald"))
+            pyrope(i("blockPyrope"))
+            moonstone(i("blockMoonstone"))
+            apatite(i("blockApatite"))
+            obsidian(i("obsidian"))
+            fluorite(i("blockFluorite"))
+            cinnabar(i("blockCinnabar"))
+            magnetite(i("blockMagnetite"))
+            glowstone(i("glowstone"))
+            smithsonite(i("blockSmithsonite"))
+            lapislazuli(i("blockLapis"))
+            sulfur(i("blockSulfur"))
+            gold(i("blockGold"))
+            redstone(i("blockRedstone"))
+            sand(i(ItemStack(Blocks.SAND)))
+            nephrite(i("blockNephrite"))
+            tourmaline(i("blockTourmaline"))
+            topaz(i("blockTopaz"))
+        }
+    })
+
+    // 妖精の関係を登録
+    erMod.createItemStack.register(Consumer {
+        operator fun RankedFairyTypeBundle.invoke(relevance: Double, vararg ingredients: Ingredient) = ingredients.forEach {
+            ApiFairy.fairyRelationRegistry.registerIngredientFairyRelation(relevance, main.createItemStack(), it)
+        }
+
+        FairyTypes.instance.run {
+            stone(1.0, i(Blocks.STONE, 0))
+            dirt(1.0, i(Blocks.DIRT, 0))
+            iron(1.0, i("ingotIron"))
+            diamond(1.0, i("gemDiamond"))
+            magnetite(1.0, i("gemMagnetite"))
+            apatite(1.0, i("gemApatite"))
+            fluorite(1.0, i("gemFluorite"))
+            sulfur(1.0, i("gemSulfur"))
+            cinnabar(1.0, i("gemCinnabar"))
+            moonstone(1.0, i("gemMoonstone"))
+            pyrope(1.0, i("gemPyrope"))
+            smithsonite(1.0, i("gemSmithsonite"))
+            redstone(1.0, i("dustRedstone"))
+            sand(1.0, i(Blocks.SAND))
+            gold(1.0, i("ingotGold"))
+            wheat(1.0, i(Items.WHEAT))
+            lilac(1.0, i(Blocks.DOUBLE_PLANT, 1))
+            torch(1.0, i(Blocks.TORCH))
+            gravel(1.0, i(Blocks.GRAVEL))
+            emerald(1.0, i("gemEmerald"))
+            lapislazuli(1.0, i("gemLapislazuli"))
+            furnace(1.0, i(Blocks.FURNACE))
+            magentaglazedterracotta(1.0, i(Blocks.MAGENTA_GLAZED_TERRACOTTA))
+            bread(1.0, i(Items.BREAD))
+            apple(1.0, i(Items.APPLE))
+            carrot(1.0, i(Items.CARROT))
+            cactus(1.0, i(Blocks.CACTUS))
+            axe(1.0, i(Items.IRON_AXE))
+            chest(1.0, i(Blocks.CHEST))
+            craftingtable(1.0, i(Blocks.CRAFTING_TABLE))
+            potion(1.0, i(Items.POTIONITEM))
+            sword(1.0, i(Items.IRON_SWORD))
+            dispenser(1.0, i(Blocks.DISPENSER))
+            cod(1.0, i(Items.FISH, 0))
+            salmon(1.0, i(Items.FISH, 1))
+            pufferfish(1.0, i(Items.FISH, 3))
+            clownfish(1.0, i(Items.FISH, 2))
+            spruce(1.0, i(Blocks.LOG, 1), i(Blocks.SAPLING, 1))
+            anvil(1.0, i(Blocks.ANVIL))
+            obsidian(1.0, i(Blocks.OBSIDIAN))
+            seed(1.0, i(Items.WHEAT_SEEDS))
+            glowstone(1.0, i(Items.GLOWSTONE_DUST), i(Blocks.GLOWSTONE))
+            coal(1.0, i(Items.COAL, 0))
+            netherstar(1.0, i(Items.NETHER_STAR))
+            brewingstand(1.0, i(Items.BREWING_STAND))
+            hoe(1.0, i(Items.IRON_HOE))
+            shield(1.0, i(Items.SHIELD))
+            hopper(1.0, i(Blocks.HOPPER))
+            nephrite(1.0, i("gemNephrite"))
+            tourmaline(1.0, i("gemTourmaline"))
+            topaz(1.0, i("gemTopaz"))
+            cookie(1.0, i(Items.COOKIE))
+            cake(1.0, i(Items.CAKE))
+            enchantedgoldenapple(1.0, i(Items.GOLDEN_APPLE, 1))
+            sugar(1.0, i(Items.SUGAR))
+            rottenflesh(1.0, i(Items.ROTTEN_FLESH))
+            poisonouspotato(1.0, i(Items.POISONOUS_POTATO))
+            melon(1.0, i(Items.MELON))
+            bakedpotato(1.0, i(Items.BAKED_POTATO))
+            cookedchicken(1.0, i(Items.COOKED_CHICKEN))
+            cookedsalmon(1.0, i(Items.COOKED_FISH, 1))
+            steak(1.0, i(Items.COOKED_BEEF))
+            goldenapple(1.0, i(Items.GOLDEN_APPLE, 0))
+        }
+    })
+
+}
