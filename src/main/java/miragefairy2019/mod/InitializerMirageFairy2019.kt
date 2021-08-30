b7758d4714258cbe7b644288f5e6fa67f27dece4
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-08-30 23:49:51 +0900

    [refactor] move: LoaderFairyCrystalDrop

--- src/main/java/miragefairy2019/mod/InitializerMirageFairy2019.kt
+++ src/main/java/miragefairy2019/mod/InitializerMirageFairy2019.kt
@@ -8,7 +8,7 @@ import miragefairy2019.mod.api.materialsfairy.ApiMaterialsFairy
 import miragefairy2019.mod.lib.EventRegistryMod
 import miragefairy2019.mod.lib.InitializationContext
 import miragefairy2019.mod.modules.fairycrystal.ModuleFairyCrystal
-import miragefairy2019.mod.modules.fairycrystal.loaderFairyCrystalDrop
+import miragefairy2019.modkt.modules.fairy.loaderFairyCrystalDrop
 import miragefairy2019.mod.modules.fairystick.ModuleFairyStick
 import miragefairy2019.mod.modules.fairyweapon.item.Loader
 import miragefairy2019.mod.modules.mirageflower.ModuleMirageFlower
