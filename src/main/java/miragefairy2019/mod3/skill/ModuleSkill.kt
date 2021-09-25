4f9e6d132d146a69d0bae42c987f7bd3ea00e085
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-26 04:52:12 +0900

    [refactor] ModuleSkill

--- src/main/java/miragefairy2019/mod3/skill/ModuleSkill.kt
+++ src/main/java/miragefairy2019/mod3/skill/ModuleSkill.kt
@@ -1,13 +1,24 @@
 package miragefairy2019.modkt.api.skill
 
 import io.netty.buffer.ByteBuf
+import miragefairy2019.libkt.Component
 import miragefairy2019.libkt.Module
+import miragefairy2019.libkt.PointInt
 import miragefairy2019.libkt.RectangleInt
+import miragefairy2019.libkt.TextAlignment
+import miragefairy2019.libkt.argb
+import miragefairy2019.libkt.component
 import miragefairy2019.libkt.drawGuiBackground
-import miragefairy2019.libkt.drawStringRightAligned
+import miragefairy2019.libkt.label
+import miragefairy2019.libkt.minus
+import miragefairy2019.libkt.position
+import miragefairy2019.libkt.rectangle
+import miragefairy2019.libkt.toArgb
+import miragefairy2019.libkt.tooltip
 import miragefairy2019.mod.ModMirageFairy2019
 import miragefairy2019.mod.api.main.ApiMain
 import miragefairy2019.mod3.main.registerGuiHandler
+import miragefairy2019.mod3.skill.EnumMastery
 import miragefairy2019.mod3.skill.api.ApiSkill
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.entity.player.EntityPlayer
@@ -123,38 +134,49 @@ class MessageSkill : IMessage {
     }
 }
 
-class ContainerSkill() : Container() {
+class ContainerSkill : Container() {
     override fun canInteractWith(playerIn: EntityPlayer) = true
 }
 
-class GuiSkill() : GuiContainer(ContainerSkill()) {
+class GuiSkill : GuiContainer(ContainerSkill()) {
+
+    private val components = mutableListOf<Component>()
+
+    init {
+        components += component(RectangleInt(4, 4, xSize - 4 - 4 - 20 - 20, 10)) {
+            label(::fontRenderer, "マスタリ名", color = 0xFF404040.toArgb())
+        }
+        components += component(RectangleInt(xSize - 44, 4, 20, 10)) {
+            label(::fontRenderer, "MLv", color = argb(0xFF404040.toInt()), align = TextAlignment.RIGHT)
+            tooltip("マスタリレベルはある領域に関する理解の深さです。")
+        }
+        components += component(RectangleInt(xSize - 24, 4, 20, 10)) {
+            label(::fontRenderer, "SLv", color = argb(0xFF404040.toInt()), align = TextAlignment.RIGHT)
+            tooltip("スキルレベルは個々のアクションの強さです。")
+        }
+        EnumMastery.values().forEachIndexed { i, it ->
+            components += component(RectangleInt(4 + 8 * it.layer, 14 + 10 * i, xSize - 4 - 4 - 20 - 20 - 8 * it.layer, 10)) {
+                label(::fontRenderer, it.displayName.unformattedText, color = 0xFF000000.toArgb())
+                tooltip(it.displayPoem.unformattedText)
+            }
+            components += component(RectangleInt(xSize - 24, 14 + 10 * i, 20, 10)) {
+                label(::fontRenderer, "${ApiSkill.skillManager.clientSkillContainer.getMasteryLevel(it)}", color = 0xFF000000.toArgb(), align = TextAlignment.RIGHT)
+            }
+            components += component(RectangleInt(xSize - 4, 14 + 10 * i, 20, 10)) {
+                label(::fontRenderer, "${ApiSkill.skillManager.clientSkillContainer.getSkillLevel(it)}", color = 0xFF000000.toArgb(), align = TextAlignment.RIGHT)
+            }
+        }
+    }
+
     override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
         drawDefaultBackground()
         super.drawScreen(mouseX, mouseY, partialTicks)
-        val x = (width - xSize) / 2
-        val y = (height - ySize) / 2
-        if (mouseX in x + xSize - 44..x + xSize - 24 && mouseY in y + 4..y + 14) {
-            drawHoveringText(listOf("マスタリレベルはある領域に関する理解の深さです。"), mouseX, mouseY)
-        }
-        if (mouseX in x + xSize - 24..x + xSize - 4 && mouseY in y + 4..y + 14) {
-            drawHoveringText(listOf("スキルレベルは個々のアクションの強さです。"), mouseX, mouseY)
-        }
+        components.forEach { it.onScreenDraw.fire { it(PointInt(mouseX, mouseY) - position, partialTicks) } }
     }
 
-    override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) {
-        val x = (width - xSize) / 2
-        val y = (height - ySize) / 2
-        RectangleInt(x, y, xSize, ySize).drawGuiBackground()
-    }
+    override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) = rectangle.drawGuiBackground()
 
     override fun drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int) {
-        fontRenderer.drawString("マスタリ名", 4, 4, 0x404040)
-        fontRenderer.drawStringRightAligned("MLv", xSize - 24, 4, 0x404040)
-        fontRenderer.drawStringRightAligned("SLv", xSize - 4, 4, 0x404040)
-        EnumMastery.values().forEachIndexed { i, it ->
-            fontRenderer.drawString(it.displayName.unformattedText, 4 + 8 * it.layer, 14 + 10 * i, 0x000000)
-            fontRenderer.drawStringRightAligned("${ApiSkill.skillManager.clientSkillContainer.getMasteryLevel(it)}", xSize - 24, 14 + 10 * i, 0x000000)
-            fontRenderer.drawStringRightAligned("${ApiSkill.skillManager.clientSkillContainer.getSkillLevel(it)}", xSize - 4, 14 + 10 * i, 0x000000)
-        }
+        components.forEach { it.onForegroundDraw.fire { it(PointInt(mouseX, mouseY) - position) } }
     }
 }
