1808027149c6e710b6343223be3f8f68698cd253
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-28 03:09:34 +0900

    [internal] add: Date

---
+++ src/main/java/miragefairy2019/libkt/Date.kt
@@ -0,0 +1,18 @@
+package miragefairy2019.libkt
+
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+// UTC変換
+fun Instant.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(this, ZoneOffset.UTC)
+fun LocalDateTime.toInstant(): Instant = toInstant(ZoneOffset.UTC)
+
+operator fun Instant.minus(other: Instant): Duration = Duration.ofMillis(toEpochMilli() - other.toEpochMilli())
+
+fun LocalDate.withZeroTime(): LocalDateTime = atTime(0, 0, 0)
+fun LocalDate.atDayStart() = withZeroTime()
+fun LocalDate.atWeekStart() = minusDays((dayOfWeek.value % 7).toLong()).withZeroTime()
+fun LocalDate.atMonthStart() = withDayOfMonth(1).withZeroTime()
