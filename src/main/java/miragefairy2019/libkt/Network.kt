ddf7178f2c0a054c52b14833c3b8b987e0c89f66
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-29 23:54:32 +0900

    [internal] add: libkt\Network

---
+++ src/main/java/miragefairy2019/libkt/Network.kt
@@ -0,0 +1,24 @@
+package miragefairy2019.libkt
+
+import io.netty.buffer.ByteBuf
+import net.minecraft.network.PacketBuffer
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage
+
+open class MessageJson(var json: String? = null) : IMessage {
+    override fun fromBytes(buf: ByteBuf) {
+        val packetBuffer = PacketBuffer(buf)
+        val size = packetBuffer.readInt()
+        json = (0 until size).joinToString("") { packetBuffer.readString(10000) }
+    }
+
+    override fun toBytes(buf: ByteBuf) {
+        val json = this.json!!
+        if (json.length > 1_000_000) throw Exception("Too long json: ${json.length}")
+        val strings = json.chunked(10000)
+        val packetBuffer = PacketBuffer(buf)
+        packetBuffer.writeInt(strings.size)
+        strings.forEach {
+            packetBuffer.writeString(it)
+        }
+    }
+}
