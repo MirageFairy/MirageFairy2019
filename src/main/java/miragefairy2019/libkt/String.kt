436a04fd34c9f2161121feebfca8f951f121c03f
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-29 23:54:44 +0900

    [internal] libkt\String: add

--- src/main/java/miragefairy2019/libkt/String.kt
+++ src/main/java/miragefairy2019/libkt/String.kt
@@ -2,3 +2,9 @@ package miragefairy2019.libkt
 
 fun Double.stringFormat(format: String) = String.format(format, this)
 fun Float.stringFormat(format: String) = String.format(format, this)
+fun Long.stringFormat(format: String) = String.format(format, this)
+fun Int.stringFormat(format: String) = String.format(format, this)
+fun Short.stringFormat(format: String) = String.format(format, this)
+fun Byte.stringFormat(format: String) = String.format(format, this)
+fun Boolean.stringFormat(format: String) = String.format(format, this)
+fun String.stringFormat(format: String) = String.format(format, this)
