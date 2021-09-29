24fec81d2628a887797fc392dd3135a1ad7d2157
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-29 23:54:22 +0900

    [comment]

--- src/main/java/miragefairy2019/libkt/Date.kt
+++ src/main/java/miragefairy2019/libkt/Date.kt
@@ -13,6 +13,10 @@ fun LocalDateTime.toInstant(): Instant = toInstant(ZoneOffset.UTC)
 operator fun Instant.minus(other: Instant): Duration = Duration.ofMillis(toEpochMilli() - other.toEpochMilli())
 
 fun LocalDate.withZeroTime(): LocalDateTime = atTime(0, 0, 0)
+
+// TODO rename atStartOfDay
+// TODO Instantに対して直接実行できるように
+// TODO remove atDayStartは既存メソッドがある
 fun LocalDate.atDayStart() = withZeroTime()
 fun LocalDate.atWeekStart() = minusDays((dayOfWeek.value % 7).toLong()).withZeroTime()
 fun LocalDate.atMonthStart() = withDayOfMonth(1).withZeroTime()
