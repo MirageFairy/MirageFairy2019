2374afdb52e70164790a364328c8dfdbd9bbafdd
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-09-30 00:15:12 +0900

    [test] add: TestSkill

---
+++ src/test/java/miragefairy2019/mod3/skill/Impl.kt
@@ -0,0 +1,48 @@
+package miragefairy2019.mod3.skill
+
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EntityPlayerMP
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.io.File
+
+class TestSkill {
+    @Test
+    fun test() {
+        val skillManager = object : SkillManager() {
+            override fun getFile(player: EntityPlayer): File = throw UnsupportedOperationException()
+            override fun send(player: EntityPlayerMP, json: String): Unit = throw UnsupportedOperationException()
+        }
+
+        // 必要経験値
+        assertEquals(0, skillManager.getFairyMasterExp(0))
+        assertEquals(5, skillManager.getFairyMasterExp(1))
+        assertEquals(5 + 10, skillManager.getFairyMasterExp(2))
+        assertEquals(5 + 10 + 15 + 20 + 25, skillManager.getFairyMasterExp(5))
+        assertEquals(5 * 10 * (10 + 1) / 2, skillManager.getFairyMasterExp(10))
+
+        // 現在レベル
+        assertEquals(0, skillManager.getFairyMasterLevel(0))
+        assertEquals(0, skillManager.getFairyMasterLevel(4))
+        assertEquals(1, skillManager.getFairyMasterLevel(5))
+        assertEquals(1, skillManager.getFairyMasterLevel(14))
+        assertEquals(2, skillManager.getFairyMasterLevel(15))
+        assertEquals(19, skillManager.getFairyMasterLevel(1049))
+        assertEquals(20, skillManager.getFairyMasterLevel(1050))
+
+        // 現在レベル計算誤差テスト
+        (1..500).forEach { lv ->
+
+            // レベルを必要経験値にしてレベルに戻すと等しい
+            assertEquals(lv, skillManager.getFairyMasterLevel(skillManager.getFairyMasterExp(lv)))
+
+            // そのレベルの必要経験値に1を足してもレベルは変わらない
+            assertEquals(lv, skillManager.getFairyMasterLevel(skillManager.getFairyMasterExp(lv) + 1))
+
+            // そのレベルの必要経験値から1を引くとレベルが1下がる
+            assertEquals(lv - 1, skillManager.getFairyMasterLevel(skillManager.getFairyMasterExp(lv) - 1))
+
+        }
+
+    }
+}
