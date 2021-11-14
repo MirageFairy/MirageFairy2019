8be2da500d6a81dfb70e490967864e609a3c8853
titanium <tacticsrealize2@gmail.com>
2021-11-14 13:51:25 +0900

    [change] 【変更】花摘みの鐘の耐久消費量を増加

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemBellFlowerPicking.kt
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemBellFlowerPicking.kt
@@ -41,7 +41,7 @@ class ItemBellFlowerPicking(weaponStrength: Double, weaponExtent: Double, weapon
     val fortune = "fortune"({ double2.positive }) { 3 + !production * 0.1 + !knowledge * 0.1 }.setRange(0.0..100.0)
     val additionalReach = "additionalReach"({ double2.positive }) { !extent * 0.1 }.setRange(0.0..10.0)
     val radius = "radius"({ double2.positive }) { 4 + !extent * 0.05 }.setRange(0.0..10.0)
-    val wear = "wear"({ percent2.negative }) { 0.2 / (1 + !endurance * 0.03) }
+    val wear = "wear"({ percent2.negative }) { 1.0 / (1 + !endurance * 0.03) }
     val coolTime = "coolTime"({ tick.negative }) { cost * 0.5 }
     val collection = "collection"({ boolean.positiveBoolean }) { !warp >= 10 }.setVisibility(ALWAYS)
     val extraItemDropRate = "extraItemDropRate"({ percent1.positive }) { (getSkillLevel(mastery) / 100.0).coerceIn(0.0, 1.0) }.setVisibility(ALWAYS)
