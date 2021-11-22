b06cda52f1b6ba7fa4c48e17bdf4320c59a15c0e
titanium <tacticsrealize2@gmail.com>
2021-11-23 02:15:47 +0900

    [fix] fairyweapon\item\Loader

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
@@ -21,21 +21,24 @@ import miragefairy2019.modkt.api.erg.ErgTypes.submission
 import miragefairy2019.modkt.api.erg.ErgTypes.water
 import miragefairy2019.modkt.api.erg.IErgType
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import net.minecraft.item.ItemStack
 import net.minecraftforge.client.event.ModelBakeEvent
 import net.minecraftforge.client.model.ModelLoader
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.oredict.OreDictionary
 import java.util.function.Supplier
 
 private fun <T : ItemFairyWeaponBase> ModInitializer.fw(
     tier: Int,
     creator: () -> T,
     registryName: String,
-    unlocalizedNameList: List<String>,
+    unlocalizedName: String,
+    oreNameList: List<String>,
     parent: (() -> Supplier<ItemFairyWeaponBase>)?,
     vararg ergTypeSuppliers: () -> IErgType
 ) = item(creator, registryName) {
-    unlocalizedNameList.forEach { setUnlocalizedName(it) }
+    setUnlocalizedName(unlocalizedName)
     setCreativeTab { ModuleMain.creativeTab }
     onRegisterItem {
         if (ApiMain.side().isClient) {
@@ -54,6 +57,9 @@ private fun <T : ItemFairyWeaponBase> ModInitializer.fw(
         ergTypeSuppliers.forEach { item.addComponent(ApiComposite.instance(ApiFairy.getComponentAbilityType(it()))) }
         item.maxDamage = Loader.getDurability(tier) - 1
     }
+    onCreateItemStack {
+        oreNameList.forEach { OreDictionary.registerOre(it, ItemStack(item, 1, OreDictionary.WILDCARD_VALUE)) }
+    }
 }
 
 
@@ -66,31 +72,31 @@ class FairyWeaponLoader(m: ModInitializer) {
         private val fw2 = "mirageFairy2019CraftingToolFairyWand"
     }
 
-    val fairyWandCrafting = m.fw(1, ::ItemFairyWandCrafting, "crafting_fairy_wand", listOf("${fw}Crafting", "${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
-    val fairyWandCrafting2 = m.fw(2, ::ItemFairyWandCrafting, "crafting_fairy_wand_2", listOf("${fw}Crafting2", "${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
-    val fairyWandCrafting3 = m.fw(3, ::ItemFairyWandCrafting, "crafting_fairy_wand_3", listOf("${fw}Crafting3", "${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
-    val fairyWandCrafting4 = m.fw(4, ::ItemFairyWandCrafting, "crafting_fairy_wand_4", listOf("${fw}Crafting4", "${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
-    val fairyWandHydrating = m.fw(1, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand", listOf("${fw}Hydrating", "${fw2}Hydrating", "container1000Water"), null, { water })
-    val fairyWandHydrating2 = m.fw(2, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_2", listOf("${fw}Hydrating2", "${fw2}Hydrating", "container1000Water"), null, { water })
-    val fairyWandHydrating3 = m.fw(3, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_3", listOf("${fw}Hydrating3", "${fw2}Hydrating", "container1000Water"), null, { water })
-    val fairyWandHydrating4 = m.fw(4, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_4", listOf("${fw}Hydrating4", "${fw2}Hydrating", "container1000Water"), null, { water })
-    val fairyWandMelting = m.fw(2, ::ItemFairyWandMelting, "melting_fairy_wand", listOf("${fw}Melting", "${fw2}Melting"), null, { flame })
-    val fairyWandMelting2 = m.fw(3, ::ItemFairyWandMelting, "melting_fairy_wand_2", listOf("${fw}Melting2", "${fw2}Melting"), null, { flame })
-    val fairyWandMelting3 = m.fw(4, ::ItemFairyWandMelting, "melting_fairy_wand_3", listOf("${fw}Melting3", "${fw2}Melting"), null, { flame })
-    val fairyWandBreaking = m.fw(2, ::ItemFairyWandBreaking, "breaking_fairy_wand", listOf("${fw}Breaking", "${fw2}Breaking"), null, { breaking })
-    val fairyWandBreaking2 = m.fw(3, ::ItemFairyWandBreaking, "breaking_fairy_wand_2", listOf("${fw}Breaking2", "${fw2}Breaking"), null, { breaking })
-    val fairyWandBreaking3 = m.fw(4, ::ItemFairyWandBreaking, "breaking_fairy_wand_3", listOf("${fw}Breaking3", "${fw2}Breaking"), null, { breaking })
-    val fairyWandFreezing = m.fw(2, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand", listOf("${fw}Freezing", "${fw2}Freezing"), null, { freeze })
-    val fairyWandFreezing2 = m.fw(3, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand_2", listOf("${fw}Freezing2", "${fw2}Freezing"), null, { freeze })
-    val fairyWandFreezing3 = m.fw(4, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand_3", listOf("${fw}Freezing3", "${fw2}Freezing"), null, { freeze })
-    val fairyWandPolishing = m.fw(3, ::ItemFairyWeaponCraftingTool, "polishing_fairy_wand", listOf("${fw}Polishing", "${fw2}Polishing"), null, { crystal })
-    val fairyWandPolishing2 = m.fw(4, ::ItemFairyWeaponCraftingTool, "polishing_fairy_wand_2", listOf("${fw}Polishing2", "${fw2}Polishing"), null, { crystal })
-    val fairyWandSummoning = m.fw(3, ::ItemFairyWandSummoning, "summoning_fairy_wand", listOf("${fw}Summoning", "${fw2}Summoning"), null, { submission })
-    val fairyWandSummoning2 = m.fw(4, ::ItemFairyWandSummoning, "summoning_fairy_wand_2", listOf("${fw}Summoning2", "${fw2}Summoning"), null, { submission })
+    val fairyWandCrafting = m.fw(1, ::ItemFairyWandCrafting, "crafting_fairy_wand", "${fw}Crafting", listOf("${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
+    val fairyWandCrafting2 = m.fw(2, ::ItemFairyWandCrafting, "crafting_fairy_wand_2", "${fw}Crafting2", listOf("${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
+    val fairyWandCrafting3 = m.fw(3, ::ItemFairyWandCrafting, "crafting_fairy_wand_3", "${fw}Crafting3", listOf("${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
+    val fairyWandCrafting4 = m.fw(4, ::ItemFairyWandCrafting, "crafting_fairy_wand_4", "${fw}Crafting4", listOf("${fw2}Crafting", "mirageFairy2019FairyStick"), null, { craft })
+    val fairyWandHydrating = m.fw(1, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand", "${fw}Hydrating", listOf("${fw2}Hydrating", "container1000Water"), null, { water })
+    val fairyWandHydrating2 = m.fw(2, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_2", "${fw}Hydrating2", listOf("${fw2}Hydrating", "container1000Water"), null, { water })
+    val fairyWandHydrating3 = m.fw(3, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_3", "${fw}Hydrating3", listOf("${fw2}Hydrating", "container1000Water"), null, { water })
+    val fairyWandHydrating4 = m.fw(4, ::ItemFairyWeaponCraftingTool, "hydrating_fairy_wand_4", "${fw}Hydrating4", listOf("${fw2}Hydrating", "container1000Water"), null, { water })
+    val fairyWandMelting = m.fw(2, ::ItemFairyWandMelting, "melting_fairy_wand", "${fw}Melting", listOf("${fw2}Melting"), null, { flame })
+    val fairyWandMelting2 = m.fw(3, ::ItemFairyWandMelting, "melting_fairy_wand_2", "${fw}Melting2", listOf("${fw2}Melting"), null, { flame })
+    val fairyWandMelting3 = m.fw(4, ::ItemFairyWandMelting, "melting_fairy_wand_3", "${fw}Melting3", listOf("${fw2}Melting"), null, { flame })
+    val fairyWandBreaking = m.fw(2, ::ItemFairyWandBreaking, "breaking_fairy_wand", "${fw}Breaking", listOf("${fw2}Breaking"), null, { breaking })
+    val fairyWandBreaking2 = m.fw(3, ::ItemFairyWandBreaking, "breaking_fairy_wand_2", "${fw}Breaking2", listOf("${fw2}Breaking"), null, { breaking })
+    val fairyWandBreaking3 = m.fw(4, ::ItemFairyWandBreaking, "breaking_fairy_wand_3", "${fw}Breaking3", listOf("${fw2}Breaking"), null, { breaking })
+    val fairyWandFreezing = m.fw(2, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand", "${fw}Freezing", listOf("${fw2}Freezing"), null, { freeze })
+    val fairyWandFreezing2 = m.fw(3, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand_2", "${fw}Freezing2", listOf("${fw2}Freezing"), null, { freeze })
+    val fairyWandFreezing3 = m.fw(4, ::ItemFairyWeaponCraftingTool, "freezing_fairy_wand_3", "${fw}Freezing3", listOf("${fw2}Freezing"), null, { freeze })
+    val fairyWandPolishing = m.fw(3, ::ItemFairyWeaponCraftingTool, "polishing_fairy_wand", "${fw}Polishing", listOf("${fw2}Polishing"), null, { crystal })
+    val fairyWandPolishing2 = m.fw(4, ::ItemFairyWeaponCraftingTool, "polishing_fairy_wand_2", "${fw}Polishing2", listOf("${fw2}Polishing"), null, { crystal })
+    val fairyWandSummoning = m.fw(3, ::ItemFairyWandSummoning, "summoning_fairy_wand", "${fw}Summoning", listOf("${fw2}Summoning"), null, { submission })
+    val fairyWandSummoning2 = m.fw(4, ::ItemFairyWandSummoning, "summoning_fairy_wand_2", "${fw}Summoning2", listOf("${fw2}Summoning"), null, { submission })
 
-    val miragiumSword = m.fw(2, ::ItemFairyWeaponBase, "miragium_sword", listOf("miragiumSword"), null, { attack }, { slash })
-    val crystalSword = m.fw(3, ::ItemCrystalSword, "crystal_sword", listOf("crystalSword"), { miragiumSword }, { crystal })
-    val fairySword = m.fw(3, ::ItemFairySword, "fairy_sword", listOf("fairySword"), { miragiumSword }, { attack })
+    val miragiumSword = m.fw(2, ::ItemFairyWeaponBase, "miragium_sword", "miragiumSword", listOf(), null, { attack }, { slash })
+    val crystalSword = m.fw(3, ::ItemCrystalSword, "crystal_sword", "crystalSword", listOf(), { miragiumSword }, { crystal })
+    val fairySword = m.fw(3, ::ItemFairySword, "fairy_sword", "fairySword", listOf(), { miragiumSword }, { attack })
 }
 
 
