e014015cc08dd35e5820a2ae74d9f5fffcf8cbff
titanium <tacticsrealize2@gmail.com>
2021-11-23 17:53:33 +0900

    [fix] 歪曲のワンドのTier

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
@@ -94,7 +94,7 @@ class FairyWeaponLoader(m: ModInitializer) {
     val fairyWandPolishing2 = m.fw(4, ::ItemFairyWeaponCraftingTool, "polishing_fairy_wand_2", "${fw}Polishing2", listOf("${fw2}Polishing"), null, { crystal })
     val fairyWandSummoning = m.fw(3, ::ItemFairyWandSummoning, "summoning_fairy_wand", "${fw}Summoning", listOf("${fw2}Summoning"), null, { submission })
     val fairyWandSummoning2 = m.fw(4, ::ItemFairyWandSummoning, "summoning_fairy_wand_2", "${fw}Summoning2", listOf("${fw2}Summoning"), null, { submission })
-    val fairyWandDistortion = m.fw(3, ::ItemFairyWeaponCraftingTool, "distortion_fairy_wand", "${fw}Distortion", listOf("${fw2}Distortion"), null, { space })
+    val fairyWandDistortion = m.fw(4, ::ItemFairyWeaponCraftingTool, "distortion_fairy_wand", "${fw}Distortion", listOf("${fw2}Distortion"), null, { space })
 
     val miragiumSword = m.fw(2, ::ItemFairyWeaponBase, "miragium_sword", "miragiumSword", listOf(), null, { attack }, { slash })
     val crystalSword = m.fw(3, ::ItemCrystalSword, "crystal_sword", "crystalSword", listOf(), { miragiumSword }, { crystal })
