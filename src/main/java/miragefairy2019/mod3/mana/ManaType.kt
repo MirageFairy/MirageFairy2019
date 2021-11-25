5fb226e9cf9819f545e768598fcf14672d02584b
titanium <tacticsrealize2@gmail.com>
2021-11-25 21:14:32 +0900

    [refactor] EnumManaType

--- src/main/java/miragefairy2019/mod3/mana/ManaType.kt
+++ src/main/java/miragefairy2019/mod3/mana/ManaType.kt
@@ -3,5 +3,29 @@ package miragefairy2019.mod3.mana
 import miragefairy2019.libkt.buildText
 import miragefairy2019.libkt.color
 import miragefairy2019.mod3.mana.api.EnumManaType
+import net.minecraft.util.text.TextFormatting
+
+/**
+ * @return すべて小文字
+ */
+fun EnumManaType.getName() = name.toLowerCase()
 
 val EnumManaType.displayName get() = buildText { translate("mirageFairy2019.mana.$name.name").color(textColor) }
+
+fun EnumManaType.getColor() = when (this) {
+    EnumManaType.shine -> 0x007068
+    EnumManaType.fire -> 0xFF0000
+    EnumManaType.wind -> 0x107A00
+    EnumManaType.gaia -> 0x664E00
+    EnumManaType.aqua -> 0x3535FF
+    EnumManaType.dark -> 0xB000D3
+}
+
+fun EnumManaType.getTextColor() = when (this) {
+    EnumManaType.shine -> TextFormatting.WHITE
+    EnumManaType.fire -> TextFormatting.RED
+    EnumManaType.wind -> TextFormatting.GREEN
+    EnumManaType.gaia -> TextFormatting.YELLOW
+    EnumManaType.aqua -> TextFormatting.BLUE
+    EnumManaType.dark -> TextFormatting.DARK_GRAY
+}
--- src/main/java/miragefairy2019/mod3/mana/api/EnumManaType.java
+++ src/main/java/miragefairy2019/mod3/mana/api/EnumManaType.java
@@ -1,5 +1,6 @@
 package miragefairy2019.mod3.mana.api;
 
+import miragefairy2019.mod3.mana.ManaTypeKt;
 import net.minecraft.util.text.TextFormatting;
 
 public enum EnumManaType {
@@ -19,41 +20,11 @@ public enum EnumManaType {
     }
 
     public int getColor() {
-        switch (this) {
-            case shine:
-                return 0x007068;
-            case fire:
-                return 0xFF0000;
-            case wind:
-                return 0x107A00;
-            case gaia:
-                return 0x664E00;
-            case aqua:
-                return 0x3535FF;
-            case dark:
-                return 0xB000D3;
-            default:
-                throw null;
-        }
+        return ManaTypeKt.getColor(this);
     }
 
     public TextFormatting getTextColor() {
-        switch (this) {
-            case shine:
-                return TextFormatting.WHITE;
-            case fire:
-                return TextFormatting.RED;
-            case wind:
-                return TextFormatting.GREEN;
-            case gaia:
-                return TextFormatting.YELLOW;
-            case aqua:
-                return TextFormatting.BLUE;
-            case dark:
-                return TextFormatting.DARK_GRAY;
-            default:
-                throw null;
-        }
+        return ManaTypeKt.getTextColor(this);
     }
 
 }
