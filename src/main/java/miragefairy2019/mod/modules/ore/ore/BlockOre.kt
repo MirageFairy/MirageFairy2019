e1b89244f3ca3d5b0a19c48bf7acba0ec2519d69
titanium <tacticsrealize2@gmail.com>
2021-12-01 03:22:59 +0900

    [refactor] kotlin: BlockOre, ItemBlockOre

---
+++ src/main/java/miragefairy2019/mod/modules/ore/ore/BlockOre.kt
@@ -0,0 +1,53 @@
+package miragefairy2019.mod.modules.ore.ore
+
+import miragefairy2019.mod.lib.multi.BlockMulti
+import miragefairy2019.mod.lib.multi.IBlockVariantList
+import net.minecraft.block.SoundType
+import net.minecraft.block.material.Material
+import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.NonNullList
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.Explosion
+import net.minecraft.world.IBlockAccess
+import net.minecraft.world.World
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import java.util.Random
+
+class BlockOre<V : IBlockVariantOre>(variantList: IBlockVariantList<V>) : BlockMulti<V>(Material.ROCK, variantList) {
+    init {
+
+        // style
+        soundType = SoundType.STONE
+
+        // 挙動
+        setHardness(3.0f)
+        setResistance(5.0f)
+        variantList.blockVariants.forEach { setHarvestLevel(it!!.harvestTool, it.harvestLevel, getState(it)) }
+
+    }
+
+
+    override fun getBlockHardness(blockState: IBlockState, worldIn: World, pos: BlockPos) = getVariant(blockState)!!.hardness
+    override fun getExplosionResistance(world: World, pos: BlockPos, exploder: Entity?, explosion: Explosion) = getVariant(world.getBlockState(pos))!!.resistance
+
+
+    override fun getDrops(drops: NonNullList<ItemStack>, world: IBlockAccess, pos: BlockPos, state: IBlockState, fortune: Int) {
+        val random = if (world is World) world.rand else RANDOM
+        getVariant(state)!!.getDrops(drops, random, this, getMetaFromState(state), fortune)
+    }
+
+    override fun canSilkHarvest(world: World, pos: BlockPos, state: IBlockState, player: EntityPlayer) = true
+    override fun getExpDrop(state: IBlockState, world: IBlockAccess, pos: BlockPos, fortune: Int): Int {
+        val random = if (world is World) world.rand else Random()
+        return getVariant(state)!!.getExpDrop(random, fortune)
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    override fun getBlockLayer() = BlockRenderLayer.CUTOUT_MIPPED
+}
