c89b630e60d7401f34c7bd261afbca409ae27be2
titanium <tacticsrealize2@gmail.com>
2021-12-01 02:44:30 +0900

    [refactor] kotlin: EnumVariantOre{1..2}

---
+++ src/main/java/miragefairy2019/mod/modules/ore/ore/EnumVariantOre1.kt
@@ -0,0 +1,76 @@
+package miragefairy2019.mod.modules.ore.ore
+
+import miragefairy2019.libkt.randomInt
+import miragefairy2019.mod.lib.UtilsMinecraft
+import miragefairy2019.mod.lib.multi.IListBlockVariant
+import net.minecraft.block.Block
+import net.minecraft.item.ItemStack
+import net.minecraft.util.IStringSerializable
+import net.minecraft.util.NonNullList
+import net.minecraft.util.math.MathHelper
+import java.util.Random
+
+enum class EnumVariantOre1(
+    @JvmField val metadata: Int,
+    @JvmField val resourceName: String,
+    @JvmField val unlocalizedName: String,
+    @JvmField val hardness: Float,
+    @JvmField val resistance: Float,
+    @JvmField val harvestLevel: Int,
+    private val gemProvider: GemProvider
+) : IStringSerializable, IBlockVariantOre {
+    APATITE_ORE(0, "apatite_ore", "oreApatite", 3f, 5f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemApatite") }, 1.0, 1.5, 1, 3)),
+    FLUORITE_ORE(1, "fluorite_ore", "oreFluorite", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemFluorite") }, 1.0, 1.0, 15, 30)),
+    SULFUR_ORE(2, "sulfur_ore", "oreSulfur", 3f, 5f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemSulfur") }, 1.0, 1.5, 1, 3)),
+    CINNABAR_ORE(3, "cinnabar_ore", "oreCinnabar", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemCinnabar") }, 1.0, 1.0, 1, 3)),
+    MOONSTONE_ORE(4, "moonstone_ore", "oreMoonstone", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemMoonstone") }, 1.0, 0.5, 20, 40)),
+    MAGNETITE_ORE(5, "magnetite_ore", "oreMagnetite", 3f, 5f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemMagnetite") }, 1.0, 2.0, 1, 2)),
+
+    PYROPE_ORE(6, "pyrope_ore", "orePyrope", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemPyrope") }, 1.0, 0.5, 1, 5)),
+    SMITHSONITE_ORE(7, "smithsonite_ore", "oreSmithsonite", 3f, 5f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemSmithsonite") }, 1.0, 1.0, 1, 3)),
+
+    NETHERRACK_APATITE_ORE(8, "netherrack_apatite_ore", "oreApatite", 0.4f, 0.4f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemApatite") }, 1.0, 1.5, 1, 3)),
+    NETHERRACK_FLUORITE_ORE(9, "netherrack_fluorite_ore", "oreFluorite", 0.4f, 0.4f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemFluorite") }, 1.0, 1.0, 15, 30)),
+    NETHERRACK_SULFUR_ORE(10, "netherrack_sulfur_ore", "oreSulfur", 0.4f, 0.4f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemSulfur") }, 1.0, 1.5, 1, 3)),
+    NETHERRACK_CINNABAR_ORE(11, "netherrack_cinnabar_ore", "oreCinnabar", 0.4f, 0.4f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemCinnabar") }, 1.0, 1.0, 1, 3)),
+    NETHERRACK_MOONSTONE_ORE(12, "netherrack_moonstone_ore", "oreMoonstone", 0.4f, 0.4f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemMoonstone") }, 1.0, 0.5, 20, 40)),
+    NETHERRACK_MAGNETITE_ORE(13, "netherrack_magnetite_ore", "oreMagnetite", 0.4f, 0.4f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemMagnetite") }, 1.0, 2.0, 1, 2)),
+
+    NEPHRITE_ORE(14, "nephrite_ore", "oreNephrite", 3f, 5f, 1, GemProvider({ UtilsMinecraft.getItemStack("gemNephrite") }, 1.0, 2.0, 1, 3)),
+    TOPAZ_ORE(15, "topaz_ore", "oreTopaz", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemTopaz") }, 1.0, 0.5, 1, 5)),
+    ;
+
+    class GemProvider(val itemStackSupplier: () -> ItemStack, val amount: Double, val amountPerFortune: Double, val expMin: Int, val expMax: Int)
+
+    override fun toString() = resourceName
+    override fun getName() = resourceName
+    override fun getMetadata() = metadata
+    override fun getResourceName() = resourceName
+    override fun getUnlocalizedName() = unlocalizedName
+    override fun getHardness() = hardness
+    override fun getResistance() = resistance
+    override fun getHarvestLevel() = harvestLevel
+
+    override fun getDrops(drops: NonNullList<ItemStack>, random: Random, block: Block, metadata: Int, fortune: Int) {
+        repeat(random.randomInt(gemProvider.amount + random.nextDouble() * gemProvider.amountPerFortune * fortune)) {
+            drops += gemProvider.itemStackSupplier().copy()
+        }
+    }
+
+    override fun getExpDrop(random: Random, fortune: Int) = MathHelper.getInt(random, gemProvider.expMin, gemProvider.expMax)
+
+    companion object {
+        val variantList: IListBlockVariant<EnumVariantOre1> = object : IListBlockVariant<EnumVariantOre1> {
+            private val values = values().toList()
+            private val metaLookup = mutableMapOf<Int, EnumVariantOre1>().also { map ->
+                values().forEach { variant ->
+                    require(!map.containsKey(variant.metadata))
+                    map[variant.metadata] = variant
+                }
+            }
+
+            override fun byMetadata(metadata: Int): EnumVariantOre1 = metaLookup[metadata] ?: values[0]
+            override fun iterator(): MutableIterator<EnumVariantOre1> = values.toMutableList().iterator() // TODO
+        }
+    }
+}
---
+++ src/main/java/miragefairy2019/mod/modules/ore/ore/EnumVariantOre2.kt
@@ -0,0 +1,60 @@
+package miragefairy2019.mod.modules.ore.ore
+
+import miragefairy2019.libkt.randomInt
+import miragefairy2019.mod.lib.UtilsMinecraft
+import miragefairy2019.mod.lib.multi.IListBlockVariant
+import net.minecraft.block.Block
+import net.minecraft.item.ItemStack
+import net.minecraft.util.IStringSerializable
+import net.minecraft.util.NonNullList
+import net.minecraft.util.math.MathHelper
+import java.util.Random
+
+enum class EnumVariantOre2(
+    @JvmField val metadata: Int,
+    @JvmField val resourceName: String,
+    @JvmField val unlocalizedName: String,
+    @JvmField val hardness: Float,
+    @JvmField val resistance: Float,
+    @JvmField val harvestLevel: Int,
+    private val gemProvider: GemProvider
+) : IStringSerializable, IBlockVariantOre {
+    TOURMALINE_ORE(0, "tourmaline_ore", "oreTourmaline", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemTourmaline") }, 1.0, 0.5, 1, 5)),
+    HELIOLITE_ORE(1, "heliolite_ore", "oreHeliolite", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemHeliolite") }, 1.0, 0.5, 10, 20)),
+    END_STONE_LABRADORITE_ORE(2, "end_stone_labradorite_ore", "oreLabradorite", 3f, 5f, 2, GemProvider({ UtilsMinecraft.getItemStack("gemLabradorite") }, 1.0, 0.5, 15, 30)),
+    ;
+
+    class GemProvider(val itemStackSupplier: () -> ItemStack, val amount: Double, val amountPerFortune: Double, val expMin: Int, val expMax: Int)
+
+    override fun toString() = resourceName
+    override fun getName() = resourceName
+    override fun getMetadata() = metadata
+    override fun getResourceName() = resourceName
+    override fun getUnlocalizedName() = unlocalizedName
+    override fun getHardness() = hardness
+    override fun getResistance() = resistance
+    override fun getHarvestLevel() = harvestLevel
+
+    override fun getDrops(drops: NonNullList<ItemStack>, random: Random, block: Block, metadata: Int, fortune: Int) {
+        repeat(random.randomInt(gemProvider.amount + random.nextDouble() * gemProvider.amountPerFortune * fortune)) {
+            drops += gemProvider.itemStackSupplier().copy()
+        }
+    }
+
+    override fun getExpDrop(random: Random, fortune: Int) = MathHelper.getInt(random, gemProvider.expMin, gemProvider.expMax)
+
+    companion object {
+        val variantList: IListBlockVariant<EnumVariantOre2> = object : IListBlockVariant<EnumVariantOre2> {
+            private val values = values().toList()
+            private val metaLookup = mutableMapOf<Int, EnumVariantOre2>().also { map ->
+                values().forEach { variant ->
+                    require(!map.containsKey(variant.metadata))
+                    map[variant.metadata] = variant
+                }
+            }
+
+            override fun byMetadata(metadata: Int): EnumVariantOre2 = metaLookup[metadata] ?: values[0]
+            override fun iterator(): MutableIterator<EnumVariantOre2> = values.toMutableList().iterator() // TODO
+        }
+    }
+}
