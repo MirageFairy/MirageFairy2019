377f41332b78844380b900ce601ca2be438dc485
titanium <tacticsrealize2@gmail.com>
2021-12-07 10:35:22 +0900

    [refactor] MirageFlower

--- src/main/java/miragefairy2019/mod3/worldgen/MirageFlower.kt
+++ src/main/java/miragefairy2019/mod3/worldgen/MirageFlower.kt
@@ -24,6 +24,8 @@ import miragefairy2019.mod3.worldgen.api.ApiMirageFlower
 import miragefairy2019.modkt.impl.fairy.erg
 import miragefairy2019.modkt.impl.fairy.shineEfficiency
 import mirrg.boron.util.UtilsMath
+import net.minecraft.advancements.CriteriaTriggers
+import net.minecraft.block.Block
 import net.minecraft.block.BlockBush
 import net.minecraft.block.BlockFarmland
 import net.minecraft.block.IGrowable
@@ -34,9 +36,12 @@ import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.init.Blocks
 import net.minecraft.init.Enchantments
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumActionResult
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.NonNullList
@@ -47,6 +52,8 @@ import net.minecraft.world.EnumSkyBlock
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import net.minecraftforge.common.BiomeDictionary
+import net.minecraftforge.common.EnumPlantType
+import net.minecraftforge.common.IPlantable
 import java.util.Random
 import java.util.function.Supplier
 
@@ -57,7 +64,7 @@ object MirageFlower {
         blockMirageFlower = block({ BlockMirageFlower() }, "mirage_flower") {
             setUnlocalizedName("mirageFlower")
             setCreativeTab { ApiMain.creativeTab }
-            ApiMirageFlower.pickHandlerRegistry.register(block, block.pickHandler);
+            ApiMirageFlower.pickHandlerRegistry.register(block, block.pickHandler)
         }
         itemMirageFlowerSeeds = item({ ItemMirageFlowerSeeds(blockMirageFlower.get()) }, "mirage_flower_seeds") {
             setUnlocalizedName("mirageFlowerSeeds")
@@ -258,7 +265,7 @@ class BlockMirageFlower : BlockBush(Material.PLANTS), IGrowable {  // Solidで�
     }
 
 
-    // Pickable関連
+    // PickHandler関連
     override fun onBlockActivated(worldIn: World, pos: BlockPos, state: IBlockState, playerIn: EntityPlayer, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): Boolean {
         var itemStack = playerIn.heldItemMainhand
         itemStack = if (itemStack.isEmpty) ItemStack.EMPTY else itemStack.copy()
@@ -301,4 +308,24 @@ class BlockMirageFlower : BlockBush(Material.PLANTS), IGrowable {  // Solidで�
         private val AABB_STAGE2 = AxisAlignedBB(2 / 16.0, 0 / 16.0, 2 / 16.0, 14 / 16.0, 16 / 16.0, 14 / 16.0)
         private val AABB_STAGE3 = AxisAlignedBB(2 / 16.0, 0 / 16.0, 2 / 16.0, 14 / 16.0, 16 / 16.0, 14 / 16.0)
     }
+}
+
+class ItemMirageFlowerSeeds<T>(private val block: T) : Item(), IPlantable where T : Block, T : IPlantable {
+    // 使われるとその場に植物を設置する。
+    override fun onItemUse(player: EntityPlayer, world: World, pos: BlockPos, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
+        val itemStack = player.getHeldItem(hand)
+        val blockState = world.getBlockState(pos)
+        if (facing != EnumFacing.UP) return EnumActionResult.FAIL // ブロックの上面にのみ使用可能
+        if (!player.canPlayerEdit(pos.offset(facing), facing, itemStack)) return EnumActionResult.FAIL // プレイヤーが編集不可能な場合は失敗
+        if (!blockState.block.canSustainPlant(blockState, world, pos, EnumFacing.UP, block)) return EnumActionResult.FAIL // ブロックがその場所に滞在できないとだめ
+        if (!world.isAirBlock(pos.up())) return EnumActionResult.FAIL // 真上が空気出ないとだめ
+
+        world.setBlockState(pos.up(), getPlant(world, pos))
+        if (player is EntityPlayerMP) CriteriaTriggers.PLACED_BLOCK.trigger(player, pos.up(), itemStack)
+        itemStack.shrink(1)
+        return EnumActionResult.SUCCESS
+    }
+
+    override fun getPlantType(world: IBlockAccess, pos: BlockPos) = EnumPlantType.Plains // 常に草の上に蒔ける
+    override fun getPlant(world: IBlockAccess, pos: BlockPos): IBlockState = MirageFlower.blockMirageFlower.get().defaultState // 常にAge0のミラ花を与える
 }
