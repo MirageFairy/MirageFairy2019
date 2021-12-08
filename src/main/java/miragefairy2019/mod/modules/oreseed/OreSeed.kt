ef2bc06b24eccb8c72de2b584965f2b1e50dc677
titanium <tacticsrealize2@gmail.com>
2021-12-09 04:40:38 +0900

    [refactor] OreSeed: blockstates

--- src/main/java/miragefairy2019/mod/modules/oreseed/OreSeed.kt
+++ src/main/java/miragefairy2019/mod/modules/oreseed/OreSeed.kt
@@ -1,7 +1,10 @@
 package miragefairy2019.mod.modules.oreseed
 
+import miragefairy2019.libkt.DataBlockState
+import miragefairy2019.libkt.DataBlockStates
 import miragefairy2019.libkt.Module
 import miragefairy2019.libkt.block
+import miragefairy2019.libkt.makeBlockStates
 import miragefairy2019.libkt.setCreativeTab
 import miragefairy2019.mod3.main.api.ApiMain
 import miragefairy2019.mod3.oreseeddrop.api.EnumOreSeedType
@@ -15,16 +18,19 @@ object OreSeed {
         // 鉱石の種
         blockOreSeed = block({ BlockOreSeed(EnumOreSeedType.STONE) }, "ore_seed") {
             setCreativeTab { ApiMain.creativeTab }
+            makeBlockStates { DataBlockStates(EnumVariantOreSeed.values().associate { "variant=$it" to DataBlockState("minecraft:stone") }) }
         }
 
         // 鉱石の種：ネザー
         blockOreSeedNether = block({ BlockOreSeed(EnumOreSeedType.NETHERRACK) }, "ore_seed_nether") {
             setCreativeTab { ApiMain.creativeTab }
+            makeBlockStates { DataBlockStates(EnumVariantOreSeed.values().associate { "variant=$it" to DataBlockState("minecraft:netherrack") }) }
         }
 
         // 鉱石の種：エンド
         blockOreSeedEnd = block({ BlockOreSeed(EnumOreSeedType.END_STONE) }, "ore_seed_end") {
             setCreativeTab { ApiMain.creativeTab }
+            makeBlockStates { DataBlockStates(EnumVariantOreSeed.values().associate { "variant=$it" to DataBlockState("minecraft:end_stone") }) }
         }
 
     }
