497f4247d6d67416649533c0f22127a3878c543b
titanium <tacticsrealize2@gmail.com>
2021-12-09 04:38:25 +0900

    [internal] ResourceMaker: インデントをスペース2個に

--- src/main/java/miragefairy2019/libkt/ResourceMaker.kt
+++ src/main/java/miragefairy2019/libkt/ResourceMaker.kt
@@ -7,9 +7,8 @@ import net.minecraft.util.ResourceLocation
 import java.io.File
 
 class ResourceMaker(val dirBase: File) {
-    private val String.replaceIndents get() = this.replace("""^( +)""".toRegex(RegexOption.MULTILINE)) { "\t".repeat(it.groups[1]!!.value.length / 2) }
     private val String.replaceSymbols get() = this.replace("""\\u003d""".toRegex(), "=")
-    private val String.format get() = this.replaceIndents.replaceSymbols + "\n"
+    private val String.format get() = this.replaceSymbols + "\n"
     private val Any.json get() = GsonBuilder().setPrettyPrinting().create().toJson(this).format
     fun File.place(data: Any) = apply { parentFile.mkdirs() }.writeText(data.json)
 }
