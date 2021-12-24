d030a4a7676b7a7117a7c05290a3013df26203a1
titanium <tacticsrealize2@gmail.com>
2021-12-24 09:49:24 +0900

    [internal] add: FairyTree

---
+++ src/main/java/miragefairy2019/mod3/artifacts/treecompile/TreeCompileException.kt
@@ -0,0 +1,5 @@
+package miragefairy2019.mod3.artifacts.treecompile
+
+import net.minecraft.util.text.ITextComponent
+
+open class TreeCompileException(val description: ITextComponent) : Exception()
---
+++ src/test/java/miragefairy2019/mod3/artifacts/FairyTreeTest.kt
@@ -0,0 +1,24 @@
+package miragefairy2019.mod3.artifacts
+
+import java.util.Random
+
+fun main() {
+    var min = 9999
+    var max = 0
+    var sum = 0L
+    var count = 0
+
+    repeat(10000000) {
+        val interval = randomSkipTicks(Random(), 1 / 200.0) + 1
+        if (interval < min) min = interval
+        if (interval > max) max = interval
+        sum += interval
+        count++
+    }
+
+    println("$min <- 1であればいい")
+    println("$max <- 3000くらいになるっぽい（いくらでもいい）")
+    println("$sum <- 2000000000くらい")
+    println("$count <- 10000000固定")
+    println("${sum / count.toDouble()} <- 非常に200に近い")
+}
