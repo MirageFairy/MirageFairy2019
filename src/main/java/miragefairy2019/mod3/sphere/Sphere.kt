1c359c42b8a66ce51d0e242413caae4bbceaba8a
titanium <tacticsrealize2@gmail.com>
2022-01-12 22:21:47 +0900

    close #278

--- src/main/java/miragefairy2019/mod3/sphere/Sphere.kt
+++ src/main/java/miragefairy2019/mod3/sphere/Sphere.kt
@@ -88,7 +88,7 @@ fun getSphereType(ergType: EnumErgType): SphereType {
         EnumErgType.FREEZE -> SphereType(EnumErgType.FREEZE, 0x5AFFFF, 0xFFFFFF, 0xF6FFFF, 0xACFFFF, ore("ice"), null)
         EnumErgType.THUNDER -> SphereType(EnumErgType.THUNDER, 0xFFFFB2, 0x359C00, 0xC370A7, 0xFFFF00, item { Items.GOLDEN_SWORD }, ore("gemTourmaline"))
         EnumErgType.LEVITATE -> SphereType(EnumErgType.LEVITATE, 0x00A2FF, 0xB7ECFF, 0x35366B, 0x8CD0FF, item { Items.FEATHER }, ore("gemLabradorite"))
-        EnumErgType.SENSE -> SphereType(EnumErgType.SENSE, 0x1B3211, 0xD3E6DF, 0x7ACF45, 0x4784A0, item { Items.ENDER_EYE }, null)
+        EnumErgType.SENSE -> SphereType(EnumErgType.SENSE, 0x1B3211, 0xD3E6DF, 0x7ACF45, 0x4784A0, item { Items.SPIDER_EYE }, null)
     }
 }
 
