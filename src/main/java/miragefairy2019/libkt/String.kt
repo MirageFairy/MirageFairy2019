d6c0f10842b77e31d07468bab7f9ee238ce5dbe3
titanium <tacticsrealize2@gmail.com>
2022-01-13 21:04:17 +0900

    [internal] String

--- src/main/java/miragefairy2019/libkt/String.kt
+++ src/main/java/miragefairy2019/libkt/String.kt
@@ -8,3 +8,18 @@ infix fun Short.with(format: String) = String.format(format, this)
 infix fun Byte.with(format: String) = String.format(format, this)
 infix fun Boolean.with(format: String) = String.format(format, this)
 infix fun String.with(format: String) = String.format(format, this)
+
+val String.upperCase get() = toUpperCase()
+val String.lowerCase get() = toLowerCase()
+
+/** 先頭の文字のみを大文字にします。 */
+val String.upperCaseHead get() = if (isEmpty()) this else take(1).upperCase + drop(1)
+
+/** 先頭の文字のみを小文字にします。 */
+val String.lowerCaseHead get() = if (isEmpty()) this else take(1).lowerCase + drop(1)
+
+/** @receiver スネークケースの文字列 */
+val String.upperCamelCase get() = split('_').joinToString("") { it.upperCaseHead }
+
+/** @receiver スネークケースの文字列 */
+val String.lowerCamelCase get() = upperCamelCase.lowerCaseHead
