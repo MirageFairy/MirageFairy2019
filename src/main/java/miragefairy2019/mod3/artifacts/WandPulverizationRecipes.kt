7976ccc7e5da094dec6678266a487fbe102e0829
titanium <tacticsrealize2@gmail.com>
2022-01-20 10:42:54 +0900

    [cleanup] WandPulverizationRecipes

--- src/main/java/miragefairy2019/mod3/artifacts/WandPulverizationRecipes.kt
+++ src/main/java/miragefairy2019/mod3/artifacts/WandPulverizationRecipes.kt
@@ -1,13 +1,15 @@
 package miragefairy2019.mod3.artifacts
 
 import miragefairy2019.libkt.Module
+import miragefairy2019.libkt.createItemStack
+import miragefairy2019.libkt.ingredient
 import miragefairy2019.libkt.oreIngredient
 import miragefairy2019.mod.ModMirageFairy2019
 import miragefairy2019.mod.lib.OreIngredientComplex
+import net.minecraft.init.Items
 import net.minecraft.item.crafting.Ingredient
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.registry.GameRegistry
-import net.minecraftforge.oredict.OreIngredient
 
 object WandPulverizationRecipes {
     val module: Module = {
@@ -25,14 +27,14 @@ object WandPulverizationRecipes {
             }
 
             // 妖精MODによる粉
-            r("miragium_dust", OreIngredient("ingotMiragium"), "dustMiragium")
+            r("miragium_dust", "ingotMiragium".oreIngredient, "dustMiragium")
 
             // 他MODによるバニラ素材の粉
-            r("iron_dust", OreIngredient("ingotIron"), "dustIron")
-            r("gold_dust", OreIngredient("ingotGold"), "dustGold")
-            r("quartz_dust", OreIngredient("gemQuartz"), "dustQuartz")
-            r("wheat_dust", OreIngredient("cropWheat"), "dustWheat")
-            r("ender_pearl_dust", OreIngredient("enderpearl"), "dustEnderPearl")
+            r("iron_dust", "ingotIron".oreIngredient, "dustIron")
+            r("gold_dust", "ingotGold".oreIngredient, "dustGold")
+            r("quartz_dust", "gemQuartz".oreIngredient, "dustQuartz")
+            r("wheat_dust", "cropWheat".oreIngredient, "dustWheat")
+            r("ender_pearl_dust", "enderpearl".oreIngredient, "dustEnderPearl")
 
         }
     }
