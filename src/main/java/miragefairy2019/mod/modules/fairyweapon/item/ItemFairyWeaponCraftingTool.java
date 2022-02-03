c0ce8af64443533c1b3561cf5bb92a7c27b487b7
titanium <tacticsrealize2@gmail.com>
2022-02-03 11:01:34 +0900

    _

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWeaponCraftingTool.java
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWeaponCraftingTool.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 public class ItemFairyWeaponCraftingTool extends ItemFairyWeaponBase {
 
