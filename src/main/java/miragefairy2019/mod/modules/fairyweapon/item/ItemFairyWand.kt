4f63287a60e82bbf12577dd5b4b6f1e8ad0a59f1
titanium <tacticsrealize2@gmail.com>
2022-02-03 12:21:42 +0900

    [refactor] ItemFairyWand: kotlin

---
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/ItemFairyWand.kt
@@ -0,0 +1,68 @@
+package miragefairy2019.mod.modules.fairyweapon.item
+
+import miragefairy2019.mod3.fairystickcraft.api.ApiFairyStickCraft
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumActionResult
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.RayTraceResult
+import net.minecraft.world.World
+
+open class ItemFairyWand : ItemFairyWeaponBase() {
+    // フェアリーステッキクラフト関係
+
+    override fun onItemUse(player: EntityPlayer, worldIn: World, pos: BlockPos, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
+
+        // フェアリーステッキクラフトのレシピを判定
+        val executor = null
+            ?: ApiFairyStickCraft.fairyStickCraftRegistry.getExecutor(player, worldIn, pos) { player.getHeldItem(hand) }
+            ?: ApiFairyStickCraft.fairyStickCraftRegistry.getExecutor(player, worldIn, pos.offset(facing)) { player.getHeldItem(hand) }
+            ?: return EnumActionResult.PASS // マッチするレシピが無かった場合は抜ける
+
+        // クラフトを実行
+        executor.onCraft { player.setHeldItem(hand, it) }
+
+        return EnumActionResult.SUCCESS
+    }
+
+    override fun onUpdate(itemStack: ItemStack, world: World, entity: Entity, itemSlot: Int, isSelected: Boolean) {
+        if (!world.isRemote) return // クライアント側のみ描画処理を行う
+        if (!(world.rand.nextDouble() < 0.1)) return // 10Tickに1回の確率で描画を行う
+        if (entity !is EntityPlayer) return // 主体はプレイヤーでなければならない
+        if (!isSelected && entity.heldItemOffhand != itemStack) return // アイテムが異常な状態なら中止
+
+        // プレイヤー視線判定
+        val rayTraceResult: RayTraceResult? = rayTrace(world, entity, false)
+        if (rayTraceResult == null || rayTraceResult.typeOfHit != RayTraceResult.Type.BLOCK) return  // 視線がブロックに当たらなかった場合は無視
+
+        // レシピ判定
+        val executor = null
+            ?: ApiFairyStickCraft.fairyStickCraftRegistry.getExecutor(entity, world, rayTraceResult.blockPos) { itemStack }
+            ?: ApiFairyStickCraft.fairyStickCraftRegistry.getExecutor(entity, world, rayTraceResult.blockPos.offset(rayTraceResult.sideHit)) { itemStack }
+            ?: return // マッチするレシピが無かった場合は抜ける
+
+        // 描画を実行
+        executor.onUpdate()
+
+    }
+
+
+    // クラフティングツール関係
+    override fun getContainerItem(itemStack: ItemStack): ItemStack {
+        if (itemStack.itemDamage >= itemStack.maxDamage) return super.getContainerItem(itemStack) // 耐久を使い果たした場合はデフォルトの挙動
+        return itemStack.copy().also { it.itemDamage = it.itemDamage + 1 }
+    }
+
+
+    // 挙動
+
+    override fun canManualRepair(itemStack: ItemStack) = false // 手修理負荷
+
+    // 妖精搭乗不可
+    override fun getMirageFairyCombiningHandler() = object : FairyCombiningHandler() {
+        override fun canCombine(itemStack: ItemStack) = false
+    }
+}
