45281611ffb4db3cdade7436cb2f4a4d5cb4210e
titanium <tacticsrealize2@gmail.com>
2022-02-05 08:41:10 +0900

    [internal] Util: add

--- src/main/java/miragefairy2019/libkt/Util.kt
+++ src/main/java/miragefairy2019/libkt/Util.kt
@@ -1,5 +1,14 @@
 package miragefairy2019.libkt
 
+import java.io.File
 import java.util.Optional
 
+@Suppress("unused")
+fun Any?.toUnit() = Unit
+
 val <T : Any> Optional<T>.orNull: T? get() = orElse(null)
+
+val File.existsOrNull get() = if (exists()) this else null
+fun File.mkdirsParent() = canonicalFile.parentFile.mkdirs()
+
+fun <K, V : Any> MutableMap<K, V>.setOrRemove(key: K, value: V?): Unit = if (value != null) set(key, value) else remove(key).toUnit()
