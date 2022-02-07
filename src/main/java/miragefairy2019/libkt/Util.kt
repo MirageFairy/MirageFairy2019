6fe43faf85edb265a0a278093e9f3b1bf20bcc2c
titanium <tacticsrealize2@gmail.com>
2022-02-08 00:04:00 +0900

    [internal] add: nextint(IntRange)

--- src/main/java/miragefairy2019/libkt/Util.kt
+++ src/main/java/miragefairy2019/libkt/Util.kt
@@ -2,6 +2,7 @@ package miragefairy2019.libkt
 
 import java.io.File
 import java.util.Optional
+import java.util.Random
 
 @Suppress("unused")
 fun Any?.toUnit() = Unit
@@ -12,3 +13,5 @@ val File.existsOrNull get() = if (exists()) this else null
 fun File.mkdirsParent() = canonicalFile.parentFile.mkdirs()
 
 fun <K, V : Any> MutableMap<K, V>.setOrRemove(key: K, value: V?): Unit = if (value != null) set(key, value) else remove(key).toUnit()
+
+fun Random.nextInt(range: IntRange) = range.first + nextInt(range.last - range.first + 1)
