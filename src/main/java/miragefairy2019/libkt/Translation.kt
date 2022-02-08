ca6f89de47704f8a09d47c6ef9310282908683de
titanium <tacticsrealize2@gmail.com>
2022-02-08 12:27:11 +0900

    _

--- src/main/java/miragefairy2019/libkt/Translation.kt
+++ src/main/java/miragefairy2019/libkt/Translation.kt
@@ -1,20 +1,17 @@
 package miragefairy2019.libkt
 
 import mirrg.kotlin.formatAs
-import net.minecraft.util.text.ITextComponent
 import java.time.Duration
 
-val Duration.displayText: ITextComponent
-    get() {
-        return textComponent {
-            val prefix = "miragefairy2019.gui.duration"
-            val millis = toMillis()
-            when {
-                millis >= 1000 * 60 * 60 * 24 -> !((millis / (1000 * 60 * 60 * 24).toDouble()) formatAs "%.2f") + translate("$prefix.days")
-                millis >= 1000 * 60 * 60 -> !((millis / (1000 * 60 * 60).toDouble()) formatAs "%.2f") + translate("$prefix.hours")
-                millis >= 1000 * 60 -> !((millis / (1000 * 60).toDouble()) formatAs "%.2f") + translate("$prefix.minutes")
-                millis >= 1000 -> !((millis / (1000).toDouble()) formatAs "%.2f") + translate("$prefix.seconds")
-                else -> !(millis.toDouble() formatAs "%.2f") + translate("$prefix.milliSeconds")
-            }
+val Duration.displayText
+    get() = textComponent {
+        fun unit(unitName: String) = translate("miragefairy2019.gui.duration.$unitName")
+        val millis = toMillis()
+        when {
+            millis >= 1000 * 60 * 60 * 24 -> !((millis / (1000 * 60 * 60 * 24).toDouble()) formatAs "%.2f") + unit("days")
+            millis >= 1000 * 60 * 60 -> !((millis / (1000 * 60 * 60).toDouble()) formatAs "%.2f") + unit("hours")
+            millis >= 1000 * 60 -> !((millis / (1000 * 60).toDouble()) formatAs "%.2f") + unit("minutes")
+            millis >= 1000 -> !((millis / (1000).toDouble()) formatAs "%.2f") + unit("seconds")
+            else -> !(millis.toDouble() formatAs "%.2f") + unit("milliSeconds")
         }
     }
