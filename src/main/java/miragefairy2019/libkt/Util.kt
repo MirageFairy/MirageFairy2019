b7ba13c0379f16871e1598f1273b88d20509883f
titanium <tacticsrealize2@gmail.com>
2022-02-10 20:24:38 +0900

    [internal] add: axisAlignedBB

--- src/main/java/miragefairy2019/libkt/Util.kt
+++ src/main/java/miragefairy2019/libkt/Util.kt
@@ -1,5 +1,7 @@
 package miragefairy2019.libkt
 
+import net.minecraft.util.math.AxisAlignedBB
+import net.minecraft.util.math.Vec3d
 import java.io.File
 import java.util.Optional
 import java.util.Random
@@ -17,3 +19,5 @@ fun File.mkdirsParent() = canonicalFile.parentFile.mkdirs()
 fun <K, V : Any> MutableMap<K, V>.setOrRemove(key: K, value: V?): Unit = if (value != null) set(key, value) else remove(key).toUnit()
 
 fun Random.nextInt(range: IntRange) = range.first + nextInt(range.last - range.first + 1)
+
+fun axisAlignedBB(a: Vec3d, b: Vec3d) = AxisAlignedBB(a.x, a.y, a.z, b.x, b.y, b.z)
