7e38bbe7c2156db3348645aa6fa68f6ac8a7f64d
titanium <tacticsrealize2@gmail.com>
2022-02-19 05:44:10 +0900

    [comment]

--- src/main/java/miragefairy2019/libkt/MinecraftUtils.kt
+++ src/main/java/miragefairy2019/libkt/MinecraftUtils.kt
@@ -7,4 +7,4 @@ import net.minecraftforge.oredict.OreDictionary
 fun canTranslate(key: String): Boolean = I18n.canTranslate(key)
 fun translateToLocal(key: String): String = I18n.translateToLocal(key).trim()
 fun translateToLocalFormatted(key: String, vararg format: Any?): String = I18n.translateToLocalFormatted(key, *format).trim()
-fun getItemStack(oreName: String): ItemStack = OreDictionary.getOres(oreName)[0]
+fun getItemStack(oreName: String): ItemStack = OreDictionary.getOres(oreName)[0] // TODO 例外処理
