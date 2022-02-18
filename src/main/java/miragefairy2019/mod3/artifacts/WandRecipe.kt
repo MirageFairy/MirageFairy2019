78f8c1430a67a08831e1c98d22aa7ccc1212389e
titanium <tacticsrealize2@gmail.com>
2022-02-18 23:58:28 +0900

    [add] 紅蓮のワンドでスポンジを乾かすレシピ

--- src/main/java/miragefairy2019/mod3/artifacts/WandRecipe.kt
+++ src/main/java/miragefairy2019/mod3/artifacts/WandRecipe.kt
@@ -3,6 +3,8 @@ package miragefairy2019.mod3.artifacts
 import miragefairy2019.libkt.DataOreIngredient
 import miragefairy2019.libkt.DataResult
 import miragefairy2019.libkt.DataShapedRecipe
+import miragefairy2019.libkt.DataShapelessRecipe
+import miragefairy2019.libkt.DataSimpleIngredient
 import miragefairy2019.libkt.Module
 import miragefairy2019.libkt.ResourceName
 import miragefairy2019.libkt.makeRecipe
@@ -10,6 +12,8 @@ import miragefairy2019.mod.ModMirageFairy2019
 
 object WandRecipe {
     val module: Module = {
+
+        // 氷8＋氷結のワンド→氷塊
         makeRecipe(
             ResourceName(ModMirageFairy2019.MODID, "packed_ice"),
             DataShapedRecipe(
@@ -27,5 +31,21 @@ object WandRecipe {
                 )
             )
         )
+
+        // 塗れたスポンジ＋紅蓮のワンド→スポンジ
+        makeRecipe(
+            ResourceName(ModMirageFairy2019.MODID, "sponge_from_wet_sponge"),
+            DataShapelessRecipe(
+                ingredients = listOf(
+                    DataSimpleIngredient(item = "minecraft:sponge", data = 1),
+                    DataOreIngredient(type = "miragefairy2019:ore_dict_complex", ore = "mirageFairy2019CraftingToolFairyWandMelting")
+                ),
+                result = DataResult(
+                    item = "minecraft:sponge",
+                    data = 0
+                )
+            )
+        )
+
     }
 }
