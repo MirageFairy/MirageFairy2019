f934f724b595a95e293466b524cdfa40cfab74b9
titanium <tacticsrealize2@gmail.com>
2022-02-19 17:04:28 +0900

    [change] CommonMaterials: 木炭を除く宝石系ブロックの硬度、ツールレベル

--- src/main/java/miragefairy2019/mod3/artifacts/CommonMaterials.kt
+++ src/main/java/miragefairy2019/mod3/artifacts/CommonMaterials.kt
@@ -189,36 +189,46 @@ class BlockMaterials<V : IBlockVariantMaterials>(variantList: BlockVariantList<V
     override fun isBeaconBase(world: IBlockAccess, blockPos: BlockPos, beaconBlockPos: BlockPos) = getVariant(world.getBlockState(blockPos)).isBeaconBase
 }
 
+class HardnessClass(val blockHardness: Float, val harvestTool: String, val harvestLevel: Int) {
+    companion object {
+        val SOFT = HardnessClass(3.0f, "pickaxe", 0) // 硬度3程度、カルサイト級
+        val HARD = HardnessClass(5.0f, "pickaxe", 0) // 硬度5程度、石級
+        val VERY_HARD = HardnessClass(5.0f, "pickaxe", 1) // 硬度7程度、クリスタル級
+        val SUPER_HARD = HardnessClass(5.0f, "pickaxe", 2) // 硬度9程度、ダイヤモンド級
+    }
+}
+
 enum class EnumVariantMaterials1(
     override val metadata: Int,
     override val resourceName: String,
     override val unlocalizedName: String,
     val oreName: String,
-    override val blockHardness: Float,
-    override val harvestTool: String,
-    override val harvestLevel: Int,
+    val hardnessClass: HardnessClass,
     override val burnTime: Int,
     override val soundType: SoundType,
     override val isFallable: Boolean,
     override val material: Material,
     override val isBeaconBase: Boolean
 ) : IStringSerializable, IBlockVariantMaterials {
-    APATITE_BLOCK(0, "apatite_block", "blockApatite", "blockApatite", 3f, "pickaxe", 1, 0, SoundType.STONE, false, Material.IRON, true),
-    FLUORITE_BLOCK(1, "fluorite_block", "blockFluorite", "blockFluorite", 5f, "pickaxe", 2, 0, SoundType.STONE, false, Material.IRON, true),
-    SULFUR_BLOCK(2, "sulfur_block", "blockSulfur", "blockSulfur", 3f, "pickaxe", 1, 0, SoundType.STONE, false, Material.IRON, true),
-    CINNABAR_BLOCK(3, "cinnabar_block", "blockCinnabar", "blockCinnabar", 5f, "pickaxe", 2, 0, SoundType.STONE, false, Material.IRON, true),
-    MOONSTONE_BLOCK(4, "moonstone_block", "blockMoonstone", "blockMoonstone", 7f, "pickaxe", 3, 0, SoundType.STONE, false, Material.IRON, true),
-    MAGNETITE_BLOCK(5, "magnetite_block", "blockMagnetite", "blockMagnetite", 3f, "pickaxe", 1, 0, SoundType.STONE, false, Material.IRON, true),
-    PYROPE_BLOCK(6, "pyrope_block", "blockPyrope", "blockPyrope", 5f, "pickaxe", 2, 0, SoundType.STONE, false, Material.IRON, true),
-    SMITHSONITE_BLOCK(7, "smithsonite_block", "blockSmithsonite", "blockSmithsonite", 3f, "pickaxe", 1, 0, SoundType.STONE, false, Material.IRON, true),
-    CHARCOAL_BLOCK(8, "charcoal_block", "blockCharcoal", "blockCharcoal", 5f, "pickaxe", 0, 20 * 10 * 8 * 9, SoundType.STONE, false, Material.ROCK, false),
-    MIRAGE_FLOWER_LEAF_BLOCK(9, "mirage_flower_leaf_block", "blockLeafMirageFlower", "blockLeafMirageFlower", 2f, "axe", 0, 0, SoundType.GLASS, false, Material.LEAVES, false),
-    MIRAGIUM_INGOT_BLOCK(10, "miragium_ingot_block", "blockMiragium", "blockMiragium", 5f, "pickaxe", 1, 0, SoundType.METAL, false, Material.IRON, false),
-    MIRAGIUM_DUST_BLOCK(11, "miragium_dust_block", "blockDustMiragium", "blockDustMiragium", 0.5f, "shovel", 0, 0, SoundType.SNOW, true, Material.SAND, false),
+    APATITE_BLOCK(0, "apatite_block", "blockApatite", "blockApatite", HardnessClass.HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    FLUORITE_BLOCK(1, "fluorite_block", "blockFluorite", "blockFluorite", HardnessClass.HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    SULFUR_BLOCK(2, "sulfur_block", "blockSulfur", "blockSulfur", HardnessClass.HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    CINNABAR_BLOCK(3, "cinnabar_block", "blockCinnabar", "blockCinnabar", HardnessClass.HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    MOONSTONE_BLOCK(4, "moonstone_block", "blockMoonstone", "blockMoonstone", HardnessClass.VERY_HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    MAGNETITE_BLOCK(5, "magnetite_block", "blockMagnetite", "blockMagnetite", HardnessClass.HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    PYROPE_BLOCK(6, "pyrope_block", "blockPyrope", "blockPyrope", HardnessClass.VERY_HARD, 0, SoundType.STONE, false, Material.IRON, true),
+    SMITHSONITE_BLOCK(7, "smithsonite_block", "blockSmithsonite", "blockSmithsonite", HardnessClass.SOFT, 0, SoundType.STONE, false, Material.IRON, true),
+    CHARCOAL_BLOCK(8, "charcoal_block", "blockCharcoal", "blockCharcoal", HardnessClass(5.0f, "pickaxe", 0), 20 * 10 * 8 * 9, SoundType.STONE, false, Material.ROCK, false),
+    MIRAGE_FLOWER_LEAF_BLOCK(9, "mirage_flower_leaf_block", "blockLeafMirageFlower", "blockLeafMirageFlower", HardnessClass(2.0f, "axe", 0), 0, SoundType.GLASS, false, Material.LEAVES, false),
+    MIRAGIUM_INGOT_BLOCK(10, "miragium_ingot_block", "blockMiragium", "blockMiragium", HardnessClass(5.0f, "pickaxe", 1), 0, SoundType.METAL, false, Material.IRON, false),
+    MIRAGIUM_DUST_BLOCK(11, "miragium_dust_block", "blockDustMiragium", "blockDustMiragium", HardnessClass(0.5f, "shovel", 0), 0, SoundType.SNOW, true, Material.SAND, false),
     ;
 
     override fun toString() = resourceName
     override fun getName() = resourceName
+    override val blockHardness = hardnessClass.blockHardness
+    override val harvestTool = hardnessClass.harvestTool
+    override val harvestLevel = hardnessClass.harvestLevel
 
     companion object {
         val variantList = BlockVariantList(values().toList())
