ed22424bb240696e8a659c4be225f024ad3a1e06
titanium <tacticsrealize2@gmail.com>
2022-02-21 23:52:31 +0900

    Update MirrgStdKotlin.kt

--- src/main/java/mirrg/kotlin/MirrgStdKotlin.kt
+++ src/main/java/mirrg/kotlin/MirrgStdKotlin.kt
@@ -20,6 +20,20 @@ fun <T> Array<T>.join(separator: CharSequence = ", ", transform: (T) -> CharSequ
 fun Sequence<CharSequence>.join(separator: CharSequence = ", ") = joinToString(separator)
 fun <T> Sequence<T>.join(separator: CharSequence = ", ", transform: (T) -> CharSequence) = joinToString(separator, transform = transform)
 
+fun <T> Iterable<T>.separate(separators: Iterable<T>): List<T> {
+    val i = iterator()
+    if (!i.hasNext()) return emptyList()
+    val left = mutableListOf<T>()
+    left += i.next()
+    while (i.hasNext()) {
+        left += separators
+        left += i.next()
+    }
+    return left
+}
+
+fun <T> Iterable<T>.separateWith(vararg separators: T) = separate(separators.asIterable())
+
 fun <T> Iterable<Iterable<T>>.concatWith(separator: Iterable<T>): List<T> {
     val i = iterator()
     if (!i.hasNext()) return emptyList()
@@ -57,3 +71,18 @@ val String.urlEncode: String get() = URLEncoder.encode(this, Charsets.UTF_8.name
 
 infix fun BigDecimal.isSameAs(other: BigDecimal) = (this cmp other) == 0
 infix fun BigDecimal.isNotSameAs(other: BigDecimal) = (this cmp other) != 0
+
+infix fun Byte.atMost(other: Byte) = coerceAtMost(other)
+infix fun Byte.atLeast(other: Byte) = coerceAtLeast(other)
+infix fun Short.atMost(other: Short) = coerceAtMost(other)
+infix fun Short.atLeast(other: Short) = coerceAtLeast(other)
+infix fun Int.atMost(other: Int) = coerceAtMost(other)
+infix fun Int.atLeast(other: Int) = coerceAtLeast(other)
+infix fun Long.atMost(other: Long) = coerceAtMost(other)
+infix fun Long.atLeast(other: Long) = coerceAtLeast(other)
+infix fun Float.atMost(other: Float) = coerceAtMost(other)
+infix fun Float.atLeast(other: Float) = coerceAtLeast(other)
+infix fun Double.atMost(other: Double) = coerceAtMost(other)
+infix fun Double.atLeast(other: Double) = coerceAtLeast(other)
+infix fun <T : Comparable<T>> T.atMost(other: T) = coerceAtMost(other)
+infix fun <T : Comparable<T>> T.atLeast(other: T) = coerceAtLeast(other)
