f6ddea5b448534d452f62e0b1e83619d0568ddc9
titanium <tacticsrealize2@gmail.com>
2022-02-24 10:36:39 +0900

    [refactor] fairyweapon

--- src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
+++ src/main/java/miragefairy2019/mod/modules/fairyweapon/item/Loader.kt
@@ -37,74 +37,69 @@ import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.oredict.OreDictionary
 
-private fun <T : ItemFairyWeapon> ModInitializer.fw(
-    tier: Int,
-    creator: () -> T,
-    registryName: String,
-    unlocalizedName: String,
-    oreNameList: List<String>,
-    parent: (() -> () -> ItemFairyWeapon)?,
-    vararg manualRepairIngredientSuppliers: () -> Ingredient
-) = item(creator, registryName) {
-    setUnlocalizedName(unlocalizedName)
-    setCreativeTab { creativeTab }
-    onRegisterItem {
-        if (side.isClient) {
-            val modelResourceLocation = ModelResourceLocation(item.registryName!!, "normal")
-            MinecraftForge.EVENT_BUS.register(object : Any() {
-                @SubscribeEvent
-                fun accept(event: ModelBakeEvent) {
-                    event.modelRegistry.putObject(modelResourceLocation, BakedModelBuiltinWrapper(event.modelRegistry.getObject(modelResourceLocation)!!))
-                }
-            })
-            ModelLoader.setCustomModelResourceLocation(item, 0, modelResourceLocation)
-        }
-    }
-    onInit {
-        val durability = (1..tier).fold(16) { a, b -> a * 2 }
-        item.maxDamage = durability - 1
-        item.tier = tier
-    }
-    onCreateItemStack {
-        if (parent != null) item.manualRepairIngredients += parent()().manualRepairIngredients
-        manualRepairIngredientSuppliers.forEach { item.manualRepairIngredients += it() }
-        oreNameList.forEach { OreDictionary.registerOre(it, item.createItemStack(metadata = OreDictionary.WILDCARD_VALUE)) }
-    }
-}
-
-
-lateinit var fairyWeaponLoader: FairyWeaponLoader
-
-@Suppress("MemberVisibilityCanBePrivate", "unused")
-class FairyWeaponLoader(m: ModInitializer) {
-    operator fun EnumErgType.not(): () -> Ingredient = { sphereType.oreName.oreIngredient }
-    operator fun String.not(): () -> Ingredient = { oreIngredient }
-
-    val miragiumSword = m.fw(2, ::ItemFairyWeapon, "miragium_sword", "miragiumSword", listOf(), null, !ATTACK, !SLASH)
-    val crystalSword = m.fw(3, ::ItemCrystalSword, "crystal_sword", "crystalSword", listOf(), { miragiumSword }, !CRYSTAL)
-    val fairySword = m.fw(3, ::ItemFairySword, "fairy_sword", "fairySword", listOf(), { miragiumSword }, !ATTACK)
-
-    val miragiumAxe = m.fw(2, ::ItemMiragiumAxe, "miragium_axe", "miragiumAxe", listOf(), null, !SLASH, !HARVEST, !"plateMiragium")
-
-    val magicWandBase = m.fw(3, ::ItemFairyWeapon, "magic_wand_base", "magicWandBase", listOf(), null, !KNOWLEDGE)
-    val magicWandLight = m.fw(3, ::ItemMagicWandLight, "light_magic_wand", "magicWandLight", listOf(), { magicWandBase }, !LIGHT)
-    val magicWandCollecting = m.fw(3, ::ItemMagicWandCollecting, "collecting_magic_wand", "magicWandCollecting", listOf(), { magicWandBase }, !WARP)
-    val magicWandLightning = m.fw(3, ::ItemMagicWandLightning, "lightning_magic_wand", "magicWandLightning", listOf(), { magicWandBase }, !THUNDER, !ENERGY)
-
-    val ocarinaBase = m.fw(3, ::ItemFairyWeapon, "ocarina_base", "ocarinaBase", listOf(), null, !SOUND)
-    val ocarinaTemptation = m.fw(3, ::ItemOcarinaTemptation, "temptation_ocarina", "ocarinaTemptation", listOf(), { ocarinaBase }, !LIFE)
-    val bellBase = m.fw(2, ::ItemBellBase, "bell_base", "bellBase", listOf(), null, !SOUND)
-    val bellFlowerPicking = m.fw(2, { ItemBellFlowerPicking(0.0, 0.001, 0.2) }, "flower_picking_bell", "bellFlowerPicking", listOf(), { bellBase }, !HARVEST)
-    val bellFlowerPicking2 = m.fw(4, { ItemBellFlowerPicking(10.0, 0.01, 10000.0) }, "flower_picking_bell_2", "bellFlowerPicking2", listOf(), { bellFlowerPicking }, !HARVEST)
-    val bellChristmas = m.fw(3, ::ItemBellChristmas, "christmas_bell", "bellChristmas", listOf(), { bellBase }, !CHRISTMAS, !ATTACK)
-    val miragiumScythe = m.fw(2, { ItemMiragiumScythe(0.0, 2.0f) }, "miragium_scythe", "miragiumScythe", listOf(), null, !SLASH, !HARVEST)
-    val lilagiumScythe = m.fw(3, { ItemMiragiumScythe(10.0, 4.0f) }, "lilagium_scythe", "lilagiumScythe", listOf(), { miragiumScythe }, !HARVEST)
-    val ryugyoDrill = m.fw(4, { ItemRyugyoDrill(0.0) }, "ryugyo_drill", "ryugyoDrill", listOf(), null, !DESTROY, !THUNDER, !WATER)
-}
-
 object FairyWeapon {
+    @Suppress("UNUSED_VARIABLE")
     val module = module {
-        fairyWeaponLoader = FairyWeaponLoader(this)
+
+        fun <T : ItemFairyWeapon> ModInitializer.fw(
+            tier: Int,
+            creator: () -> T,
+            registryName: String,
+            unlocalizedName: String,
+            oreNameList: List<String>,
+            parent: (() -> ItemFairyWeapon)?,
+            vararg manualRepairIngredientSuppliers: () -> Ingredient
+        ) = item(creator, registryName) {
+            setUnlocalizedName(unlocalizedName)
+            setCreativeTab { creativeTab }
+            onRegisterItem {
+                if (side.isClient) {
+                    val modelResourceLocation = ModelResourceLocation(item.registryName!!, "normal")
+                    MinecraftForge.EVENT_BUS.register(object : Any() {
+                        @SubscribeEvent
+                        fun accept(event: ModelBakeEvent) {
+                            event.modelRegistry.putObject(modelResourceLocation, BakedModelBuiltinWrapper(event.modelRegistry.getObject(modelResourceLocation)!!))
+                        }
+                    })
+                    ModelLoader.setCustomModelResourceLocation(item, 0, modelResourceLocation)
+                }
+            }
+            onInit {
+                val durability = (1..tier).fold(16) { a, _ -> a * 2 }
+                item.maxDamage = durability - 1
+                item.tier = tier
+            }
+            onCreateItemStack {
+                if (parent != null) item.manualRepairIngredients += parent().manualRepairIngredients
+                manualRepairIngredientSuppliers.forEach { item.manualRepairIngredients += it() }
+                oreNameList.forEach { OreDictionary.registerOre(it, item.createItemStack(metadata = OreDictionary.WILDCARD_VALUE)) }
+            }
+        }
+
+        operator fun EnumErgType.not(): () -> Ingredient = { sphereType.oreName.oreIngredient }
+        operator fun String.not(): () -> Ingredient = { oreIngredient }
+
+        val miragiumSword = fw(2, { ItemFairyWeapon() }, "miragium_sword", "miragiumSword", listOf(), null, !ATTACK, !SLASH)
+        val crystalSword = fw(3, { ItemCrystalSword() }, "crystal_sword", "crystalSword", listOf(), miragiumSword, !CRYSTAL)
+        val fairySword = fw(3, { ItemFairySword() }, "fairy_sword", "fairySword", listOf(), miragiumSword, !ATTACK)
+
+        val miragiumAxe = fw(2, { ItemMiragiumAxe() }, "miragium_axe", "miragiumAxe", listOf(), null, !SLASH, !HARVEST, !"plateMiragium")
+
+        val magicWandBase = fw(3, { ItemFairyWeapon() }, "magic_wand_base", "magicWandBase", listOf(), null, !KNOWLEDGE)
+        val magicWandLight = fw(3, { ItemMagicWandLight() }, "light_magic_wand", "magicWandLight", listOf(), magicWandBase, !LIGHT)
+        val magicWandCollecting = fw(3, { ItemMagicWandCollecting() }, "collecting_magic_wand", "magicWandCollecting", listOf(), magicWandBase, !WARP)
+        val magicWandLightning = fw(3, { ItemMagicWandLightning() }, "lightning_magic_wand", "magicWandLightning", listOf(), magicWandBase, !THUNDER, !ENERGY)
+
+        val ocarinaBase = fw(3, { ItemFairyWeapon() }, "ocarina_base", "ocarinaBase", listOf(), null, !SOUND)
+        val ocarinaTemptation = fw(3, { ItemOcarinaTemptation() }, "temptation_ocarina", "ocarinaTemptation", listOf(), ocarinaBase, !LIFE)
+        val bellBase = fw(2, { ItemBellBase() }, "bell_base", "bellBase", listOf(), null, !SOUND)
+        val bellFlowerPicking = fw(2, { ItemBellFlowerPicking(0.0, 0.001, 0.2) }, "flower_picking_bell", "bellFlowerPicking", listOf(), bellBase, !HARVEST)
+        val bellFlowerPicking2 = fw(4, { ItemBellFlowerPicking(10.0, 0.01, 10000.0) }, "flower_picking_bell_2", "bellFlowerPicking2", listOf(), bellFlowerPicking, !HARVEST)
+        val bellChristmas = fw(3, { ItemBellChristmas() }, "christmas_bell", "bellChristmas", listOf(), bellBase, !CHRISTMAS, !ATTACK)
+        val miragiumScythe = fw(2, { ItemMiragiumScythe(0.0, 2.0f) }, "miragium_scythe", "miragiumScythe", listOf(), null, !SLASH, !HARVEST)
+        val lilagiumScythe = fw(3, { ItemMiragiumScythe(10.0, 4.0f) }, "lilagium_scythe", "lilagiumScythe", listOf(), miragiumScythe, !HARVEST)
+        val ryugyoDrill = fw(4, { ItemRyugyoDrill(0.0) }, "ryugyo_drill", "ryugyoDrill", listOf(), null, !DESTROY, !THUNDER, !WATER)
+
         onMakeLang {
             enJa("miragefairy2019.magic.${MagicMessage.NO_FAIRY.unlocalizedName}.text", "You don't have a fairy", "妖精を所持していません")
             enJa("miragefairy2019.magic.${MagicMessage.INSUFFICIENT_DURABILITY.unlocalizedName}.text", "Insufficient durability", "耐久値が不足しています")
