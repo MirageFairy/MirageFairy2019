77d43a27cfa51fff71fea2cdf5296b4a7045e88d
MirageFairy <_@example.com>
2022-03-26 11:25:16 +0900

    [clenaup] delete: MutableManaSet

--- src/main/java/miragefairy2019/mod3/mana/ManaSet.kt
+++ src/main/java/miragefairy2019/mod3/mana/ManaSet.kt
@@ -17,67 +17,12 @@ data class ManaSet(
         val ZERO = ManaSet(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
     }
 
-    constructor() : this(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-
     override fun getShine() = shine
     override fun getFire() = fire
     override fun getWind() = wind
     override fun getGaia() = gaia
     override fun getAqua() = aqua
     override fun getDark() = dark
-}
-
-data class MutableManaSet(
-    @Expose private var shine: Double,
-    @Expose private var fire: Double,
-    @Expose private var wind: Double,
-    @Expose private var gaia: Double,
-    @Expose private var aqua: Double,
-    @Expose private var dark: Double
-) : IManaSet {
-    constructor() : this(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-
-    override fun getShine() = shine
-    override fun getFire() = fire
-    override fun getWind() = wind
-    override fun getGaia() = gaia
-    override fun getAqua() = aqua
-    override fun getDark() = dark
-    fun setShine(shine: Double) = run { this.shine = shine }
-    fun setFire(fire: Double) = run { this.fire = fire }
-    fun setWind(wind: Double) = run { this.wind = wind }
-    fun setGaia(gaia: Double) = run { this.gaia = gaia }
-    fun setAqua(aqua: Double) = run { this.aqua = aqua }
-    fun setDark(dark: Double) = run { this.dark = dark }
-
-    fun set(manaSet: IManaSet) {
-        this.shine = manaSet.shine
-        this.fire = manaSet.fire
-        this.wind = manaSet.wind
-        this.gaia = manaSet.gaia
-        this.aqua = manaSet.aqua
-        this.dark = manaSet.dark
-    }
-
-    fun set(shine: Double, fire: Double, wind: Double, gaia: Double, aqua: Double, dark: Double) {
-        this.shine = shine
-        this.fire = fire
-        this.wind = wind
-        this.gaia = gaia
-        this.aqua = aqua
-        this.dark = dark
-    }
-
-    fun reset() = set(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-
-    operator fun plusAssign(other: IManaSet) {
-        this.shine += other.shine
-        this.fire += other.fire
-        this.wind += other.wind
-        this.gaia += other.gaia
-        this.aqua += other.aqua
-        this.dark += other.dark
-    }
 }
 
 
@@ -91,7 +36,6 @@ fun IManaSet.getMana(manaType: EnumManaType) = when (manaType) {
 }
 
 fun IManaSet.copy() = ManaSet(shine, fire, wind, gaia, aqua, dark)
-fun IManaSet.copyAsMutable() = MutableManaSet(shine, fire, wind, gaia, aqua, dark)
 operator fun IManaSet.plus(a: IManaSet): IManaSet = ManaSet(shine + a.shine, fire + a.fire, wind + a.wind, gaia + a.gaia, aqua + a.aqua, dark + a.dark)
 operator fun IManaSet.times(a: Double): IManaSet = ManaSet(shine * a, fire * a, wind * a, gaia * a, aqua * a, dark * a)
 operator fun IManaSet.div(a: Double): IManaSet = ManaSet(shine / a, fire / a, wind / a, gaia / a, aqua / a, dark / a)
