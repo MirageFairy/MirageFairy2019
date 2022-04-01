bdb2d1a62497f93a09babb7a32ca85cb302527ad
MirageFairy <_@example.com>
2022-04-02 01:38:35 +0900

    [refactor] Main

--- src/main/java/miragefairy2019/mod3/main/Main.kt
+++ src/main/java/miragefairy2019/mod3/main/Main.kt
@@ -1,11 +1,12 @@
 package miragefairy2019.mod3.main
 
+import miragefairy2019.libkt.GuiHandlerContext
+import miragefairy2019.libkt.ISimpleGuiHandler
+import miragefairy2019.libkt.guiHandler
 import miragefairy2019.libkt.module
 import miragefairy2019.mod.ModMirageFairy2019
 import miragefairy2019.mod3.artifacts.FairyCrystal
 import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.world.World
 import net.minecraftforge.fml.common.network.IGuiHandler
 import net.minecraftforge.fml.common.network.NetworkRegistry
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper
@@ -46,10 +47,10 @@ object Main {
 
         // Gui Handler
         onInit {
-            NetworkRegistry.INSTANCE.registerGuiHandler(ModMirageFairy2019.instance, object : IGuiHandler {
-                override fun getServerGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int) = guiHandlers[id]?.getServerGuiElement(id, player, world, x, y, z)
-                override fun getClientGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int) = guiHandlers[id]?.getClientGuiElement(id, player, world, x, y, z)
-            })
+            NetworkRegistry.INSTANCE.registerGuiHandler(ModMirageFairy2019.instance, object : ISimpleGuiHandler {
+                override fun GuiHandlerContext.onServer() = guiHandlers[id]?.getServerGuiElement(id, player, world, x, y, z)
+                override fun GuiHandlerContext.onClient() = guiHandlers[id]?.getClientGuiElement(id, player, world, x, y, z)
+            }.guiHandler)
         }
 
     }
