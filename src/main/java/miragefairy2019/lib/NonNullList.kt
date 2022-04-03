40915b626a5a3d77fbe105ddea3f4bea79067c01
MirageFairy <_@example.com>
2022-04-03 13:57:10 +0900

    [internal] fix: Iterable<T>.toNonNullList

--- src/main/java/miragefairy2019/lib/NonNullList.kt
+++ src/main/java/miragefairy2019/lib/NonNullList.kt
@@ -2,4 +2,4 @@ package miragefairy2019.lib
 
 import net.minecraft.util.NonNullList
 
-fun <T> Iterable<T>.toNonNullList(): NonNullList<T> = NonNullList.create<T>().also { this.forEach { item -> it.add(item) } }
+fun <T : Any> Iterable<T>.toNonNullList(): NonNullList<T> = NonNullList.create<T>().also { this.forEach { item -> it.add(item) } }
