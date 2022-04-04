a653e6cc5ed478bdea15ebc4cf76dff48968ba4a
MirageFairy <_@example.com>
2022-04-04 14:50:34 +0900

    _

--- src/main/java/miragefairy2019/mod3/artifacts/CommonMaterials.kt
+++ src/main/java/miragefairy2019/mod3/artifacts/CommonMaterials.kt
@@ -60,7 +60,6 @@ import net.minecraft.world.World
 import net.minecraftforge.fml.common.registry.GameRegistry
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
-import java.time.LocalDateTime
 import java.util.Random
 
 object CommonMaterials {
