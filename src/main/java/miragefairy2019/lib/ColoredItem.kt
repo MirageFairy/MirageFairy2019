6739f56678eb0ae304968482234267f983b065c1
MirageFairy <_@example.com>
2022-04-07 10:01:11 +0900

    [internal] add: ColoredItem

---
+++ src/main/java/miragefairy2019/lib/ColoredItem.kt
@@ -0,0 +1,24 @@
+package miragefairy2019.lib
+
+import miragefairy2019.libkt.ItemInitializer
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.color.IItemColor
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+
+interface IColoredItem {
+    @SideOnly(Side.CLIENT)
+    fun colorMultiplier(itemStack: ItemStack, tintIndex: Int): Int
+}
+
+fun <I> ItemInitializer<I>.registerItemColorHandler() where  I : Item, I : IColoredItem {
+    modInitializer.onRegisterItemColorHandler {
+        @SideOnly(Side.CLIENT)
+        class ItemColorImpl : IItemColor {
+            override fun colorMultiplier(itemStack: ItemStack, tintIndex: Int) = item.colorMultiplier(itemStack, tintIndex)
+        }
+        Minecraft.getMinecraft().itemColors.registerItemColorHandler(ItemColorImpl(), item)
+    }
+}
