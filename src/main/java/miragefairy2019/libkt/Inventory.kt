f7533cce4d85523049c2482305bfcf5d3a7e5315
MirageFairy <_@example.com>
2022-04-17 17:44:49 +0900

    [format] Inventory

--- src/main/java/miragefairy2019/libkt/Inventory.kt
+++ src/main/java/miragefairy2019/libkt/Inventory.kt
@@ -18,13 +18,6 @@ operator fun IInventory.set(index: Int, itemStack: ItemStack) = setInventorySlot
 val IInventory.indices get() = 0 until size
 val IInventory.itemStacks get() = (0 until size).map { this[it] }
 
-val NBTTagCompound.inventorySlotCount: Int
-    get() {
-        val tags = this.nbtProvider["Items"].tags ?: return 0 // Itemsがリスト出ない場合は0
-        val maxSlotIndex = tags.mapNotNull { it["Slot"].int }.max() ?: return 0 // Slotに数値を持つタグが1個もなかった場合は0
-        return maxSlotIndex + 1
-    }
-
 fun IInventory.readFromNBT(nbt: NBTTagCompound) {
     val list = NonNullList.withSize(size, ItemStack.EMPTY)
     ItemStackHelper.loadAllItems(nbt, list)
@@ -40,3 +33,10 @@ fun IInventory.writeToNBT(nbt: NBTTagCompound) {
 class SmartSlot(inventory: IInventory, index: Int, xPosition: Int, yPosition: Int) : Slot(inventory, index, xPosition, yPosition) {
     override fun isItemValid(itemStack: ItemStack) = inventory.isItemValidForSlot(slotIndex, itemStack)
 }
+
+val NBTTagCompound.inventorySlotCount: Int
+    get() {
+        val tags = this.nbtProvider["Items"].tags ?: return 0 // Itemsがリスト出ない場合は0
+        val maxSlotIndex = tags.mapNotNull { it["Slot"].int }.max() ?: return 0 // Slotに数値を持つタグが1個もなかった場合は0
+        return maxSlotIndex + 1
+    }
