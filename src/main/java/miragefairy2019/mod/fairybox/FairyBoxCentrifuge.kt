b3ce816e26ffa2ec129e02b2160e0c1e4f7d7a46
MirageFairy <_@example.com>
2022-04-18 22:31:19 +0900

    [internal] FairyBoxCentrifuge: add: Gui

--- src/main/java/miragefairy2019/mod/fairybox/FairyBoxCentrifuge.kt
+++ src/main/java/miragefairy2019/mod/fairybox/FairyBoxCentrifuge.kt
@@ -5,6 +5,15 @@ import miragefairy2019.api.IFairyCentrifugeCraftArguments
 import miragefairy2019.api.IFairyCentrifugeCraftProcess
 import miragefairy2019.api.IFairyCentrifugeCraftRecipe
 import miragefairy2019.api.Mana
+import miragefairy2019.lib.Alignment
+import miragefairy2019.lib.ComponentBackgroundLabel
+import miragefairy2019.lib.ComponentLabel
+import miragefairy2019.lib.ComponentSlot
+import miragefairy2019.lib.GuiComponent
+import miragefairy2019.lib.IComponent
+import miragefairy2019.lib.SlotResult
+import miragefairy2019.lib.Symbols
+import miragefairy2019.lib.WindowProperty
 import miragefairy2019.lib.compound
 import miragefairy2019.lib.compoundOrCreate
 import miragefairy2019.lib.container
@@ -19,6 +28,7 @@ import miragefairy2019.lib.readInventory
 import miragefairy2019.lib.set
 import miragefairy2019.lib.writeToNBT
 import miragefairy2019.libkt.flatten
+import miragefairy2019.libkt.gray
 import miragefairy2019.libkt.randomInt
 import miragefairy2019.libkt.sandwich
 import miragefairy2019.libkt.textComponent
@@ -27,7 +37,9 @@ import miragefairy2019.mod.ModMirageFairy2019
 import miragefairy2019.util.InventoryTileEntity
 import mirrg.kotlin.atLeast
 import mirrg.kotlin.atMost
+import mirrg.kotlin.formatAs
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.inventory.Slot
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
@@ -161,8 +173,105 @@ class TileEntityFairyBoxCentrifuge : TileEntityFairyBoxBase() {
     // Gui
 
     fun createContainer(player: EntityPlayer) = container {
-        width = 100
-        height = 100
+
+        // Guiを開いたときにサーバー側でフォリアを計測する機能
+        val propertyMilliFolia = WindowProperty()
+        if (!world.isRemote) propertyMilliFolia.value = (getFolia(10000) * 1000.0).toInt()
+        windowProperties[0] = propertyMilliFolia
+        fun getFolia() = propertyMilliFolia.value / 1000.0 // ローカル変数にすると計測したフォリアを同期できない
+        fun getFoliaSpeedFactor() = getFoliaSpeedFactor(getFolia())
+
+
+        // レシピ判定機能
+        var recipeMatchResult: RecipeMatchResult? = null
+        components += object : IComponent {
+            override fun drawGuiContainerForegroundLayer(gui: GuiComponent, mouseX: Int, mouseY: Int) {
+                recipeMatchResult = match()
+            }
+        }
+
+
+        width = 3 + 4 + 18 * 9 + 4 + 3
+
+
+        var yi = 0
+        yi += 3
+
+        yi += 2 // 文字マージン
+
+
+        // Guiタイトル
+        components += ComponentLabel(width / 2, yi, Alignment.CENTER) { inputInventory.displayName }
+        yi += 8
+        yi += 2
+
+
+        // 入力
+        repeat(9) { c ->
+            components += ComponentSlot(this, 3 + 4 + 18 * c, yi) { x, y -> Slot(inputInventory, c, x, y) }
+            components += ComponentBackgroundLabel(3 + 4 + 18 * c + 9, yi + 9 - 4, Alignment.CENTER) { textComponent { "IN"().gray } }
+        }
+        yi += 18
+
+        // 妖精
+        fun defineProcess(index: Int) {
+            fun getProcessResult() = recipeMatchResult?.processeResults?.getOrNull(index)
+            val c = 1 + 3 * index
+            components += ComponentLabel(3 + 4 + 18 * c + 9, yi + 18 * 0, Alignment.CENTER) { getProcessResult()?.process?.name }
+            components += ComponentLabel(3 + 4 + 18 * c + 9, yi + 18 * 0 + 9, Alignment.CENTER) { getProcessResult()?.factors }
+            components += ComponentSlot(this, 3 + 4 + 18 * c, yi + 18 * 1) { x, y -> Slot(fairyInventory, index, x, y) }
+            components += ComponentBackgroundLabel(3 + 4 + 18 * c + 9, yi + 18 * 1 + 9 - 4, Alignment.CENTER) { textComponent { "F"().gray } }
+            components += ComponentLabel(3 + 4 + 18 * c + 9, yi + 18 * 2, Alignment.CENTER) { getProcessResult()?.speed?.let { textComponent { (it * getFoliaSpeedFactor()).formatAs("%.2f/分")() } } } // TODO translate
+            components += ComponentLabel(3 + 4 + 18 * c + 9, yi + 18 * 2 + 9, Alignment.CENTER) { getProcessResult()?.fortune?.let { textComponent { it.formatAs("%.02f" + Symbols.FORTUNE)() } } }
+        }
+        defineProcess(0)
+        defineProcess(1)
+        defineProcess(2)
+        yi += 18 * 3
+
+        // 出力
+        repeat(9) { c ->
+            components += ComponentSlot(this, 3 + 4 + 18 * c, yi) { x, y -> SlotResult(player, outputInventory, c, x, y) }
+            components += ComponentBackgroundLabel(3 + 4 + 18 * c + 9, yi + 9 - 4, Alignment.CENTER) { textComponent { "OUT"().gray } }
+        }
+        yi += 18
+
+        // 速度
+        components += ComponentLabel(3 + 4 + 18 * 1 + 9, yi, Alignment.CENTER) { recipeMatchResult?.speed?.let { textComponent { (it * getFoliaSpeedFactor()).formatAs("%.2f/分")() } } }
+        components += ComponentLabel(3 + 4 + 18 * 4 + 9, yi, Alignment.CENTER) { recipeMatchResult?.fortune?.let { textComponent { it.formatAs("%.02f" + Symbols.FORTUNE)() } } }
+        yi += 9
+
+        // フォリア表示
+        components += ComponentLabel(width - 3 - 4, yi, Alignment.RIGHT) { textComponent { "${getFolia() formatAs "%.2f"} Folia, 速度ブースト: ${getFoliaSpeedFactor() * 100.0 formatAs "%.2f"}%"() } }
+        yi += 9
+
+
+        // プレイヤーインベントリタイトル
+        yi += 2
+        components += ComponentLabel(3 + 4, yi, Alignment.LEFT) { player.inventory.displayName }
+        yi += 8
+        yi += 2
+
+
+        // プレイヤーインベントリメイン
+        repeat(3) { r -> repeat(9) { c -> components += ComponentSlot(this, 3 + 4 + 18 * c, yi + 18 * r) { x, y -> Slot(player.inventory, 9 + 9 * r + c, x, y) } } }
+        yi += 18 * 3
+
+        yi += 4
+
+        // プレイヤーインベントリ最下段
+        repeat(9) { c -> components += ComponentSlot(this, 3 + 4 + 18 * c, yi) { x, y -> Slot(player.inventory, c, x, y) } }
+        yi += 18
+
+        yi += 4
+
+        yi += 3
+        height = yi
+
+
+        interactInventories += inputInventory
+        interactInventories += outputInventory
+
     }
 
 }
