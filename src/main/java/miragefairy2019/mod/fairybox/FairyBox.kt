44e33e86adca68bc1dd2301ac4a7173d439ab796
MirageFairy <_@example.com>
2022-06-04 15:59:25 +0900

    [format]

--- src/main/java/miragefairy2019/mod/fairybox/FairyBox.kt
+++ src/main/java/miragefairy2019/mod/fairybox/FairyBox.kt
@@ -105,6 +105,8 @@ object FairyBox {
 
             onAddRecipe {
 
+                // 鉱石処理
+
                 // 樹液16＋ガラス瓶→シロップ
                 FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
                     process("ろ過", 20.0) { !Mana.DARK + !Erg.WATER }, // TODO translate
@@ -125,6 +127,9 @@ object FairyBox {
                     Items.SUGAR.ingredient to 1
                 )
 
+
+                // 抽出
+
                 // グロウストーンダスト10＋ボウル→金3
                 FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
                     process("融解", 30.0) { !Mana.GAIA + !Erg.FLAME }, // TODO translate
@@ -133,27 +138,6 @@ object FairyBox {
                     { "ingotGold".toOreName().copyItemStack(3) }, 0.0,
                     "dustGlowstone".oreIngredient to 10,
                     Items.BOWL.ingredient to 1
-                )
-
-                // 硫黄3＋赤石10→辰砂3
-                FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
-                    process("融解", 30.0) { !Mana.GAIA + !Erg.FLAME }, // TODO translate
-                    process("析出", 60.0) { !Mana.FIRE + !Erg.CHEMICAL }, // TODO translate
-                    process("洗浄", 10.0) { !Mana.DARK + !Erg.WATER }, // TODO translate
-                    { "dustCinnabar".toOreName().copyItemStack(3) }, 0.0,
-                    "dustSulfur".oreIngredient to 3,
-                    "dustRedstone".oreIngredient to 10
-                )
-
-                // 石炭の粉64＋フリント8＋黒曜石→ダイヤモンド
-                FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
-                    process("圧縮", 10.0) { !Mana.GAIA + !Erg.CRAFT }, // TODO translate
-                    process("歪縮", 40.0) { !Mana.FIRE + !Erg.SPACE }, // TODO translate
-                    process("結晶成長", 50.0) { !Mana.SHINE + !Erg.CRYSTAL }, // TODO translate
-                    { "gemDiamond".toOreName().copyItemStack() }, 0.0,
-                    "dustCoal".oreIngredient to 64,
-                    Items.FLINT.ingredient to 8,
-                    "obsidian".oreIngredient to 1
                 )
 
                 // マグマクリーム2＋雪玉→スライムボール
@@ -174,6 +158,33 @@ object FairyBox {
                     { Items.BLAZE_POWDER.createItemStack() }, 0.0,
                     Items.MAGMA_CREAM.ingredient to 2,
                     Blocks.CONCRETE_POWDER.ingredient!! to 1
+                )
+
+
+                // 化合
+
+                // 硫黄3＋赤石10→辰砂3
+                FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
+                    process("融解", 30.0) { !Mana.GAIA + !Erg.FLAME }, // TODO translate
+                    process("析出", 60.0) { !Mana.FIRE + !Erg.CHEMICAL }, // TODO translate
+                    process("洗浄", 10.0) { !Mana.DARK + !Erg.WATER }, // TODO translate
+                    { "dustCinnabar".toOreName().copyItemStack(3) }, 0.0,
+                    "dustSulfur".oreIngredient to 3,
+                    "dustRedstone".oreIngredient to 10
+                )
+
+
+                // 結晶合成
+
+                // 石炭の粉64＋フリント8＋黒曜石→ダイヤモンド
+                FairyCentrifugeCraftRegistry.fairyCentrifugeCraftHandlers += fairyCentrifugeCraftHandler(
+                    process("圧縮", 10.0) { !Mana.GAIA + !Erg.CRAFT }, // TODO translate
+                    process("歪縮", 40.0) { !Mana.FIRE + !Erg.SPACE }, // TODO translate
+                    process("結晶成長", 50.0) { !Mana.SHINE + !Erg.CRYSTAL }, // TODO translate
+                    { "gemDiamond".toOreName().copyItemStack() }, 0.0,
+                    "dustCoal".oreIngredient to 64,
+                    Items.FLINT.ingredient to 8,
+                    "obsidian".oreIngredient to 1
                 )
 
 
