bd2059317cb879128d4a50f86f67016b51fde29c
MirageFairy <_@example.com>
2022-06-11 02:03:22 +0900

    [comment]

--- src/main/java/miragefairy2019/export/IngredientFactoryOreIngredientComplex.kt
+++ src/main/java/miragefairy2019/export/IngredientFactoryOreIngredientComplex.kt
@@ -9,6 +9,7 @@ import net.minecraftforge.common.crafting.JsonContext
 /**
  * 耐久が削れたクラフティングツールを鉱石辞書名にマッチさせるためのIngredient
  */
+@Suppress("unused") // リフレクション経由で呼ばれる
 class IngredientFactoryOreIngredientComplex : IIngredientFactory {
     override fun parse(context: JsonContext, json: JsonObject) = OreIngredientComplex(JsonUtils.getString(json, "ore"))
 }
