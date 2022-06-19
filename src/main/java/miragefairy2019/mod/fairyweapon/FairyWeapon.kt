db21dc2c96fceeefdff18d73d0fafce790e7f75a
MirageFairy <_@example.com>
2022-06-19 12:28:14 +0900

    [refactor] FairyWeapon

--- src/main/java/miragefairy2019/mod/fairyweapon/FairyWeapon.kt
+++ src/main/java/miragefairy2019/mod/fairyweapon/FairyWeapon.kt
@@ -71,166 +71,212 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class LangPair(val english: String, val japanese: String)
 
 enum class FairyWeaponKind(
+    val parent: FairyWeaponKind?,
     val registryName: String,
     val unlocalizedName: String,
     val displayName: LangPair,
     val poem: LangPair,
     val author: LangPair?,
-    val advancementText: LangPair
+    val advancementText: LangPair,
+    val frame: String?
 ) {
     miragiumSword(
-        "?", "miragiumSword",
+        null,
+        "miragium_sword", "miragiumSword",
         LangPair("Miragium Sword", "ミラジウムの剣"),
         LangPair("", "その刃で何を切る？"),
         null,
-        LangPair("Get a specific item", "ミラジウムは軟らかいので刃物には向かない")
+        LangPair("Get a specific item", "ミラジウムは軟らかいので刃物には向かない"),
+        null
     ),
     fairySword(
-        "?", "fairySword",
+        miragiumSword,
+        "fairy_sword", "fairySword",
         LangPair("Fairy Sword", "妖精剣"),
         LangPair("Design Contest Work", "デザインコンテスト武器"),
         LangPair("tanun3sei", "たぬん三世"),
-        LangPair("Get a specific item", "デザインコンテスト武器")
+        LangPair("Get a specific item", "デザインコンテスト武器"),
+        "goal"
     ),
     miragiumAxe(
-        "?", "miragiumAxe",
+        null,
+        "miragium_axe", "miragiumAxe",
         LangPair("Miragium Axe", "ミラジウムの斧"),
         LangPair("", "飛べるって素敵"),
         null,
-        LangPair("Get a specific item", "切断のエルグを真空波にする構造")
+        LangPair("Get a specific item", "切断のエルグを真空波にする構造"),
+        null
     ),
     miragiumScythe(
-        "?", "miragiumScythe",
+        null,
+        "miragium_scythe", "miragiumScythe",
         LangPair("Miragium Scythe", "ミラジウムの大鎌"),
         LangPair("", "自分を切らないように！"),
         null,
-        LangPair("Get a specific item", "作物を刈り奪る形をしてるだろ？")
+        LangPair("Get a specific item", "作物を刈り奪る形をしてるだろ？"),
+        null
     ),
     lilagiumScythe(
-        "?", "lilagiumScythe",
+        miragiumScythe,
+        "lilagium_scythe", "lilagiumScythe",
         LangPair("Lilagium Scythe", "リラジウムの大鎌"),
         LangPair("", "葉っぱが吸い込まれてくる"),
         null,
-        LangPair("Get a specific item", "植物だって話せばわかる")
+        LangPair("Get a specific item", "植物だって話せばわかる"),
+        null
     ),
     magicWandBase(
-        "?", "magicWandBase",
+        null,
+        "magic_wand_base", "magicWandBase",
         LangPair("Magic Wand Base", "ロッドベース"),
         LangPair("", "風の心、探求"),
         null,
-        LangPair("Get a specific item", "妖精→持ち手→魔導芯棒→スフィア→対象物")
+        LangPair("Get a specific item", "妖精→持ち手→魔導芯棒→スフィア→対象物"),
+        null
     ),
     magicWandLight(
-        "?", "magicWandLight",
+        magicWandBase,
+        "light_magic_wand", "magicWandLight",
         LangPair("Magic Wand of Light", "光のロッド"),
         LangPair("", "古代の魔法「ニクトフォビア」、優しい光が洞窟を照らす"),
         null,
-        LangPair("Get a specific item", "パラボラの焦点に発光のスフィア")
+        LangPair("Get a specific item", "パラボラの焦点に発光のスフィア"),
+        "goal"
     ),
     magicWandCollecting(
-        "?", "magicWandCollecting",
+        magicWandBase,
+        "collecting_magic_wand", "magicWandCollecting",
         LangPair("Magic Wand of Collecting", "収集のロッド"),
         LangPair("", "新開発の魔法「ソルメローシェ・トリーパ」、魔法のマジックハンド"),
         null,
-        LangPair("Get a specific item", "縮地のエルグが渦を巻いて収束するように")
+        LangPair("Get a specific item", "縮地のエルグが渦を巻いて収束するように"),
+        "goal"
     ),
     chargingRod(
-        "?", "chargingRod",
+        magicWandBase,
+        "charging_rod", "chargingRod",
         LangPair("Charging Rod", "チャージングロッド"),
         LangPair("", "電気の力で栄えた文明があったという"),
         null,
-        LangPair("Get a specific item", "カミナリのエルグなんか集めて何に使うんだろう？")
+        LangPair("Get a specific item", "カミナリのエルグなんか集めて何に使うんだろう？"),
+        null
     ),
     magicWandLightning(
-        "?", "magicWandLightning",
+        chargingRod,
+        "lightning_magic_wand", "magicWandLightning",
         LangPair("Magic Wand of Lightning", "ライトニングロッド"),
         LangPair("", "古代魔法「ライトニングボルト」"),
         null,
-        LangPair("Get a specific item", "雷電のエルグは金属の中を伝うことが知られている")
+        LangPair("Get a specific item", "雷電のエルグは金属の中を伝うことが知られている"),
+        "goal"
     ),
     gravityRod(
-        "?", "gravityRod",
+        null,
+        "gravity_rod", "gravityRod",
         LangPair("Gravity Rod", "グラビティロッド"),
         LangPair("", "局所エーテル超球面歪曲技術"),
         null,
-        LangPair("Get a specific item", "ミラージュオイルで物体と空間を接着するのだ！")
+        LangPair("Get a specific item", "ミラージュオイルで物体と空間を接着するのだ！"),
+        "goal"
     ),
     ocarinaBase(
-        "?", "ocarinaBase",
+        null,
+        "ocarina_base", "ocarinaBase",
         LangPair("Ocarina Base", "オカリナベース"),
         LangPair("", "適当に吹いても音楽になる笛"),
         null,
-        LangPair("Get a specific item", "カラカラする？音の妖精の魂が入っている")
+        LangPair("Get a specific item", "カラカラする？音の妖精の魂が入っている"),
+        null
     ),
     ocarinaTemptation(
-        "?", "ocarinaTemptation",
+        ocarinaBase,
+        "temptation_ocarina", "ocarinaTemptation",
         LangPair("Ocarina of Temptation", "魅惑のオカリナ"),
         LangPair("", "その音は人の腹を満たし、淫靡な気分にさせる"),
         null,
-        LangPair("Get a specific item", "生物は生命のエルグさえあれば増える")
+        LangPair("Get a specific item", "生物は生命のエルグさえあれば増える"),
+        "goal"
     ),
     bellBase(
-        "?", "bellBase",
+        null,
+        "bell_base", "bellBase",
         LangPair("Bell Base", "鐘ベース"),
         LangPair("", "妖精の力を解放せよ"),
         null,
-        LangPair("Get a specific item", "妖精よ、わずかの間、我に力を与えたまえ")
+        LangPair("Get a specific item", "妖精よ、わずかの間、我に力を与えたまえ"),
+        null
     ),
     bellFlowerPicking(
-        "?", "bellFlowerPicking",
+        bellBase,
+        "flower_picking_bell", "bellFlowerPicking",
         LangPair("Bell of Flower Picking", "花摘みの鐘"),
         LangPair("", "ちょっとお花を摘みに"),
         null,
-        LangPair("Get a specific item", "リラジウムの音は草花の心に響くという")
+        LangPair("Get a specific item", "リラジウムの音は草花の心に響くという"),
+        null
     ),
     bellFlowerPicking2(
-        "?", "bellFlowerPicking2",
+        bellFlowerPicking,
+        "flower_picking_bell_2", "bellFlowerPicking2",
         LangPair("Bell of Flower Picking II", "花摘みの鐘 II"),
         LangPair("", "光輝のフェロモン"),
         null,
-        LangPair("Get a specific item", "妖精の正体はミラージュの花粉、つまり花に魅かれる")
+        LangPair("Get a specific item", "妖精の正体はミラージュの花粉、つまり花に魅かれる"),
+        "goal"
     ),
     bellChristmas(
-        "?", "bellChristmas",
+        bellBase,
+        "christmas_bell", "bellChristmas",
         LangPair("Christmas Bell", "クリスマスの鐘"),
         LangPair("", "いけない子には"),
         null,
-        LangPair("Get a specific item", "金メッキする、輪っかを付ける、木の枝を付ける")
+        LangPair("Get a specific item", "金メッキする、輪っかを付ける、木の枝を付ける"),
+        "challenge"
     ),
     crystalSword(
-        "?", "crystalSword",
+        miragiumSword,
+        "crystal_sword", "crystalSword",
         LangPair("Crystal Sword", "クリスタルソード"),
         LangPair("", "妖精はこれをおやつにするという"),
         null,
-        LangPair("Get a specific item", "金属質よりも非晶質の方が鋭利だ、って")
+        LangPair("Get a specific item", "金属質よりも非晶質の方が鋭利だ、って"),
+        "goal"
     ),
     ryugyoDrill(
-        "?", "ryugyoDrill",
+        null,
+        "ryugyo_drill", "ryugyoDrill",
         LangPair("Ryugyo Drill", "龍魚ドリル"),
         LangPair("Design Contest Work", "デザインコンテスト武器"),
         LangPair("Yoshinon", "よしのん"),
-        LangPair("Get a specific item", "デザインコンテスト武器")
+        LangPair("Get a specific item", "デザインコンテスト武器"),
+        "goal"
     ),
     prayerWheel(
-        "?", "prayerWheel",
+        null,
+        "prayer_wheel", "prayerWheel",
         LangPair("Prayer Wheel", "収束の地"),
         LangPair("", "重合するアストラル光。魂の叫び。"),
         null,
-        LangPair("Get a specific item", "ホイールの外側に妖精語の呪文なんか書いて読めるの？")
+        LangPair("Get a specific item", "ホイールの外側に妖精語の呪文なんか書いて読めるの？"),
+        null
     ),
     prayerWheel2(
-        "?", "prayerWheel2",
+        prayerWheel,
+        "prayer_wheel_2", "prayerWheel2",
         LangPair("Prayer Wheel II", "約束の地"),
         LangPair("", "吹き荒れる妖精の声、宇宙のジェット"),
         null,
-        LangPair("Get a specific item", "人は死ぬと妖精になるんだって")
+        LangPair("Get a specific item", "人は死ぬと妖精になるんだって"),
+        null
     ),
     prayerWheel3(
-        "?", "prayerWheel3",
+        prayerWheel2,
+        "prayer_wheel_3", "prayerWheel3",
         LangPair("Prayer Wheel III", "束縛の地"),
         LangPair("", "覚えてる？前世、自分が何の妖精だったか"),
         null,
-        LangPair("Get a specific item", "人、牛、妖精、ゾンビ、植物、土")
+        LangPair("Get a specific item", "人、牛、妖精、ゾンビ、植物、土"),
+        "goal"
     ),
 }
 
@@ -635,68 +681,35 @@ object FairyWeapon {
             )
 
             // 進捗
-            class Achievement(val registerName: String, val unlocalizedName: String, val parent: Achievement? = null, val frame: String? = null) {
-                init {
-                    place(registerName, jsonElement(
-                        "display" to jsonElementNotNull(
-                            "icon" to jsonElement(
-                                "item" to "miragefairy2019:$registerName".jsonElement
-                            ),
-                            "title" to jsonElement(
-                                "translate" to "item.$unlocalizedName.name".jsonElement
-                            ),
-                            "description" to jsonElement(
-                                "translate" to "item.$unlocalizedName.recipe".jsonElement
-                            ),
-                            frame?.let { "frame" to it.jsonElement }
+            FairyWeaponKind.values().forEach { fairyWeaponKind ->
+                place(fairyWeaponKind.registryName, jsonElement(
+                    "display" to jsonElementNotNull(
+                        "icon" to jsonElement(
+                            "item" to "miragefairy2019:${fairyWeaponKind.registryName}".jsonElement
                         ),
-                        "parent" to "miragefairy2019:fairy_weapon/${parent?.registerName ?: "root"}".jsonElement,
-                        "criteria" to jsonElement(
-                            "main" to jsonElement(
-                                "trigger" to "minecraft:inventory_changed".jsonElement,
-                                "conditions" to jsonElement(
-                                    "items" to jsonElement(
-                                        jsonElement(
-                                            "item" to "miragefairy2019:$registerName".jsonElement
-                                        )
+                        "title" to jsonElement(
+                            "translate" to "item.${fairyWeaponKind.unlocalizedName}.name".jsonElement
+                        ),
+                        "description" to jsonElement(
+                            "translate" to "item.${fairyWeaponKind.unlocalizedName}.recipe".jsonElement
+                        ),
+                        fairyWeaponKind.frame?.let { "frame" to it.jsonElement }
+                    ),
+                    "parent" to "miragefairy2019:fairy_weapon/${fairyWeaponKind.parent?.registryName ?: "root"}".jsonElement,
+                    "criteria" to jsonElement(
+                        "main" to jsonElement(
+                            "trigger" to "minecraft:inventory_changed".jsonElement,
+                            "conditions" to jsonElement(
+                                "items" to jsonElement(
+                                    jsonElement(
+                                        "item" to "miragefairy2019:${fairyWeaponKind.registryName}".jsonElement
                                     )
                                 )
                             )
                         )
-                    ))
-                }
+                    )
+                ))
             }
-
-            val miragium_axe = Achievement("miragium_axe", "miragiumAxe", null, null)
-
-            val miragium_sword = Achievement("miragium_sword", "miragiumSword", null, null)
-            val fairy_sword = Achievement("fairy_sword", "fairySword", miragium_sword, "goal")
-            val crystal_sword = Achievement("crystal_sword", "crystalSword", miragium_sword, "goal")
-
-            val miragium_scythe = Achievement("miragium_scythe", "miragiumScythe", null, null)
-            val lilagium_scythe = Achievement("lilagium_scythe", "lilagiumScythe", miragium_scythe, null)
-
-            val bell_base = Achievement("bell_base", "bellBase", null, null)
-            val flower_picking_bell = Achievement("flower_picking_bell", "bellFlowerPicking", bell_base, null)
-            val flower_picking_bell_2 = Achievement("flower_picking_bell_2", "bellFlowerPicking2", flower_picking_bell, "goal")
-            val christmas_bell = Achievement("christmas_bell", "bellChristmas", bell_base, "challenge")
-
-            val ocarina_base = Achievement("ocarina_base", "ocarinaBase", null, null)
-            val temptation_ocarina = Achievement("temptation_ocarina", "ocarinaTemptation", ocarina_base, "goal")
-
-            val magic_wand_base = Achievement("magic_wand_base", "magicWandBase", null, null)
-            val light_magic_wand = Achievement("light_magic_wand", "magicWandLight", magic_wand_base, "goal")
-            val collecting_magic_wand = Achievement("collecting_magic_wand", "magicWandCollecting", magic_wand_base, "goal")
-            val charging_rod = Achievement("charging_rod", "chargingRod", magic_wand_base, null)
-            val lightning_magic_wand = Achievement("lightning_magic_wand", "magicWandLightning", charging_rod, "goal")
-
-            val gravity_rod = Achievement("gravity_rod", "gravityRod", null, "goal")
-
-            val ryugyo_drill = Achievement("ryugyo_drill", "ryugyoDrill", null, "goal")
-
-            val prayer_wheel = Achievement("prayer_wheel", "prayerWheel", null, null)
-            val prayer_wheel_2 = Achievement("prayer_wheel_2", "prayerWheel2", prayer_wheel, null)
-            val prayer_wheel_3 = Achievement("prayer_wheel_3", "prayerWheel3", prayer_wheel_2, "goal")
 
         }
 
