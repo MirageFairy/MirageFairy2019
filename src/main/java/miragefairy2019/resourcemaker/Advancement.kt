fb8c28076570f4c3bec601d040703e7e1c26a42d
MirageFairy <_@example.com>
2022-06-20 02:27:11 +0900

    [format]

--- src/main/java/miragefairy2019/resourcemaker/Advancement.kt
+++ src/main/java/miragefairy2019/resourcemaker/Advancement.kt
@@ -4,5 +4,5 @@ import com.google.gson.JsonElement
 import miragefairy2019.libkt.ModInitializer
 
 fun ModInitializer.makeAdvancement(path: String, creator: () -> JsonElement) = onMakeResource {
-    place("assets/$modId/advancements/${path}.json", creator())
+    place("assets/$modId/advancements/$path.json", creator())
 }
--- src/main/java/miragefairy2019/resourcemaker/BlockModel.kt
+++ src/main/java/miragefairy2019/resourcemaker/BlockModel.kt
@@ -4,5 +4,5 @@ import com.google.gson.JsonElement
 import miragefairy2019.libkt.ModInitializer
 
 fun ModInitializer.makeBlockModel(path: String, creator: () -> JsonElement) = onMakeResource {
-    place("assets/$modId/models/block/${path}.json", creator())
+    place("assets/$modId/models/block/$path.json", creator())
 }
