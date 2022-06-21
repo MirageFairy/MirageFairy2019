75e525f75a02449658942170cefb316ff8c3f57d
MirageFairy <_@example.com>
2022-06-22 00:35:30 +0900

    refactor: MagicArguments

--- src/main/java/miragefairy2019/mod/fairyweapon/magic4/MagicApi.kt
+++ src/main/java/miragefairy2019/mod/fairyweapon/magic4/MagicApi.kt
@@ -1,14 +1,14 @@
 package miragefairy2019.mod.fairyweapon.magic4
 
-import miragefairy2019.mod.fairyweapon.items.ItemFairyWeaponMagic4
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumActionResult
 import net.minecraft.util.EnumHand
 
 interface MagicArguments : FormulaArguments {
-    val weaponItem: ItemFairyWeaponMagic4
+    val weaponItem: Item
     val weaponItemStack: ItemStack
     val player: EntityPlayer
     operator fun <T> MagicStatus<T>.invoke() = formula.calculate(this@MagicArguments)
