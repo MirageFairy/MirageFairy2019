f7607252e062cbd1b82cf8eaadd2d0546c5eaeca
MirageFairy <_@example.com>
2022-06-22 15:19:35 +0900

    CHANGE: ItemMagicWandCollecting

--- src/main/java/miragefairy2019/mod/fairyweapon/items/ItemMagicWandCollecting.kt
+++ src/main/java/miragefairy2019/mod/fairyweapon/items/ItemMagicWandCollecting.kt
@@ -12,12 +12,14 @@ import miragefairy2019.libkt.randomInt
 import miragefairy2019.mod.fairyweapon.MagicMessage
 import miragefairy2019.mod.fairyweapon.displayText
 import miragefairy2019.mod.fairyweapon.magic4.MagicHandler
-import miragefairy2019.mod.fairyweapon.magic4.duration
-import miragefairy2019.mod.fairyweapon.magic4.float1
+import miragefairy2019.mod.fairyweapon.magic4.float2
 import miragefairy2019.mod.fairyweapon.magic4.integer
 import miragefairy2019.mod.fairyweapon.magic4.magic
+import miragefairy2019.mod.fairyweapon.magic4.percent0
 import miragefairy2019.mod.fairyweapon.magic4.status
 import miragefairy2019.mod.fairyweapon.magic4.world
+import miragefairy2019.mod.skill.Mastery
+import mirrg.kotlin.hydrogen.atLeast
 import mirrg.kotlin.hydrogen.atMost
 import net.minecraft.entity.item.EntityItem
 import net.minecraft.init.SoundEvents
@@ -28,14 +30,15 @@ import net.minecraft.util.SoundCategory
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 import kotlin.math.ceil
-import kotlin.math.pow
+import kotlin.math.floor
 
-class ItemMagicWandCollecting : ItemFairyWeaponBase2() {
-    val additionalReach = status("additionalReach", { !Mana.WIND / 5.0 atMost 8.0 }, { float1 })
-    val radius = status("radius", { 2.0 + !Mana.FIRE / 10.0 + !Erg.WARP / 10.0 atMost 7.0 }, { float1 })
-    val maxTargetCount = status("maxTargetCount", { (1.0 + !Mana.GAIA / 2.0 + !Erg.SPACE / 2.0 atMost 20.0).toInt() }, { integer })
-    val wear = status("wear", { 0.25 * 0.5.pow(!Mana.AQUA / 30.0) }, { float1 })
-    val coolTime = status("coolTime", { cost * 3.0 * 0.5.pow(!Mana.DARK / 40.0) }, { duration })
+class ItemMagicWandCollecting : ItemFairyWeaponMagic4() {
+    val speed = status("speed", { (5.0 + !Mana.WIND / 5.0 + !Erg.WARP / 2.0) }, { float2 })
+    val speedBoost = status("speedBoost", { 1.0 + !Mastery.magicCombat / 100 }, { percent0 })
+    val additionalReach = status("additionalReach", { 0.0 + (!Mana.GAIA + !Erg.SHOOT) / 5.0 atMost 30.0 }, { float2 })
+    val radius = status("radius", { 4.0 + (!Mana.AQUA + !Erg.SENSE) / 20.0 atMost 20.0 }, { float2 })
+    val maxTargetCount = status("maxTargetCount", { floor(20.0 * costFactor * costFactor).toInt() atLeast 1 }, { integer })
+    val wear = status("wear", { 0.1 / (1.0 + (!Mana.FIRE + !Erg.SPACE) / 20.0) }, { percent0 })
 
     @SideOnly(Side.CLIENT)
     override fun getMagicDescription(itemStack: ItemStack) = "右クリックでアイテムを回収" // TODO translate
@@ -47,7 +50,20 @@ class ItemMagicWandCollecting : ItemFairyWeaponBase2() {
         val targetsMagicSelector = rangeMagicSelector.entities(EntityItem::class.java, { true }, maxTargetCount())
         val targets = targetsMagicSelector.item.entities
 
-        fun error(color: Int, magicMessage: MagicMessage) = object : MagicHandler() {
+        fun pass(color: Int, magicMessage: MagicMessage) = object : MagicHandler() {
+            override fun onClientUpdate(itemSlot: Int, isSelected: Boolean) {
+                cursorMagicSelector.item.doEffect(color)
+                rangeMagicSelector.item.doEffect()
+                targetsMagicSelector.item.doEffect()
+            }
+
+            override fun onItemRightClick(hand: EnumHand): EnumActionResult {
+                if (!world.isRemote) player.sendStatusMessage(magicMessage.displayText, true)
+                return EnumActionResult.PASS
+            }
+        }
+
+        fun fail(color: Int, magicMessage: MagicMessage) = object : MagicHandler() {
             override fun onClientUpdate(itemSlot: Int, isSelected: Boolean) {
                 cursorMagicSelector.item.doEffect(color)
                 rangeMagicSelector.item.doEffect()
@@ -60,10 +76,10 @@ class ItemMagicWandCollecting : ItemFairyWeaponBase2() {
             }
         }
 
-        if (!hasPartnerFairy) return@magic error(0xFF00FF, MagicMessage.NO_FAIRY) // パートナー妖精判定
-        if (weaponItemStack.itemDamage + ceil(wear()).toInt() > weaponItemStack.maxDamage) return@magic error(0xFF0000, MagicMessage.INSUFFICIENT_DURABILITY) // 耐久判定
-        if (targets.isEmpty()) return@magic error(0xFF8800, MagicMessage.NO_TARGET) // 対象判定
-        if (player.cooldownTracker.hasCooldown(weaponItem)) return@magic error(0xFFFF00, MagicMessage.COOL_TIME) // クールタイム判定
+        if (!hasPartnerFairy) return@magic fail(0xFF00FF, MagicMessage.NO_FAIRY) // パートナー妖精判定
+        if (weaponItemStack.itemDamage + ceil(wear()).toInt() > weaponItemStack.maxDamage) return@magic fail(0xFF0000, MagicMessage.INSUFFICIENT_DURABILITY) // 耐久判定
+        if (targets.isEmpty()) return@magic pass(0xFF8800, MagicMessage.NO_TARGET) // 対象判定
+        if (player.cooldownTracker.hasCooldown(weaponItem)) return@magic pass(0xFFFF00, MagicMessage.COOL_TIME) // クールタイム判定
 
         // 魔法成立
         object : MagicHandler() {
@@ -95,10 +111,9 @@ class ItemMagicWandCollecting : ItemFairyWeaponBase2() {
                     }
 
                     if (successed > 0) {
-                        val rate = successed.toDouble() / maxTargetCount().toDouble()
 
                         // 行使ごとの消費
-                        player.cooldownTracker.setCooldown(this@ItemMagicWandCollecting, ceil(coolTime() * rate).toInt()) // クールタイム
+                        player.cooldownTracker.setCooldown(this@ItemMagicWandCollecting, ceil(successed * 20.0 / (speed() * speedBoost())).toInt()) // クールタイム
 
                         // 行使ごとのエフェクト
                         world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F) // 音
