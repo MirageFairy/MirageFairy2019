c05b920e558b3bdda9f646a3d2e6b1ad0ec790ab
MirageFairy <_@example.com>
2022-06-24 20:30:02 +0900

    internal: add: INSUFFICIENT_RESOURCE

--- src/main/java/miragefairy2019/mod/fairyweapon/MagicMessage.kt
+++ src/main/java/miragefairy2019/mod/fairyweapon/MagicMessage.kt
@@ -6,6 +6,7 @@ import miragefairy2019.libkt.textComponent
 
 enum class MagicMessage(val unlocalizedName: String) {
     NO_FAIRY("noFairy"),
+    INSUFFICIENT_RESOURCE("insufficientResource"),
     INSUFFICIENT_DURABILITY("insufficientDurability"),
     NO_TARGET("noTarget"),
     COOL_TIME("coolTime"),
@@ -16,6 +17,7 @@ val MagicMessage.displayText get() = textComponent { translate("miragefairy2019.
 val magicMessageModule = module {
     onMakeLang {
         enJa("miragefairy2019.magic.${MagicMessage.NO_FAIRY.unlocalizedName}.text", "You don't have a fairy", "妖精を所持していません")
+        enJa("miragefairy2019.magic.${MagicMessage.INSUFFICIENT_RESOURCE.unlocalizedName}.text", "Insufficient resources", "資源が不足しています")
         enJa("miragefairy2019.magic.${MagicMessage.INSUFFICIENT_DURABILITY.unlocalizedName}.text", "Insufficient durability", "耐久値が不足しています")
         enJa("miragefairy2019.magic.${MagicMessage.NO_TARGET.unlocalizedName}.text", "There is no target", "発動対象がありません")
         enJa("miragefairy2019.magic.${MagicMessage.COOL_TIME.unlocalizedName}.text", "Cool time remains", "クールタイムが残っています")
