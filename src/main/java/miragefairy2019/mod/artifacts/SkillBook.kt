f7de92d4d3dcac06736064e1f7b5a02bf5c12726
MirageFairy <_@example.com>
2022-06-24 16:56:56 +0900

    VIEW: skill book: ツールチップ表示追加

--- src/main/java/miragefairy2019/mod/artifacts/SkillBook.kt
+++ src/main/java/miragefairy2019/mod/artifacts/SkillBook.kt
@@ -5,12 +5,26 @@ import miragefairy2019.lib.modinitializer.module
 import miragefairy2019.lib.modinitializer.setCreativeTab
 import miragefairy2019.lib.modinitializer.setCustomModelResourceLocation
 import miragefairy2019.lib.modinitializer.setUnlocalizedName
+import miragefairy2019.lib.proxy
 import miragefairy2019.lib.resourcemaker.generated
 import miragefairy2019.lib.resourcemaker.makeItemModel
+import miragefairy2019.lib.skillContainer
+import miragefairy2019.libkt.TextComponentScope
+import miragefairy2019.libkt.TextComponentWrapper
+import miragefairy2019.libkt.blue
 import miragefairy2019.libkt.enJa
+import miragefairy2019.libkt.formattedText
+import miragefairy2019.libkt.green
+import miragefairy2019.libkt.plus
+import miragefairy2019.libkt.red
+import miragefairy2019.libkt.white
 import miragefairy2019.mod.GuiId
 import miragefairy2019.mod.Main
 import miragefairy2019.mod.ModMirageFairy2019
+import miragefairy2019.mod.skill.canResetMastery
+import miragefairy2019.mod.skill.getRequiredFairyMasterExpForNextLevel
+import miragefairy2019.mod.skill.remainingSkillPoints
+import net.minecraft.client.Minecraft
 import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.Item
@@ -21,6 +35,7 @@ import net.minecraft.util.EnumHand
 import net.minecraft.world.World
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
+import java.time.Instant
 
 object SkillBook {
     lateinit var itemSkillBook: () -> ItemSkillBook
@@ -33,6 +48,12 @@ object SkillBook {
         }
         onMakeLang {
             enJa("item.skillBook.name", "Skill Book", "スキルブック")
+            enJa("skill_book.fairyMasterLevel", "Fairy Master Level", "フェアリーマスターレベル")
+            enJa("skill_book.requiredFairyMasterXp", "Required Fairy Master XP", "必要経験値")
+            enJa("skill_book.sp", "SP", "SP")
+            enJa("skill_book.masteryLevelReset", "Mastery Level Reset", "マスタリレベル初期化")
+            enJa("skill_book.possible", "Possible", "可能")
+            enJa("skill_book.impossible", "Impossible", "不可能")
         }
     }
 }
@@ -45,6 +66,20 @@ class ItemSkillBook : Item() {
 
     @SideOnly(Side.CLIENT)
     override fun addInformation(itemStack: ItemStack, world: World?, tooltip: MutableList<String>, flag: ITooltipFlag) {
-        // TODO 現在スキルレベルとか表示
+        val player = Minecraft.getMinecraft().player ?: return
+        val skillContainer = player.proxy.skillContainer
+
+        fun parameter(key: String, valueSupplier: TextComponentScope.() -> TextComponentWrapper) {
+            tooltip += formattedText { (translate(key) + ": "() + valueSupplier().white).blue }
+        }
+        parameter("skill_book.fairyMasterLevel") { "${skillContainer.skillManager.getFairyMasterLevel(skillContainer.variables.getExp())}"() }
+        parameter("skill_book.requiredFairyMasterXp") { "${skillContainer.skillManager.getRequiredFairyMasterExpForNextLevel(skillContainer.variables.getExp())}"() }
+        parameter("skill_book.sp") { "${skillContainer.remainingSkillPoints}"() }
+        parameter("skill_book.masteryLevelReset") {
+            when (skillContainer.canResetMastery(Instant.now())) {
+                true -> translate("skill_book.possible").green
+                false -> translate("skill_book.impossible").red
+            }
+        }
     }
 }
