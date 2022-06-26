993576bba73bd58fc549f1b4bf71ab77baabe47c
MirageFairy <_@example.com>
2022-06-26 19:44:29 +0900

    refactor: OreName

--- src/main/java/miragefairy2019/libkt/OreName.kt
+++ src/main/java/miragefairy2019/libkt/OreName.kt
@@ -5,7 +5,6 @@ import net.minecraft.item.ItemStack
 import net.minecraftforge.oredict.OreDictionary
 
 fun OreName.getItemStacks(): List<ItemStack> = OreDictionary.getOres(this.string)
-fun OreName.getItemStack() = getItemStacks().firstOrNull()
 
 @Deprecated(message = "下位素材の辞書名を経由して過剰な品質の素材が返却される可能性があります。ワイルドカードにより不正なアイテムが返却される可能性があります。")
-fun OreName.copyItemStack(count: Int = 1) = getItemStack()?.copy(count)
+fun OreName.copyItemStack(count: Int = 1) = getItemStacks().firstOrNull()?.copy(count)
