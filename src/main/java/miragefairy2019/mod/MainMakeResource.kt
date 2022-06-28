57714fc3cd1568779f15a87b6e2b170010e1c5d1
MirageFairy <_@example.com>
2022-06-29 00:36:07 +0900

    refactor: kotlin: fairy lang

--- src/main/java/miragefairy2019/mod/MainMakeResource.kt
+++ src/main/java/miragefairy2019/mod/MainMakeResource.kt
@@ -14,21 +14,6 @@ object MainMakeResource {
         modInitializer.onMakeResource(ResourceMaker(File("src/main/resources")))
 
         val langMaker = LangMaker(File("src/main/resources/assets/miragefairy2019/lang"))
-        File("src/lang").listFiles()!!.filter { it.isDirectory }.forEach { languageDir ->
-            languageDir.listFiles()!!.filter { it.isFile }.forEach { langFile ->
-                langFile.readText().lines().forEachIndexed ignoredLine@{ i, line ->
-                    if (line.isBlank()) return@ignoredLine
-                    if (line.startsWith("#")) return@ignoredLine
-
-                    val array = line.split("=")
-                    langMaker.registerTranslation(
-                        languageDir.name,
-                        array.getOrNull(0) ?: throw Exception("malformed line: $line (${langFile.path}:${i + 1})"),
-                        array.getOrNull(1) ?: throw Exception("malformed line: $line (${langFile.path}:${i + 1})")
-                    )
-                }
-            }
-        }
         modInitializer.onMakeLang(langMaker)
         langMaker.make()
 
