07abb23d41718e167b5b7c7fc1d55e92eed74a32
MirageFairy <_@example.com>
2022-07-01 17:39:14 +0900

    cleanup: FairyMetamorphosis

--- src/main/java/miragefairy2019/mod/recipes/FairyMetamorphosis.kt
+++ src/main/java/miragefairy2019/mod/recipes/FairyMetamorphosis.kt
@@ -13,7 +13,7 @@ val fairyMetamorphosisModule = module {
         makeRecipe("fairy_metamorphosis/${resultName}_from_${fairyMotif}") {
             DataShapelessRecipe(
                 ingredients = listOf(
-                    DataOreIngredient(type = "miragefairy2019:ore_dict_complex", ore = "mirageFairyStick"),
+                    DataOreIngredient(ore = "mirageFairyStick"),
                     DataOreIngredient(ore = "container250MiragiumWater"),
                     DataOreIngredient(ore = "mirageFairy2019Fairy${fairyMotif.toUpperCamelCase()}Rank1"),
                     DataSimpleIngredient(item = "minecraft:dirt", data = 0)
