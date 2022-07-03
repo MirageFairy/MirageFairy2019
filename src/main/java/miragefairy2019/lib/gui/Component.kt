ab03a14ffa3dba2ed6dfa48a207e70b69579b1a2
MirageFairy <_@example.com>
2022-07-03 19:33:57 +0900

    refactor: move: ContainerComponent, GuiComponent

--- src/main/java/miragefairy2019/lib/gui/Component.kt
+++ src/main/java/miragefairy2019/lib/gui/Component.kt
@@ -4,6 +4,7 @@ import miragefairy2019.libkt.IArgb
 import miragefairy2019.libkt.PointInt
 import miragefairy2019.libkt.RectangleInt
 import miragefairy2019.libkt.contains
+import miragefairy2019.libkt.drawGuiBackground
 import miragefairy2019.libkt.drawSlot
 import miragefairy2019.libkt.drawString
 import miragefairy2019.libkt.drawStringCentered
@@ -11,8 +12,11 @@ import miragefairy2019.libkt.drawStringRightAligned
 import miragefairy2019.libkt.minus
 import miragefairy2019.libkt.toArgb
 import mirrg.kotlin.hydrogen.unit
+import net.minecraft.client.gui.FontRenderer
 import net.minecraft.client.gui.Gui
+import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.inventory.Container
 import net.minecraft.inventory.Slot
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.text.ITextComponent
@@ -37,6 +41,51 @@ interface IComponent {
 
     @SideOnly(Side.CLIENT)
     fun mouseClicked(gui: GuiComponent, mouse: PointInt, mouseButton: Int) = Unit
+
+}
+
+abstract class ContainerComponent : Container() {
+    val components = mutableListOf<IComponent>()
+
+    fun init() {
+        components.forEach { it.onContainerInit() }
+    }
+
+
+    // Public化
+    public override fun addSlotToContainer(slot: Slot): Slot = super.addSlotToContainer(slot)
+
+}
+
+@SideOnly(Side.CLIENT)
+abstract class GuiComponent(private val container: ContainerComponent) : GuiContainer(container) {
+
+    // イベント
+
+    override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
+        drawDefaultBackground()
+        super.drawScreen(mouseX, mouseY, partialTicks)
+        renderHoveredToolTip(mouseX, mouseY)
+        container.components.forEach { it.drawScreen(this, PointInt(mouseX, mouseY), partialTicks) }
+    }
+
+    override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) {
+        rectangle.drawGuiBackground()
+        container.components.forEach { it.drawGuiContainerBackgroundLayer(this, PointInt(mouseX, mouseY), partialTicks) }
+    }
+
+    override fun drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int) {
+        container.components.forEach { it.drawGuiContainerForegroundLayer(this, PointInt(mouseX, mouseY)) }
+    }
+
+    override fun mouseClicked(mouseX: Int, mouseY: Int, mouseButton: Int) {
+        container.components.forEach { it.mouseClicked(this, PointInt(mouseX, mouseY), mouseButton) }
+        super.mouseClicked(mouseX, mouseY, mouseButton)
+    }
+
+
+    // Public化
+    val fontRenderer: FontRenderer get() = super.fontRenderer
 
 }
 
