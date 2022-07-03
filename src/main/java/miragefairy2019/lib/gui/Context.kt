de486d87d912c6d7e1ba4252cfb6a02d1a40971e
MirageFairy <_@example.com>
2022-07-03 19:28:11 +0900

    internal: Context

--- src/main/java/miragefairy2019/lib/gui/Context.kt
+++ src/main/java/miragefairy2019/lib/gui/Context.kt
@@ -5,9 +5,9 @@ import miragefairy2019.libkt.RectangleInt
 
 class RectangleContext(val container: ContainerComponent, val rectangle: RectangleInt)
 
-fun ContainerComponent.rectangle(rectangle: RectangleInt, block: RectangleContext.() -> Unit) = block(RectangleContext(this, rectangle))
+fun ContainerComponent.rectangle(rectangle: RectangleInt, block: RectangleContext.() -> Unit = {}) = RectangleContext(this, rectangle).also { block(it) }
 
 
 class PointContext(val container: ContainerComponent, val point: PointInt)
 
-fun ContainerComponent.point(x: Int, y: Int, block: PointContext.() -> Unit) = block(PointContext(this, PointInt(x, y)))
+fun ContainerComponent.point(x: Int, y: Int, block: PointContext.() -> Unit = {}) = PointContext(this, PointInt(x, y)).also { block(it) }
