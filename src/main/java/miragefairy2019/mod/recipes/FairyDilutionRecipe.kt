98c2deb8611073f861c69578fdae1e4ee568921d
MirageFairy <_@example.com>
2022-07-10 06:01:37 +0900

    refactor: FairyDilutionRecipe

--- src/main/java/miragefairy2019/mod/recipes/FairyDilutionRecipe.kt
+++ src/main/java/miragefairy2019/mod/recipes/FairyDilutionRecipe.kt
@@ -14,22 +14,22 @@ import net.minecraftforge.oredict.OreIngredient
 
 val fairyDilutionRecipeModule = module {
     onAddRecipe {
-        FairyCard.values().forEach { fairyCard ->
-            val inputFairyBundle = fairyCard
-            val inputFairy = inputFairyBundle.getVariant(1)
-            val outputFairyBundle = fairyCard.getVariant(1).type.parentFairy() ?: return@forEach
-            val outputFairy = outputFairyBundle.getVariant(1)
-            val rankDiff = (outputFairy.rare - inputFairy.rare) atLeast 0 // 必要凝縮回数
+        FairyCard.values().forEach { inputFairyCard ->
+            val inputFairyCard = inputFairyCard
+            val inputFairyVariant = inputFairyCard.getVariant(1)
+            val outputFairyCard = inputFairyCard.getVariant(1).type.parentFairy() ?: return@forEach
+            val outputFairyVariant = outputFairyCard.getVariant(1)
+            val rankDiff = (outputFairyVariant.rare - inputFairyVariant.rare) atLeast 0 // 必要凝縮回数
 
-            val inputMotif = inputFairy.type.motif ?: return@forEach
+            val inputMotif = inputFairyVariant.type.motif ?: return@forEach
 
             GameRegistry.addShapelessRecipe(
                 ResourceLocation("${inputMotif.resourceDomain}:fairy_dilution/${inputMotif.resourcePath}"),
                 null,
-                outputFairy.createItemStack(),
+                outputFairyVariant.createItemStack(),
                 WandType.SUMMONING.ingredient,
                 OreIngredient("container1000MirageFlowerExtract"),
-                inputFairyBundle.createItemStack(1 + rankDiff).ingredient // レベルが上昇するような希釈はできない
+                inputFairyCard.createItemStack(1 + rankDiff).ingredient // レベルが上昇するような希釈はできない
             )
         }
     }
