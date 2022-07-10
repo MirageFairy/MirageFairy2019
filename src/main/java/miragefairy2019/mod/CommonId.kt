f06d360692a13bc64885ced27b1ab6f3f94ae532
MirageFairy <_@example.com>
2022-07-10 19:45:42 +0900

    refactor: merge: CommonId

--- src/main/java/miragefairy2019/mod/CommonId.kt
+++ src/main/java/miragefairy2019/mod/CommonId.kt
@@ -1,5 +1,10 @@
 package miragefairy2019.mod
 
+object GuiId {
+    const val commonTileEntity = 1
+    const val skill = 2
+}
+
 object PacketId {
     const val placedItem = 0
     const val playerAura = 1
--- src/main/java/miragefairy2019/mod/GuiId.kt
+++
@@ -1,6 +0,0 @@
-package miragefairy2019.mod
-
-object GuiId {
-    const val commonTileEntity = 1
-    const val skill = 2
-}
