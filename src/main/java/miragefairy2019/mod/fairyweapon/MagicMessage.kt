ac03e39764ac6821649296e8667241ebabd3b95c
MirageFairy <_@example.com>
2022-07-12 17:02:03 +0900

    fix: MagicMessage

--- src/main/java/miragefairy2019/mod/fairyweapon/MagicMessage.kt
+++ src/main/java/miragefairy2019/mod/fairyweapon/MagicMessage.kt
@@ -4,24 +4,21 @@ import miragefairy2019.lib.modinitializer.module
 import miragefairy2019.libkt.enJa
 import miragefairy2019.libkt.textComponent
 
-enum class MagicMessage(val unlocalizedName: String) {
-    NO_FAIRY("noFairy"),
-    INSUFFICIENT_RESOURCE("insufficientResource"),
-    INSUFFICIENT_DURABILITY("insufficientDurability"),
-    NO_TARGET("noTarget"),
-    COOL_TIME("coolTime"),
-    INVALID_TARGET("insufficientDurability"),
+enum class MagicMessage(val unlocalizedName: String, val englishText: String, val japaneseText: String) {
+    NO_FAIRY("noFairy", "You don't have a fairy", "妖精を所持していません"),
+    INSUFFICIENT_RESOURCE("insufficientResource", "Insufficient resources", "資源が不足しています"),
+    INSUFFICIENT_DURABILITY("insufficientDurability", "Insufficient durability", "耐久値が不足しています"),
+    NO_TARGET("noTarget", "There is no target", "発動対象がありません"),
+    COOL_TIME("coolTime", "Cool time remains", "クールタイムが残っています"),
+    INVALID_TARGET("invalidTarget", "This target is not supported", "非対応の対象です"),
 }
 
 val MagicMessage.displayText get() = textComponent { translate("miragefairy2019.magic.$unlocalizedName.text") }
 
 val magicMessageModule = module {
-    onMakeLang {
-        enJa("miragefairy2019.magic.${MagicMessage.NO_FAIRY.unlocalizedName}.text", "You don't have a fairy", "妖精を所持していません")
-        enJa("miragefairy2019.magic.${MagicMessage.INSUFFICIENT_RESOURCE.unlocalizedName}.text", "Insufficient resources", "資源が不足しています")
-        enJa("miragefairy2019.magic.${MagicMessage.INSUFFICIENT_DURABILITY.unlocalizedName}.text", "Insufficient durability", "耐久値が不足しています")
-        enJa("miragefairy2019.magic.${MagicMessage.NO_TARGET.unlocalizedName}.text", "There is no target", "発動対象がありません")
-        enJa("miragefairy2019.magic.${MagicMessage.COOL_TIME.unlocalizedName}.text", "Cool time remains", "クールタイムが残っています")
-        enJa("miragefairy2019.magic.${MagicMessage.INVALID_TARGET.unlocalizedName}.text", "This target is not supported", "非対応の対象です")
+    MagicMessage.values().forEach { magicMessage ->
+        onMakeLang {
+            enJa("miragefairy2019.magic.${magicMessage.unlocalizedName}.text", magicMessage.englishText, magicMessage.japaneseText)
+        }
     }
 }
