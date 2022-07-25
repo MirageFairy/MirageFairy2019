e00581a6ce9e801c92e2b0b9594592e192154b2b
MirageFairy <_@example.com>
2022-07-25 11:30:30 +0900

    fix: MirageFlower

--- src/main/java/miragefairy2019/mod/magicplant/MirageFlower.kt
+++ src/main/java/miragefairy2019/mod/magicplant/MirageFlower.kt
@@ -228,8 +228,9 @@ class BlockMirageFlower : BlockMagicPlant(3) {
 
     override fun grow(world: World, blockPos: BlockPos, blockState: IBlockState, random: Random) {
         repeat(random.randomInt(mirageFlowerGrowthHandlers.getGrowthRateModifiers(world, blockPos).growthRate)) {
-            if (!canGrow(getAge(blockState))) return
-            world.setBlockState(blockPos, defaultState.withProperty(AGE, getAge(blockState) + 1), 2)
+            val blockState2 = world.getBlockState(blockPos)
+            if (!canGrow(getAge(blockState2))) return
+            world.setBlockState(blockPos, defaultState.withProperty(AGE, getAge(blockState2) + 1), 2)
         }
     }
 
