1514bee30baaea2bbc76abb2d7192da08f592f4d
MirageFairy <_@example.com>
2022-08-09 14:45:15 +0900

    format:

--- src/main/java/miragefairy2019/mod/beanstalk/Beanstalk.kt
+++ src/main/java/miragefairy2019/mod/beanstalk/Beanstalk.kt
@@ -3,8 +3,6 @@ package miragefairy2019.mod.beanstalk
 import miragefairy2019.lib.modinitializer.module
 
 val beanstalkModule = module {
-
     beanstalkPipeModule()
     beanstalkRootModule()
-    
 }
