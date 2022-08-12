780d14e08ec28a5d937b40f1c2e15ed8963a71cd
MirageFairy <_@example.com>
2022-08-12 16:24:40 +0900

    internal: add: List<AxisAlignedBB>.union

--- src/main/java/miragefairy2019/libkt/AxisAlignedBB.kt
+++ src/main/java/miragefairy2019/libkt/AxisAlignedBB.kt
@@ -5,3 +5,5 @@ import net.minecraft.util.math.Vec3d
 
 fun axisAlignedBBOf(min: Vec3d, max: Vec3d) = AxisAlignedBB(min.x, min.y, min.z, max.x, max.y, max.z)
 fun axisAlignedBBOf(point: Vec3d) = axisAlignedBBOf(point, point)
+
+fun List<AxisAlignedBB>.union() = if (this.isNotEmpty()) this.reduce { a, b -> a.union(b) } else null
