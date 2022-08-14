0159c7fb95393b12beec60947e915629f25df6ed
MirageFairy <_@example.com>
2022-08-15 08:32:45 +0900

    internal: add: lib\TileEntity

---
+++ src/main/java/miragefairy2019/lib/TileEntity.kt
@@ -0,0 +1,15 @@
+package miragefairy2019.lib
+
+import net.minecraft.item.ItemStack
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.items.IItemHandler
+import net.minecraftforge.items.IItemHandlerModifiable
+
+fun <T> TileEntity.getCapabilityIfHas(capability: Capability<T>, facing: EnumFacing?) = if (this.hasCapability(capability, facing)) this.getCapability(capability, facing) else null
+
+operator fun IItemHandler.get(index: Int) = this.getStackInSlot(index)
+operator fun IItemHandlerModifiable.set(index: Int, itemStack: ItemStack) = this.setStackInSlot(index, itemStack)
+val IItemHandler.size get() = this.slots
+val IItemHandler.itemStacks get() = (0 until this.size).map { this[it] }
