4a1677d5ffcd7f1fcc6c9ab0b0c5e8def0171586
MirageFairy <_@example.com>
2022-08-16 11:00:58 +0900

    internal: add: IItemHandler.indices

--- src/main/java/miragefairy2019/lib/TileEntity.kt
+++ src/main/java/miragefairy2019/lib/TileEntity.kt
@@ -12,4 +12,5 @@ fun <T> TileEntity.getCapabilityIfHas(capability: Capability<T>, facing: EnumFac
 operator fun IItemHandler.get(index: Int) = this.getStackInSlot(index)
 operator fun IItemHandlerModifiable.set(index: Int, itemStack: ItemStack) = this.setStackInSlot(index, itemStack)
 val IItemHandler.size get() = this.slots
+val IItemHandler.indices get() = 0 until this.size
 val IItemHandler.itemStacks get() = (0 until this.size).map { this[it] }
