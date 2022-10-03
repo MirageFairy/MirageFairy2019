cb481e0841d9b08f2b16d37e708e131799c07eae
MirageFairy <_@example.com>
2022-10-03 20:09:59 +0900

    ADD: オーラ検査キット

--- src/main/java/miragefairy2019/mod/aura/Aura.kt
+++ src/main/java/miragefairy2019/mod/aura/Aura.kt
@@ -5,8 +5,13 @@ import miragefairy2019.lib.modinitializer.module
 import miragefairy2019.lib.modinitializer.setCreativeTab
 import miragefairy2019.lib.modinitializer.setCustomModelResourceLocation
 import miragefairy2019.lib.modinitializer.setUnlocalizedName
+import miragefairy2019.lib.resourcemaker.DataOreIngredient
+import miragefairy2019.lib.resourcemaker.DataResult
+import miragefairy2019.lib.resourcemaker.DataShapelessRecipe
+import miragefairy2019.lib.resourcemaker.DataSimpleIngredient
 import miragefairy2019.lib.resourcemaker.generated
 import miragefairy2019.lib.resourcemaker.makeItemModel
+import miragefairy2019.lib.resourcemaker.makeRecipe
 import miragefairy2019.libkt.enJa
 import miragefairy2019.mod.Main
 
@@ -20,6 +25,17 @@ val auraModule = module {
         setCreativeTab { Main.creativeTab }
         setCustomModelResourceLocation()
         makeItemModel { generated }
+        makeRecipe {
+            DataShapelessRecipe(
+                ingredients = listOf(
+                    DataSimpleIngredient(item = "minecraft:writable_book"),
+                    DataOreIngredient(ore = "mirageFairyCrystal"),
+                    DataOreIngredient(ore = "dustMiragium"),
+                    DataOreIngredient(ore = "gemQuartz")
+                ),
+                result = DataResult(item = "miragefairy2019:aura_book")
+            )
+        }
     }
     onMakeLang { enJa("item.auraBook.name", "Aura Test Kit", "オーラ検査キット") }
 
