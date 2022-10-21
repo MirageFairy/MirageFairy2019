a00c32211d3deb1c8bb609cd0636be893a6eac55
MirageFairy <_@example.com>
2022-10-22 04:08:09 +0900

    cleanup: LangMaker

--- src/main/java/miragefairy2019/libkt/LangMaker.kt
+++
@@ -1,8 +0,0 @@
-package miragefairy2019.libkt
-
-import miragefairy2019.lib.resourcemaker.LangMaker
-
-fun LangMaker.enJa(key: String, english: String?, japanese: String?) {
-    if (english != null) registerTranslation("en_us", key, english)
-    if (japanese != null) registerTranslation("ja_jp", key, japanese)
-}
