a08a0135ee333d6f0f5952ea0eda6a6b728c254c
MirageFairy <_@example.com>
2023-09-23 10:15:22 +0900

    v0.2.27.65

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 // Forge対応バージョンの区別
 // クライアント互換性の区別
 // ビルドバージョンの区別
-val versionTag = "v0.2.27.64"
+val versionTag = "v0.2.27.65"
 val versionZero = versionTag.substring(1).split(".")[0].toInt()
 val versionForge = versionTag.substring(1).split(".")[1].toInt()
 val versionCompatibility = versionTag.substring(1).split(".")[2].toInt()
